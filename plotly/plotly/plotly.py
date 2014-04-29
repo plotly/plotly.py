8245b6b1fd9cb02c2f5975959af2998a9c4e3ad7
chriddyp <christopher.parmer@gmail.com>
2014-04-29 02:26:08 -0400

    image to all

--- plotly/plotly/plotly.py
+++ plotly/plotly/plotly.py
@@ -28,7 +28,7 @@ from .. import version
 
 __all__ = ["sign_in", "update_plot_options", "get_plot_options",
            "get_credentials", "iplot", "plot", "iplot_mpl", "plot_mpl",
-           "get_figure", "Stream"]
+           "get_figure", "Stream", "image"]
 
 _DEFAULT_PLOT_OPTIONS = dict(
     filename="plot from API",
