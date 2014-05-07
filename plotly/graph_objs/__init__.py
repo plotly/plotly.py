72039daa7a1a2d9b4716ac61b4953ca252ecdff9
theengineear <andseier@gmail.com>
2014-05-07 16:16:35 -0700

    Added docs to packages that hold same-name modules for better help.

--- plotly/graph_objs/__init__.py
+++ plotly/graph_objs/__init__.py
@@ -1,3 +1,45 @@
+"""
+graph_objs
+==========
+
+This package imports definitions for all of Plotly's graph objects. For more
+information, run help(Obj) on any of the following objects defined here...
+
+Defined Objects:
+
+    Data Objects (things that make up your 'Data' list:
+        Bar
+        Box
+        Contour
+        Heatmap
+        Histogram
+        Histogram2d
+        Histogram2dContour
+        Scatter
+
+    Other Objects (everything else):
+
+        Data
+        Annotations
+        Annotation
+        ColorBar
+        Contours
+        ErrorY
+        Figure
+        Font
+        Layout
+        Legend
+        Line
+        Margin
+        Marker
+        Stream
+        Trace
+        XAxis
+        XBins
+        YAxis
+        YBins
+
+"""
 from graph_objs import *
 
 __all__ = ["Data", "Annotations", "Bar", "Box", "Contour", "Heatmap",
