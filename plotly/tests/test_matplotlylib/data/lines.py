b6bcbb163daf4bd57cafbac2494686d2b41f08a0
theengineear <andseier@gmail.com>
2014-05-14 19:34:29 -0700

    Modified tests for new changes, added notebooks to aid test process.

--- plotly/tests/test_matplotlylib/data/lines.py
+++ plotly/tests/test_matplotlylib/data/lines.py
@@ -1,3 +1,5 @@
+from plotly.graph_objs import *
+
 D = dict(
     x1=[0, 1, 2, 3, 4, 5],
     y1=[10, 20, 50, 80, 100, 200],
@@ -5,108 +7,182 @@ D = dict(
     y2=[1, 4, 8, 16, 32, 64, 128]
 )
 
-SIMPLE_LINE = {
-    'data': [{'line': {'color': '#0000FF',
-                       'dash': 'solid',
-                       'opacity': 1,
-                       'width': 1.0},
-              'type': 'scatter',
-              'mode': 'lines',
-              'name': 'simple',
-              'xaxis': 'x1',
-              'yaxis': 'y1',
-              'x': [0.0, 1.0, 2.0, 3.0, 4.0, 5.0],
-              'y': [10.0, 20.0, 50.0, 80.0, 100.0, 200.0]}],
-    'layout': {
-        'autosize': False,
-        'height': 480,
-        'margin': {'b': 47, 'l': 80, 'pad': 0, 'r': 63, 't': 47},
-        'showlegend': False,
-        'hovermode': 'closest',
-        'width': 640,
-        'xaxis1': {'anchor': 'y1',
-                  'zeroline': False,
-                  'domain': [0.0, 1.0],
-                  'range': (0.0, 5.0),
-                  'showgrid': False,
-                  'showline': True,
-                  'mirror': True,
-                  'ticks': 'inside'},
-        'yaxis1': {'anchor': 'x1',
-                  'zeroline': False,
-                  'domain': [0.0, 1.0],
-                  'range': (0.0, 200.0),
-                  'showgrid': False,
-                  'showline': True,
-                  'mirror': True,
-                  'ticks': 'inside'}}}
+SIMPLE_LINE = Figure(
+    data=Data([
+        Scatter(
+            x=[0.0, 1.0, 2.0, 3.0, 4.0, 5.0],
+            y=[10.0, 20.0, 50.0, 80.0, 100.0, 200.0],
+            name='simple',
+            mode='lines',
+            line=Line(
+                dash='solid',
+                color='#0000FF',
+                width=1.0,
+                opacity=1
+            ),
+            xaxis='x1',
+            yaxis='y1'
+        )
+    ]),
+    layout=Layout(
+        width=640,
+        height=480,
+        autosize=False,
+        margin=Margin(
+            l=80,
+            r=63,
+            b=47,
+            t=47,
+            pad=0
+        ),
+        hovermode='closest',
+        showlegend=False,
+        xaxis1=XAxis(
+            domain=[0.0, 1.0],
+            range=[0.0, 5.0],
+            type='linear',
+            showline=True,
+            ticks='inside',
+            nticks=6,
+            showgrid=False,
+            zeroline=False,
+            tickfont=Font(
+                size=12.0
+            ),
+            anchor='y1',
+            side='bottom',
+            mirror='ticks'
+        ),
+        yaxis1=YAxis(
+            domain=[0.0, 1.0],
+            range=[0.0, 200.0],
+            type='linear',
+            showline=True,
+            ticks='inside',
+            nticks=5,
+            showgrid=False,
+            zeroline=False,
+            tickfont=Font(
+                size=12.0
+            ),
+            anchor='x1',
+            side='left',
+            mirror='ticks'
+        )
+    )
+)
 
-COMPLICATED_LINE = {
-    'data': [{'marker': {'color': '#FF0000',
-                         'line': {'color': '#000000', 'width': 0.5},
-                         'opacity': 0.5,
-                         'size': 10,
-                         'symbol': 'dot'},
-              'type': 'scatter',
-              'mode': 'markers',
-              'name': 'one',
-              'xaxis': 'x1',
-              'yaxis': 'y1',
-              'x': [0.0, 1.0, 2.0, 3.0, 4.0, 5.0],
-              'y': [10.0, 20.0, 50.0, 80.0, 100.0, 200.0]},
-             {'line': {'color': '#0000FF',
-                       'dash': 'solid',
-                       'opacity': 0.7,
-                       'width': 2},
-              'type': 'scatter',
-              'mode': 'lines',
-              'name': 'two',
-              'xaxis': 'x1',
-              'yaxis': 'y1',
-              'x': [0.0, 1.0, 2.0, 3.0, 4.0, 5.0],
-              'y': [10.0, 20.0, 50.0, 80.0, 100.0, 200.0]},
-             {'marker': {'color': '#0000FF',
-                         'line': {'color': '#0000FF', 'width': 2},
-                         'opacity': 0.6,
-                         'size': 10,
-                         'symbol': 'cross'},
-              'type': 'scatter',
-              'mode': 'markers',
-              'name': 'three',
-              'xaxis': 'x1',
-              'yaxis': 'y1',
-              'x': [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0],
-              'y': [1.0, 4.0, 8.0, 16.0, 32.0, 64.0, 128.0]},
-             {'line': {'color': '#FF0000',
-                       'dash': 'dash',
-                       'opacity': 0.8,
-                       'width': 2},
-              'type': 'scatter',
-              'mode': 'lines',
-              'name': 'four',
-              'xaxis': 'x1',
-              'yaxis': 'y1',
-              'x': [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0],
-              'y': [1.0, 4.0, 8.0, 16.0, 32.0, 64.0, 128.0]}],
-    'layout': {'autosize': False,
-               'height': 480,
-               'margin': {'b': 47, 'l': 80, 'pad': 0, 'r': 63, 't': 47},
-               'showlegend': False,
-               'hovermode': 'closest',
-               'width': 640,
-               'xaxis1': {'anchor': 'y1',
-                         'zeroline': False,
-                         'domain': [0.0, 1.0],
-                         'range': (0.0, 6.0),
-                         'showgrid': False,
-                         'showline': True,
-                         'mirror': True,
-                         'ticks': 'inside'},
-               'yaxis1': {'anchor': 'x1',
-                         'zeroline': False,
-                         'domain': [0.0, 1.0],
-                         'range': (0.0, 200.0),
-                         'showgrid': False,
-                         'showline': True,
-                         'mirror': True,
-                         'ticks': 'inside'}}}
+COMPLICATED_LINE = Figure(
+    data=Data([
+        Scatter(
+            x=[0.0, 1.0, 2.0, 3.0, 4.0, 5.0],
+            y=[10.0, 20.0, 50.0, 80.0, 100.0, 200.0],
+            name='one',
+            mode='markers',
+            marker=Marker(
+                symbol='dot',
+                line=Line(
+                    color='#000000',
+                    width=0.5
+                ),
+                size=10,
+                color='#FF0000',
+                opacity=0.5
+            ),
+            xaxis='x1',
+            yaxis='y1'
+        ),
+        Scatter(
+            x=[0.0, 1.0, 2.0, 3.0, 4.0, 5.0],
+            y=[10.0, 20.0, 50.0, 80.0, 100.0, 200.0],
+            name='two',
+            mode='lines',
+            line=Line(
+                dash='solid',
+                color='#0000FF',
+                width=2,
+                opacity=0.7
+            ),
+            xaxis='x1',
+            yaxis='y1'
+        ),
+        Scatter(
+            x=[0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0],
+            y=[1.0, 4.0, 8.0, 16.0, 32.0, 64.0, 128.0],
+            name='three',
+            mode='markers',
+            marker=Marker(
+                symbol='cross',
+                line=Line(
+                    color='#0000FF',
+                    width=2
+                ),
+                size=10,
+                color='#0000FF',
+                opacity=0.6
+            ),
+            xaxis='x1',
+            yaxis='y1'
+        ),
+        Scatter(
+            x=[0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0],
+            y=[1.0, 4.0, 8.0, 16.0, 32.0, 64.0, 128.0],
+            name='four',
+            mode='lines',
+            line=Line(
+                dash='dash',
+                color='#FF0000',
+                width=2,
+                opacity=0.8
+            ),
+            xaxis='x1',
+            yaxis='y1'
+        )
+    ]),
+    layout=Layout(
+        width=640,
+        height=480,
+        autosize=False,
+        margin=Margin(
+            l=80,
+            r=63,
+            b=47,
+            t=47,
+            pad=0
+        ),
+        hovermode='closest',
+        showlegend=False,
+        xaxis1=XAxis(
+            domain=[0.0, 1.0],
+            range=[0.0, 6.0],
+            type='linear',
+            showline=True,
+            ticks='inside',
+            nticks=7,
+            showgrid=False,
+            zeroline=False,
+            tickfont=Font(
+                size=12.0
+            ),
+            anchor='y1',
+            side='bottom',
+            mirror='ticks'
+        ),
+        yaxis1=YAxis(
+            domain=[0.0, 1.0],
+            range=[0.0, 200.0],
+            type='linear',
+            showline=True,
+            ticks='inside',
+            nticks=5,
+            showgrid=False,
+            zeroline=False,
+            tickfont=Font(
+                size=12.0
+            ),
+            anchor='x1',
+            side='left',
+            mirror='ticks'
+        )
+    )
+)
--- plotly/tests/test_matplotlylib/data/scatter.py
+++ plotly/tests/test_matplotlylib/data/scatter.py
@@ -1,3 +1,5 @@
+from plotly.graph_objs import *
+
 D = dict(
     x1=[1, 2, 2, 4, 5, 6, 1, 7, 8, 5 ,3],
     y1=[5, 3, 7, 2, 9, 7, 8, 4, 5, 9, 2],
@@ -5,97 +7,155 @@ D = dict(
     y2=[-0.5, 0.4, 0.7, -0.6, 0.3, -1, 0, 0.3]
 )
 
-SIMPLE_SCATTER = {
-    'data': [{'marker': {'color': 'rgb(0,0,255)',
-                         'line': {'color': 'rgb(0,0,0)', 'width': 1.0},
-                         'opacity': 1.0,
-                         'size': 4.4721359549995796,
-                         'symbol': 'dot'},
-              'type': 'scatter',
-              'mode': 'markers',
-              'xaxis': 'x1',
-              'yaxis': 'y1',
-              'x': [1.0, 2.0, 2.0, 4.0, 5.0, 6.0, 1.0, 7.0, 8.0, 5.0, 3.0],
-              'y': [5.0, 3.0, 7.0, 2.0, 9.0, 7.0, 8.0, 4.0, 5.0, 9.0, 2.0]}],
-    'layout': {'autosize': False,
-               'hovermode': 'closest',
-               'height': 480,
-               'margin': {'b': 47, 'l': 80, 'pad': 0, 'r': 63, 't': 47},
-               'showlegend': False,
-               'width': 640,
-               'xaxis1': {'anchor': 'y1',
-                         'domain': [0.0, 1.0],
-                         'range': (0.0, 9.0),
-                         'showgrid': False,
-                         'zeroline': False,
-                         'showline': True,
-                         'mirror': True,
-                         'ticks': 'inside'},
-               'yaxis1': {'anchor': 'x1',
-                         'domain': [0.0, 1.0],
-                         'range': (1.0, 10.0),
-                         'showgrid': False,
-                         'zeroline': False,
-                         'showline': True,
-                         'mirror': True,
-                         'ticks': 'inside'}}}
+SIMPLE_SCATTER = Figure(
+    data=Data([
+        Scatter(
+            x=[1.0, 2.0, 2.0, 4.0, 5.0, 6.0, 1.0, 7.0, 8.0, 5.0, 3.0],
+            y=[5.0, 3.0, 7.0, 2.0, 9.0, 7.0, 8.0, 4.0, 5.0, 9.0, 2.0],
+            mode='markers',
+            marker=Marker(
+                symbol='dot',
+                line=Line(
+                    color='rgb(0,0,0)',
+                    width=1.0
+                ),
+                size=4.4721359549995796,
+                color='rgb(0,0,255)',
+                opacity=1.0
+            ),
+            xaxis='x1',
+            yaxis='y1'
+        )
+    ]),
+    layout=Layout(
+        width=640,
+        height=480,
+        autosize=False,
+        margin=Margin(
+            l=80,
+            r=63,
+            b=47,
+            t=47,
+            pad=0
+        ),
+        hovermode='closest',
+        showlegend=False,
+        xaxis1=XAxis(
+            domain=[0.0, 1.0],
+            range=[0.0, 9.0],
+            type='linear',
+            showline=True,
+            ticks='inside',
+            nticks=10,
+            showgrid=False,
+            zeroline=False,
+            tickfont=Font(
+                size=12.0
+            ),
+            anchor='y1',
+            side='bottom',
+            mirror='ticks'
+        ),
+        yaxis1=YAxis(
+            domain=[0.0, 1.0],
+            range=[1.0, 10.0],
+            type='linear',
+            showline=True,
+            ticks='inside',
+            nticks=10,
+            showgrid=False,
+            zeroline=False,
+            tickfont=Font(
+                size=12.0
+            ),
+            anchor='x1',
+            side='left',
+            mirror='ticks'
+        )
+    )
+)
 
-DOUBLE_SCATTER = {
-    'data': [{'marker': {'color': 'rgb(255,0,0)',
-                         'line': {'color': 'rgb(255,0,0)', 'width': 1.0},
-                         'opacity': 0.5,
-                         'size': 11.0,
-                         'symbol': 'triangle-up'},
-              'type': 'scatter',
-              'mode': 'markers',
-              'xaxis': 'x1',
-              'yaxis': 'y1',
-              'x': [1.0, 2.0, 2.0, 4.0, 5.0, 6.0, 1.0, 7.0, 8.0, 5.0, 3.0],
-              'y': [5.0, 3.0, 7.0, 2.0, 9.0, 7.0, 8.0, 4.0, 5.0, 9.0, 2.0]},
-             {'marker': {'color': 'rgb(128,0,128)',
-                         'line': {'color': 'rgb(128,0,128)', 'width': 1.0},
-                         'opacity': 0.5,
-                         'size': 8.0,
-                         'symbol': 'square'},
-              'type': 'scatter',
-              'mode': 'markers',
-              'xaxis': 'x1',
-              'yaxis': 'y1',
-              'x': [-1.0,
-                    1.0,
-                    -0.29999999999999999,
-                    -0.59999999999999998,
-                    0.40000000000000002,
-                    0.80000000000000004,
-                    -0.10000000000000001,
-                    0.69999999999999996],
-                    'y': [-0.5,
-                    0.40000000000000002,
-                    0.69999999999999996,
-                    -0.59999999999999998,
-                    0.29999999999999999,
-                    -1.0,
-                    0.0,
-                    0.29999999999999999]}],
-    'layout': {'autosize': False,
-               'hovermode': 'closest',
-               'height': 480,
-               'margin': {'b': 47, 'l': 80, 'pad': 0, 'r': 63, 't': 47},
-               'showlegend': False,
-               'width': 640,
-               'xaxis1': {'anchor': 'y1',
-                         'domain': [0.0, 1.0],
-                         'range': (-2.0, 10.0),
-                         'showgrid': False,
-                         'zeroline': False,
-                         'showline': True,
-                         'mirror': True,
-                         'ticks': 'inside'},
-               'yaxis1': {'anchor': 'x1',
-                         'domain': [0.0, 1.0],
-                         'range': (-2.0, 10.0),
-                         'showgrid': False,
-                         'zeroline': False,
-                         'showline': True,
-                         'mirror': True,
-                         'ticks': 'inside'}}}
+DOUBLE_SCATTER = Figure(
+    data=Data([
+        Scatter(
+            x=[1.0, 2.0, 2.0, 4.0, 5.0, 6.0, 1.0, 7.0, 8.0, 5.0, 3.0],
+            y=[5.0, 3.0, 7.0, 2.0, 9.0, 7.0, 8.0, 4.0, 5.0, 9.0, 2.0],
+            mode='markers',
+            marker=Marker(
+                symbol='triangle-up',
+                line=Line(
+                    color='rgb(255,0,0)',
+                    width=1.0
+                ),
+                size=11.0,
+                color='rgb(255,0,0)',
+                opacity=0.5
+            ),
+            xaxis='x1',
+            yaxis='y1'
+        ),
+        Scatter(
+            x=[-1.0, 1.0, -0.29999999999999999, -0.59999999999999998, 0.40000000000000002, 0.80000000000000004, -0.10000000000000001, 0.69999999999999996],
+            y=[-0.5, 0.40000000000000002, 0.69999999999999996, -0.59999999999999998, 0.29999999999999999, -1.0, 0.0, 0.29999999999999999],
+            mode='markers',
+            marker=Marker(
+                symbol='square',
+                line=Line(
+                    color='rgb(128,0,128)',
+                    width=1.0
+                ),
+                size=8.0,
+                color='rgb(128,0,128)',
+                opacity=0.5
+            ),
+            xaxis='x1',
+            yaxis='y1'
+        )
+    ]),
+    layout=Layout(
+        width=640,
+        height=480,
+        autosize=False,
+        margin=Margin(
+            l=80,
+            r=63,
+            b=47,
+            t=47,
+            pad=0
+        ),
+        hovermode='closest',
+        showlegend=False,
+        xaxis1=XAxis(
+            domain=[0.0, 1.0],
+            range=[-2.0, 10.0],
+            type='linear',
+            showline=True,
+            ticks='inside',
+            nticks=7,
+            showgrid=False,
+            zeroline=False,
+            tickfont=Font(
+                size=12.0
+            ),
+            anchor='y1',
+            side='bottom',
+            mirror='ticks'
+        ),
+        yaxis1=YAxis(
+            domain=[0.0, 1.0],
+            range=[-2.0, 10.0],
+            type='linear',
+            showline=True,
+            ticks='inside',
+            nticks=7,
+            showgrid=False,
+            zeroline=False,
+            tickfont=Font(
+                size=12.0
+            ),
+            anchor='x1',
+            side='left',
+            mirror='ticks'
+        )
+    )
+)
