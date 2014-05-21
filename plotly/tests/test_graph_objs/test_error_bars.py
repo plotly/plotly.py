4fb55fb3e7076775b2c4eb8610777df984bf559f
theengineear <andseier@gmail.com>
2014-05-21 16:53:54 -0700

    Updated graph_references, which now includes asymmetric error bars.
    Added tests for instantiation of these objects.

--- plotly/tests/test_graph_objs/test_error_bars.py
+++ plotly/tests/test_graph_objs/test_error_bars.py
@@ -10,14 +10,38 @@ from ...graph_objs.graph_objs import *
 from ...exceptions import (PlotlyDictKeyError, PlotlyDictValueError,
                            PlotlyDataTypeError, PlotlyListEntryError)
 
+
 def test_instantiate_error_x():
     ErrorX()
-    ErrorX(value=0.1, type='percent', color='red')
+    ErrorX(array=[1, 2, 3],
+           arrayminus=[2, 1, 2],
+           color='red',
+           copy_ystyle=False,
+           opacity=.4,
+           symmetric=False,
+           thickness=2,
+           traceref=0,  # TODO, what's this do again?
+           type='percent',
+           value=1,
+           valueminus=4,
+           visible=True,
+           width=5)
 
 
 def test_instantiate_error_y():
     ErrorY()
-    ErrorY(value=0.1, type='percent', color='red')
+    ErrorY(array=[1, 2, 3],
+           arrayminus=[2, 1, 2],
+           color='red',
+           opacity=.4,
+           symmetric=False,
+           thickness=2,
+           traceref=0,  # TODO, what's this do again?
+           type='percent',
+           value=1,
+           valueminus=4,
+           visible=True,
+           width=5)
 
 
 @raises(PlotlyDictKeyError)
