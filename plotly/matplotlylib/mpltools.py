671d8375e42062e0073a148ae2a17243a541ccd7
theengineear <andseier@gmail.com>
2014-06-17 21:10:51 -0700

    Added length check to finding gaps. Otherwise error with length 1!

--- plotly/matplotlylib/mpltools.py
+++ plotly/matplotlylib/mpltools.py
@@ -195,13 +195,14 @@ def get_axes_bounds(fig):
 
 
 def get_bar_gap(bar_starts, bar_ends, tol=1e-10):
-    sides1 = bar_starts[1:]
-    sides2 = bar_ends[:-1]
-    gaps = [s2-s1 for s2, s1 in zip(sides1, sides2)]
-    gap0 = gaps[0]
-    uniform = all([abs(gap0-gap) < tol for gap in gaps])
-    if uniform:
-        return gap0
+    if len(bar_starts) == len(bar_ends) and len(bar_starts) > 1:
+        sides1 = bar_starts[1:]
+        sides2 = bar_ends[:-1]
+        gaps = [s2-s1 for s2, s1 in zip(sides1, sides2)]
+        gap0 = gaps[0]
+        uniform = all([abs(gap0-gap) < tol for gap in gaps])
+        if uniform:
+            return gap0
 
 
 def get_rect_xmin(data):
