6dfa3100484d95c69f95037abdd67ab1931ee801
Chris P <christopher.parmer@gmail.com>
2014-07-07 10:45:57 -0400

    rm unused imports

--- plotly/plotly/plotly.py
+++ plotly/plotly/plotly.py
@@ -18,9 +18,7 @@ import requests
 import chunked_requests
 import json
 import warnings
-import httplib
 import copy
-import base64
 import os
 from .. import utils  # TODO make non-relative
 from .. import tools
