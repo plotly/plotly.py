33eb5d8006a62d3a4deac48d4fbbabdcf78582f2
Andrew Seier <andseier@gmail.com>
2014-07-07 12:38:36 -0700

    Added docs to `plotly.__init__.py`, revamped others.

--- plotly/graph_objs/__init__.py
+++ plotly/graph_objs/__init__.py
@@ -3,44 +3,8 @@ graph_objs
 ==========
 
 This package imports definitions for all of Plotly's graph objects. For more
-information, run help(Obj) on any of the following objects defined here...
+information, run help(Obj) on any of the following objects defined here.
 
-Defined Objects:
-
-    Data Objects (things that make up your 'Data' list:
-        Bar
-        Box
-        Contour
-        Heatmap
-        Histogram
-        Histogram2d
-        Histogram2dContour
-        Scatter
-
-    Other Objects (everything else):
-
-        Data
-        Annotations
-        Annotation
-        AngularAxis
-        ColorBar
-        Contours
-        ErrorX
-        ErrorY
-        Figure
-        Font
-        Layout
-        Legend
-        Line
-        Margin
-        Marker
-        RadialAxis
-        Stream
-        Trace
-        XAxis
-        XBins
-        YAxis
-        YBins
 
 """
 from graph_objs import *
--- plotly/plotly/__init__.py
+++ plotly/plotly/__init__.py
@@ -3,23 +3,8 @@ plotly
 ======
 
 This module defines functionality that requires interaction between your
-local machine and Plotly. Almost all functionality used here will required a
-verifiable account and an internet connection.
-
-Classes:
-    image
-    Stream
-
-Functions:
-    sign_in
-    iplot
-    plot
-    iplot_mpl
-    plot_mpl
-    get_figure
-    update_plot_options
-    get_plot_options
-    get_credentials
+local machine and Plotly. Almost all functionality used here will require a
+verifiable account (username/api-key pair) and a network connection.
 
 """
 from plotly import *
