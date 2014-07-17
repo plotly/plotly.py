14f603bf490aa1c155d6855a561049eec9739f86
Andrew Seier <andseier@gmail.com>
2014-07-16 19:32:09 -0700

    added the graph_objs_tools.py that was missing before.

---
+++ plotly/graph_objs/graph_objs_tools.py
@@ -0,0 +1,14 @@
+def update_keys(keys):
+    """Change keys we used to support to their new equivalent."""
+    updated_keys = list()
+    for key in keys:
+        if key in translations:
+            updated_keys += [translations[key]]
+        else:
+            updated_keys += [key]
+    return updated_keys
+
+translations = dict(
+    scl="colorscale",
+    reversescale="reversescale"
+)
