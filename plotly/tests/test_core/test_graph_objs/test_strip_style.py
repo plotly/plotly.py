b8ac4394a07f74b297cbc2be9142e0ee9ba16f1e
Andrew Seier <andseier@gmail.com>
2014-07-18 19:57:32 -0700

    Added graph_objs test to increase coverage.

---
+++ plotly/tests/test_core/test_graph_objs/test_strip_style.py
@@ -0,0 +1,143 @@
+from __future__ import absolute_import
+
+from plotly.graph_objs import *
+
+
+def test_strip_style():
+    fig = Figure(
+        data=Data([
+            Scatter(
+                x=[52698, 43117],
+                y=[53, 31],
+                mode=u'markers',
+                name=u'North America',
+                text=[u'United States', u'Canada'],
+                marker=Marker(
+                    color=u'rgb(164, 194, 244)',
+                    size=12,
+                    line=Line(
+                        color=u'white',
+                        width=0.5
+                    )
+                )
+            ),
+            Scatter(
+                x=[39317, 37236, 35650, 30066, 29570, 27159, 23557, 21046, 18007],
+                y=[33, 20, 13, 19, 27, 19, 49, 44, 38],
+                mode=u'markers',
+                name=u'Europe',
+                text=[u'Germany', u'Britain', u'France', u'Spain', u'Italy', u'Czech Rep.', u'Greece', u'Poland'],
+                marker=Marker(
+                    color=u'rgb(255, 217, 102)',
+                    size=12,
+                    line=Line(
+                        color=u'white',
+                        width=0.5
+                    )
+                )
+            ),
+            Scatter(
+                x=[42952, 37037, 33106, 17478, 9813, 5253, 4692, 3899],
+                y=[23, 42, 54, 89, 14, 99, 93, 70],
+                mode=u'markers',
+                name=u'Asia/Pacific',
+                text=[u'Australia', u'Japan', u'South Korea', u'Malaysia', u'China', u'Indonesia', u'Philippines', u'India'],
+                marker=Marker(
+                    color=u'rgb(234, 153, 153)',
+                    size=12,
+                    line=Line(
+                        color=u'white',
+                        width=0.5
+                    )
+                )
+            ),
+            Scatter(
+                x=[19097, 18601, 15595, 13546, 12026, 7434, 5419],
+                y=[43, 47, 56, 80, 86, 93, 80],
+                mode=u'markers',
+                name=u'Latin America',
+                text=[u'Chile', u'Argentina', u'Mexico', u'Venezuela', u'Venezuela', u'El Salvador', u'Bolivia'],
+                marker=Marker(
+                    color=u'rgb(142, 124, 195)',
+                    size=12,
+                    line=Line(
+                        color=u'white',
+                        width=0.5
+                    )
+                )
+            )
+        ]),
+        layout=Layout(
+            title=u'Quarter 1 Growth',
+            autosize=False,
+            width=500,
+            height=500,
+            xaxis=XAxis(
+                title=u'GDP per Capita',
+                showgrid=False,
+                zeroline=False
+            ),
+            yaxis=YAxis(
+                title=u'Percent',
+                showline=False
+            ),
+            margin=Margin(
+                l=65,
+                r=50,
+                b=65,
+                t=90
+            )
+        )
+    )
+    fig.strip_style()
+    comp_fig = Figure(
+        data=Data([
+            Scatter(
+                x=[52698, 43117],
+                y=[53, 31],
+                name=u'North America',
+                text=[u'United States', u'Canada'],
+                marker=Marker(
+                    line=Line()
+                )
+            ),
+            Scatter(
+                x=[39317, 37236, 35650, 30066, 29570, 27159, 23557, 21046, 18007],
+                y=[33, 20, 13, 19, 27, 19, 49, 44, 38],
+                name=u'Europe',
+                text=[u'Germany', u'Britain', u'France', u'Spain', u'Italy', u'Czech Rep.', u'Greece', u'Poland'],
+                marker=Marker(
+                    line=Line()
+                )
+            ),
+            Scatter(
+                x=[42952, 37037, 33106, 17478, 9813, 5253, 4692, 3899],
+                y=[23, 42, 54, 89, 14, 99, 93, 70],
+                name=u'Asia/Pacific',
+                text=[u'Australia', u'Japan', u'South Korea', u'Malaysia', u'China', u'Indonesia', u'Philippines', u'India'],
+                marker=Marker(
+                    line=Line()
+                )
+            ),
+            Scatter(
+                x=[19097, 18601, 15595, 13546, 12026, 7434, 5419],
+                y=[43, 47, 56, 80, 86, 93, 80],
+                name=u'Latin America',
+                text=[u'Chile', u'Argentina', u'Mexico', u'Venezuela', u'Venezuela', u'El Salvador', u'Bolivia'],
+                marker=Marker(
+                    line=Line()
+                )
+            )
+        ]),
+        layout=Layout(
+            title=u'Quarter 1 Growth',
+            xaxis=XAxis(
+                title=u'GDP per Capita'
+            ),
+            yaxis=YAxis(
+                title=u'Percent'
+            ),
+            margin=Margin()
+        )
+    )
+    assert fig == comp_fig
---
+++ plotly/tests/test_core/test_graph_objs/test_to_string.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import
+
+from plotly.graph_objs import *
+
+
+def test_to_string():
+    fig = Figure(
+        data=Data([
+            Scatter(
+                x=[1, 2, 3, 4],
+                y=[10, 15, 13, 17]
+            ),
+            Scatter(
+                x=[1, 2, 3, 4],
+                y=[16, 5, 11, 9]
+            )
+        ]),
+        layout=Layout(
+            autosize=False,
+            width=500,
+            height=500,
+            margin=Margin(
+                l=65,
+                r=50,
+                b=65,
+                t=65
+            )
+        )
+    )
+    fig_string = fig.to_string(pretty=False)
+    comp_string = 'Figure(\n    data=Data([\n        Scatter(\n            x=[1, 2, 3, 4],\n            y=[10, 15, 13, 17]\n        ),\n        Scatter(\n            x=[1, 2, 3, 4],\n            y=[16, 5, 11, 9]\n        )\n    ]),\n    layout=Layout(\n        autosize=False,\n        width=500,\n        height=500,\n        margin=Margin(\n            l=65,\n            r=50,\n            b=65,\n            t=65\n        )\n    )\n)'
+    assert fig_string == comp_string
