74f4f6383074427e4511871bdf8982843ade9593
Andrew Seier <andseier@gmail.com>
2014-07-29 21:53:12 -0700

    Added formatting changes for to_string() method of graph objects.
    before:
    
                text=[u'4179 Toutatis<br>Radius: 2188 meters <br> Velocity: 11....],
    
    after:
    
                text=[u'4179 Toutatis<br>Radius: 2188 meters <br> Velocity: 1...'],
    
    additionally:
    
    * the end of line (eol) is considered in max len
    * the conditional comma is considered in max len
    * lines >= to max line length are guaranteed to equal max line length!
    * to do the above, spaces, ' ', are filled as necessary.

--- plotly/graph_objs/graph_objs_tools.py
+++ plotly/graph_objs/graph_objs_tools.py
@@ -1,3 +1,5 @@
+import six
+
 def update_keys(keys):
     """Change keys we used to support to their new equivalent."""
     updated_keys = list()
@@ -12,3 +14,30 @@ translations = dict(
     scl="colorscale",
     reversescl="reversescale"
 )
+
+
+def curtail_val_repr(val, max_chars, add_delim=False):
+    delim = ", "
+    end = "..."
+    if isinstance(val, six.string_types):
+        if max_chars <= len("'" + end + "'"):
+            return ' ' * max_chars
+        elif add_delim and max_chars <= len("'" + end + "'") + len(delim):
+            return "'" + end + "'" + ' ' * (max_chars - len("'" + end + "'"))
+    else:
+        if max_chars <= len(end):
+            return ' ' * max_chars
+        elif add_delim and max_chars <= len(end) + len(delim):
+            return end + ' ' * (max_chars - len(end))
+    if add_delim:
+        max_chars -= len(delim)
+    r = repr(val)
+    if len(r) > max_chars:
+        if isinstance(val, six.string_types):
+            # TODO: can we assume this ends in "'"
+            r = r[:max_chars - len(end + "'")] + end + "'"
+        else:
+            r = r[:max_chars - len(end)] + end
+    if add_delim:
+        r += delim
+    return r
