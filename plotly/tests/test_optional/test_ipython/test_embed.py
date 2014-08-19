d6dd58851d7549aaa7ed920fc544d1688198dd8c
Andrew Seier <andseier@gmail.com>
2014-08-18 23:45:33 -0700

    trying to use more standard testing formats.

--- plotly/tests/test_optional/test_ipython/test_embed.py
+++ plotly/tests/test_optional/test_ipython/test_embed.py
@@ -3,51 +3,52 @@ from __future__ import absolute_import
 import plotly.tools as tls
 import imghdr
 import threading
+import unittest
 
 
-def test_plotly_display():
-    plot_info = {"un": "plotlyimagetest", "fid": "2"}
-    url = "https://plot.ly/~{un}/{fid}".format(**plot_info)
-    disp_obj = tls.embed(url)
-    format_to_func = {
-        "jpeg": jpeg_worker,
-        "png": png_worker,
-        "svg": svg_worker,
-        "pdf": pdf_worker
-    }
-    results = {}
-    threads = []
-    for f_format, func in format_to_func.items():
-        threads += [threading.Thread(target=func, args=(disp_obj, results))]
-        threads[-1].setDaemon(True)
-        threads[-1].start()
-    for thread in threads:
-        thread.join()
-    for f_format in format_to_func:
-        result = results.get(f_format, False)
-        print("{f_format}: {result}".format(f_format=f_format, result=result))
-        assert results.get(f_format)
+class TestPlotlyDisplay(unittest.TestCase):
 
+    def setUp(self):
+        plot_info = {"un": "plotlyimagetest", "fid": "2"}
+        url = "https://plot.ly/~{un}/{fid}".format(**plot_info)
+        self.display_obj = tls.embed(url)
+        self.results = {}
+        self.threads = []
+        self.format_to_func = {
+            "jpeg": self.jpeg_worker,
+            "png": self.png_worker,
+            "svg": self.svg_worker,
+            "pdf": self.pdf_worker}
 
-def jpeg_worker(display_obj, results):
-    img = display_obj._repr_jpeg_()
-    if imghdr.what('', img) == "jpeg":
-        results["jpeg"] = True
+    def test_plotly_display(self):
+        for f_format, func in self.format_to_func.items():
+            self.threads += [threading.Thread(target=func)]
+            self.threads[-1].setDaemon(True)
+            self.threads[-1].start()
+        for thread in self.threads:
+            thread.join()
+        for f_format in self.format_to_func:
+            result = self.results.get(f_format, False)
+            print("{f_format}: {result}".format(f_format=f_format,
+                                                result=result))
+            assert self.results.get(f_format)
 
+    def jpeg_worker(self):
+        img = self.display_obj._repr_jpeg_()
+        if imghdr.what('', img) == "jpeg":
+            self.results["jpeg"] = True
 
-def png_worker(display_obj, results):
-    img = display_obj._repr_png_()
-    if imghdr.what('', img) == "png":
-        results["png"] = True
+    def png_worker(self):
+        img = self.display_obj._repr_png_()
+        if imghdr.what('', img) == "png":
+            self.results["png"] = True
 
+    def svg_worker(self):
+        img = self.display_obj._repr_svg_()
+        if img[:4] == '<svg':
+            self.results["svg"] = True
 
-def svg_worker(display_obj, results):
-    img = display_obj._repr_svg_()
-    if img[:4] == '<svg':
-        results["svg"] = True
-
-
-def pdf_worker(display_obj, results):
-    img = display_obj._repr_pdf_()
-    if img[:4] == '%PDF':
-        results["pdf"] = True
+    def pdf_worker(self):
+        img = self.display_obj._repr_pdf_()
+        if img[:4] == '%PDF':
+            self.results["pdf"] = True
