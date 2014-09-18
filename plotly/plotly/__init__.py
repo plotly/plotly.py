7a0da88638c3d0fe0f9088c0d9008ff60503fe06
Andrew Seier <andseier@gmail.com>
2014-09-18 00:39:42 -0700

    Remove `import *`. Replace with explicit public interface.

--- plotly/plotly/__init__.py
+++ plotly/plotly/__init__.py
@@ -7,10 +7,7 @@ local machine and Plotly. Almost all functionality used here will require a
 verifiable account (username/api-key pair) and a network connection.
 
 """
-from __future__ import absolute_import
-
-from plotly.plotly.plotly import *
-
-__all__ = ["sign_in", "update_plot_options", "get_plot_options",
-           "get_credentials", "iplot", "plot", "iplot_mpl", "plot_mpl",
-           "get_figure", "Stream", "image"]
+from . plotly import (
+    sign_in, update_plot_options, get_plot_options, get_credentials, iplot,
+    plot, iplot_mpl, plot_mpl, get_figure, Stream, image
+)
