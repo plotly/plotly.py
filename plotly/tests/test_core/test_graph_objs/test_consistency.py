67cc0218e1106c531bc118633acc64be037e952e
etpinard <etienne.t.pinard@gmail.com>
2014-09-19 13:07:15 -0400

    fix update tests to new INFO

--- plotly/tests/test_core/test_graph_objs/test_consistency.py
+++ plotly/tests/test_core/test_graph_objs/test_consistency.py
@@ -10,17 +10,17 @@ from plotly.graph_objs import graph_objs
 
 
 def test_info_keys_in_key_to_name():
-    for key in graph_objs.INFO:
+    for key in graph_objs.INFO.keys():
         class_name = graph_objs.KEY_TO_NAME[key]
 
 
 def test_names_in_name_to_key():
-    for key in graph_objs.INFO:
+    for key in graph_objs.INFO.keys():
         class_name = graph_objs.KEY_TO_NAME[key]
         key_name = graph_objs.NAME_TO_KEY[class_name]
 
 
 def test_names_in_name_to_class():
-    for key in graph_objs.INFO:
+    for key in graph_objs.INFO.keys():
         class_name = graph_objs.KEY_TO_NAME[key]
         _class = graph_objs._factory(class_name)
