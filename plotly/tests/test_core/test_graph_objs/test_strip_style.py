ea2e7addccecfcf8f25d87538b3b0de753d19976
etpinard <etienne.t.pinard@gmail.com>
2014-10-14 14:44:27 -0400

    update test_strip_style to new graph ref 'val_types'

--- plotly/tests/test_core/test_graph_objs/test_strip_style.py
+++ plotly/tests/test_core/test_graph_objs/test_strip_style.py
@@ -100,6 +100,7 @@ def test_strip_style():
             Scatter(
                 x=[52698, 43117],
                 y=[53, 31],
+                mode='markers',
                 name='North America',
                 text=['United States', 'Canada'],
                 marker=Marker(
@@ -109,6 +110,7 @@ def test_strip_style():
             Scatter(
                 x=[39317, 37236, 35650, 30066, 29570, 27159, 23557, 21046, 18007],
                 y=[33, 20, 13, 19, 27, 19, 49, 44, 38],
+                mode='markers',
                 name='Europe',
                 text=['Germany', 'Britain', 'France', 'Spain', 'Italy', 'Czech Rep.', 'Greece', 'Poland'],
                 marker=Marker(
@@ -118,6 +120,7 @@ def test_strip_style():
             Scatter(
                 x=[42952, 37037, 33106, 17478, 9813, 5253, 4692, 3899],
                 y=[23, 42, 54, 89, 14, 99, 93, 70],
+                mode='markers',
                 name='Asia/Pacific',
                 text=['Australia', 'Japan', 'South Korea', 'Malaysia', 'China', 'Indonesia', 'Philippines', 'India'],
                 marker=Marker(
@@ -127,6 +130,7 @@ def test_strip_style():
             Scatter(
                 x=[19097, 18601, 15595, 13546, 12026, 7434, 5419],
                 y=[43, 47, 56, 80, 86, 93, 80],
+                mode='markers',
                 name='Latin America',
                 text=['Chile', 'Argentina', 'Mexico', 'Venezuela', 'Venezuela', 'El Salvador', 'Bolivia'],
                 marker=Marker(
@@ -136,13 +140,20 @@ def test_strip_style():
         ]),
         layout=Layout(
             title='Quarter 1 Growth',
+            width=500,
+            height=500,
             xaxis=XAxis(
                 title='GDP per Capita'
             ),
             yaxis=YAxis(
                 title='Percent'
             ),
-            margin=Margin()
+            margin=Margin(
+                l=65,
+                r=50,
+                b=65,
+                t=90
+            )
         )
     )
     equivalent, msg = compare_dict(fig, comp_fig)
