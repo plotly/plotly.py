decc4232f3ec27487839e559a97dec5ec5d0bf0e
Chris P <christopher.parmer@gmail.com>
2014-10-20 22:43:15 -0400

    duplicate aware, column retrieving Grid object (list)

--- plotly/grid_objs/grid_objs.py
+++ plotly/grid_objs/grid_objs.py
@@ -5,6 +5,7 @@ grid_objs
 """
 
 import json
+from collections import MutableSequence
 
 
 class Column():
@@ -24,12 +25,42 @@ class Column():
             ellipses='...]' if len(jdata) > 10 else '')
 
 
-class Grid(list):
-    def __init__(self, columns):
-        # TODO: column type checking
-        column_names = [column.name for column in columns]
+class Grid(MutableSequence):
+    def __init__(self, iterable_of_columns):
+        column_names = [column.name for column in iterable_of_columns]
         if (len(column_names) > len(set(column_names))):
             # TODO: Descriptive exception
-            raise Exception('Duplicate column names')
+            raise Exception('duplicate!')
+        self._columns = list(iterable_of_columns)
 
-        return super(Grid, self).__init__(columns)
+    def __repr__(self):
+        return self._columns.__repr__()
+
+    def __getitem__(self, index):
+        return self._columns[index]
+
+    def __setitem__(self, index, column):
+        self._validate_insertion(column)
+        return self._columns.__setitem__(index, column)
+
+    def __delitem__(self, index):
+        del self._columns[index]
+
+    def __len__(self):
+        return len(self._columns)
+
+    def insert(self, index, column):
+        self._validate_insertion(column)
+        self._columns.insert(index, column)
+
+    def _validate_insertion(self, column):
+        """
+        Raise an error if we're gonna add a duplicate column name
+        """
+        existing_column_names = [col.name for col in self._columns]
+        if column.name in existing_column_names:
+            # TODO: Descriptive exception
+            raise Exception('duplicate')
+
+    def get_column(self, column_name):
+        return next(column for column in self._columns if column.name==column_name)
