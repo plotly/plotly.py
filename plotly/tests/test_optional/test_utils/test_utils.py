e477f07256ea03fef0f3eef29f66a3d26a5bc7ee
Chris P <christopher.parmer@gmail.com>
2014-11-14 23:09:39 -0500

    add pandas to encoding tests

--- plotly/tests/test_optional/test_utils/test_utils.py
+++ plotly/tests/test_optional/test_utils/test_utils.py
@@ -5,15 +5,17 @@ from nose.plugins.attrib import attr
 from datetime import datetime as dt
 import numpy as np
 import json
+import pandas as pd
 
 from plotly import utils
 from plotly.grid_objs import Column
-from plotly.graph_objs import Scatter3d, Figure, Data
+from plotly.graph_objs import Scatter, Scatter3d, Figure, Data
 
 ## JSON encoding
 numeric_list = [1, 2, 3]
 np_list = np.array([1, 2, 3])
 mixed_list = [1, 'A', dt(2014, 1, 5)]
+pd = pd.DataFrame(columns=['col 1'], data=[1, 2, 3])
 
 
 def test_column_json_encoding():
@@ -29,10 +31,15 @@ def test_column_json_encoding():
 
 
 def test_figure_json_encoding():
-    s = Scatter3d(x=numeric_list, y=np_list, z=mixed_list)
-    data = Data([s])
+    s1 = Scatter3d(x=numeric_list, y=np_list, z=mixed_list)
+    s2 = Scatter(x=pd['col 1'])
+    data = Data([s1, s2])
     figure = Figure(data=data)
 
-    json.dumps(s, cls=utils._plotlyJSONEncoder)
+    js1 = json.dumps(s1, cls=utils._plotlyJSONEncoder)
+    js2 = json.dumps(s2, cls=utils._plotlyJSONEncoder)
+    assert(js1 == '{"y": [1, 2, 3], "x": [1, 2, 3], "z": '
+                  '[1, "A", "2014-01-05"], "type": "scatter3d"}')
+    assert(js2 == '{"x": [1, 2, 3], "type": "scatter"}')
     json.dumps(data, cls=utils._plotlyJSONEncoder)
     json.dumps(figure, cls=utils._plotlyJSONEncoder)
