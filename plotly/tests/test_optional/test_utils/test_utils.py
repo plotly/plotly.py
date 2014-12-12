8b0113ef6bd91235847f736bf97f7c3636c499ee
Chris P <christopher.parmer@gmail.com>
2014-12-12 12:51:48 -0800

    Test that json encoding doesn't mutate

--- plotly/tests/test_optional/test_utils/test_utils.py
+++ plotly/tests/test_optional/test_utils/test_utils.py
@@ -6,6 +6,7 @@ from datetime import datetime as dt
 import numpy as np
 import json
 import pandas as pd
+from pandas.util.testing import assert_series_equal
 
 from plotly import utils
 from plotly.grid_objs import Column
@@ -57,24 +58,43 @@ def test_figure_json_encoding():
                   '"z": [1, "A", "2014-01-05", '
                   '"2014-01-05 01:01:01", "2014-01-05 01:01:01.000001"]}')
     assert(js2 == '{"type": "scatter", "x": [1, 2, 3]}')
+
+    # Test JSON encoding works
     json.dumps(data, cls=utils._plotlyJSONEncoder, sort_keys=True)
     json.dumps(figure, cls=utils._plotlyJSONEncoder, sort_keys=True)
+
+    # Test data wasn't mutated
+    assert(bool(np.asarray(np_list ==
+                np.array([1, 2, 3, np.NaN,
+                          np.NAN, np.Inf, dt(2014, 1, 5)])).all()))
+    assert(set(data[0]['z']) ==
+           set([1, 'A', dt(2014, 1, 5), dt(2014, 1, 5, 1, 1, 1),
+                dt(2014, 1, 5, 1, 1, 1, 1)]))
 
 
 def test_pandas_json_encoding():
     j1 = json.dumps(df['col 1'], cls=utils._plotlyJSONEncoder)
     assert(j1 == '[1, 2, 3, "2014-01-05", null, NaN, Infinity]')
+
+    # Test that data wasn't mutated
+    assert_series_equal(df['col 1'],
+                        pd.Series([1, 2, 3, dt(2014, 1, 5),
+                                   pd.NaT, np.NaN, np.Inf]))
+
     j2 = json.dumps(df.index, cls=utils._plotlyJSONEncoder)
     assert(j2 == '[0, 1, 2, 3, 4, 5, 6]')
 
-    j3 = json.dumps([pd.NaT], cls=utils._plotlyJSONEncoder)
+    nat = [pd.NaT]
+    j3 = json.dumps(nat, cls=utils._plotlyJSONEncoder)
     assert(j3 == '[null]')
+    assert(nat[0] is pd.NaT)
 
     j4 = json.dumps(rng, cls=utils._plotlyJSONEncoder)
     assert(j4 == '["2011-01-01", "2011-01-01 01:00:00"]')
 
     j5 = json.dumps(ts, cls=utils._plotlyJSONEncoder)
     assert(j5 == '[1.5, 2.5]')
+    assert_series_equal(ts, pd.Series([1.5, 2.5], index=rng))
 
     j6 = json.dumps(ts.index, cls=utils._plotlyJSONEncoder)
     assert(j6 == '["2011-01-01", "2011-01-01 01:00:00"]')
