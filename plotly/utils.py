9bd3755c05b7441bbab5732c5f4810d7b58d1500
Andrew Seier <andseier@gmail.com>
2014-12-15 09:32:05 -0800

    Move `default` function up. It should be seen 1st.

--- plotly/utils.py
+++ plotly/utils.py
@@ -112,6 +112,55 @@ class PlotlyJSONEncoder(json.JSONEncoder):
     See PlotlyJSONEncoder.default for more implementation information.
 
     """
+
+    def default(self, obj):
+        """
+        Accept an object (of unknown type) and try to encode with priority:
+        1. builtin:     user-defined objects
+        2. sage:        sage math cloud
+        3. pandas:      dataframes/series
+        4. numpy:       ndarrays
+        5. datetime:    time/datetime objects
+
+        Each method throws a NotEncoded exception if it fails.
+
+        The default method will only get hit if the object is not a type that
+        is naturally encoded by json:
+
+            Normal objects:
+                dict                object
+                list, tuple         array
+                str, unicode        string
+                int, long, float    number
+                True                true
+                False               false
+                None                null
+
+            Extended objects:
+                float('nan')        'NaN'
+                float('infinity')   'Infinity'
+                float('-infinity')  '-Infinity'
+
+        Therefore, we only anticipate either unknown iterables or values here.
+
+        """
+        # TODO: The ordering if these methods is *very* important. Is this OK?
+        encoding_methods = (
+            self.encode_as_plotly,
+            self.encode_as_sage,
+            self.encode_as_numpy,
+            self.encode_as_pandas,
+            self.encode_as_datetime,
+            self.encode_as_date,
+            self.encode_as_list  # because some values have `tolist` do last.
+        )
+        for encoding_method in encoding_methods:
+            try:
+                return encoding_method(obj)
+            except NotEncodable:
+                pass
+        return json.JSONEncoder.default(self, obj)
+
     @staticmethod
     def encode_as_plotly(obj):
         """Attempt to use a builtin `ploty_to_json` method."""
@@ -184,54 +233,6 @@ class PlotlyJSONEncoder(json.JSONEncoder):
             raise NotEncodable
         else:
             return iso_to_plotly_time_string(time_string)
-
-    def default(self, obj):
-        """
-        Accept an object (of unknown type) and try to encode with priority:
-        1. builtin:     user-defined objects
-        2. sage:        sage math cloud
-        3. pandas:      dataframes/series
-        4. numpy:       ndarrays
-        5. datetime:    time/datetime objects
-
-        Each method throws a NotEncoded exception if it fails.
-
-        The default method will only get hit if the object is not a type that
-        is naturally encoded by json:
-
-            Normal objects:
-                dict                object
-                list, tuple         array
-                str, unicode        string
-                int, long, float    number
-                True                true
-                False               false
-                None                null
-
-            Extended objects:
-                float('nan')        'NaN'
-                float('infinity')   'Infinity'
-                float('-infinity')  '-Infinity'
-
-        Therefore, we only anticipate either unknown iterables or values here.
-
-        """
-        # TODO: The ordering if these methods is *very* important. Is this OK?
-        encoding_methods = (
-            self.encode_as_plotly,
-            self.encode_as_sage,
-            self.encode_as_numpy,
-            self.encode_as_pandas,
-            self.encode_as_datetime,
-            self.encode_as_date,
-            self.encode_as_list  # because some values have `tolist` do last.
-        )
-        for encoding_method in encoding_methods:
-            try:
-                return encoding_method(obj)
-            except NotEncodable:
-                pass
-        return json.JSONEncoder.default(self, obj)
 
 
 ### unicode stuff ###
