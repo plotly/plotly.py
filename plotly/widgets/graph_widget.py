f03624603f39b828be38df93e06fdbfa957e740c
Andrew Seier <andseier@gmail.com>
2015-01-22 18:22:12 -0500

    no s… ugg.

--- plotly/widgets/graph_widget.py
+++ plotly/widgets/graph_widget.py
@@ -14,10 +14,8 @@ from pkg_resources import resource_string
 # Load JS widget code
 # No officially recommended way to do this in any other way
 # http://mail.scipy.org/pipermail/ipython-dev/2014-April/013835.html
-directory = os.path.dirname(os.path.realpath(__file__))
-js_widget_file = os.path.join(directory, 'graphWidget.js')
 js_widget_code = resource_string('plotly',
-                                 'widgets/graphWidgets.js').decode('utf-8')
+                                 'widgets/graphWidget.js').decode('utf-8')
 
 display(Javascript(js_widget_code))
 
