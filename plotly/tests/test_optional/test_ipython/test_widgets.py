37ac31a1a7add49d64b3de9fdc329af68f983094
Andrew Seier <andseier@gmail.com>
2015-03-13 07:08:15 -0700

    protect widgets import in test (python 2.6)

--- plotly/tests/test_optional/test_ipython/test_widgets.py
+++ plotly/tests/test_optional/test_ipython/test_widgets.py
@@ -1,9 +1,16 @@
+import six
 from unittest import TestCase
 
-from plotly.widgets import GraphWidget
+version = six.sys.version_info[:2]  # need this for conditional testing
+
+# unittest `skip` not supported in 2.6 and IPython not supported in 2.6/3.2
+if version < (2, 7) or (2, 7) < version < (3, 3):
+    pass
+else:
+    from plotly.widgets import GraphWidget
 
 
-class TestWidgets(TestCase):
+    class TestWidgets(TestCase):
 
-    def test_instantiate_graph_widget(self):
-        widget = GraphWidget
+        def test_instantiate_graph_widget(self):
+            widget = GraphWidget
