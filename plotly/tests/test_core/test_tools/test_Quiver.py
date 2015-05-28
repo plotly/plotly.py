bfbc83e7cc0c1c46c2bb6d1a3eb901d870ef742a
Chelsea <cldougl@gmail.com>
2015-05-28 16:13:57 -0400

    Quiver Test

--- plotly/tests/test_core/test_tools/test_Quiver.py
+++ plotly/tests/test_core/test_tools/test_Quiver.py
@@ -2,3 +2,38 @@ import plotly
 from plotly.graph_objs import *
 import plotly.tools as tls
 from nose.tools import raises
+import numpy as np
+
+
+@raises(Exception)
+def unequal_xy_length():
+    data = tls.Quiver(x=[1, 2], y=[1], u=[1, 2], v=[1, 2])
+
+
+@raises(Exception)
+def unequal_uv_length():
+    data = tls.Quiver(x=[1, 2], y=[1, 3], u=[1], v=[1, 2])
+
+
+@raises(Exception)
+def test_wrong_kwarg():
+    data = tls.Quiver(stuff='not gonna work')
+
+
+def test_one_arrow():
+    trace1 = Scatter(
+            x=np.array([0., 1., np.nan]),
+            y=np.array([0., 1., np.nan]),
+            mode='lines',
+            name='Barb',
+            line=Line(color='rgb(114, 132, 314)', width=1)
+            )
+    trace2 = Scatter(
+            x=np.array([0.82069826, 1, 0.61548617, np.nan]),
+            y=np.array([0.61548617,  1,  0.82069826, np.nan]),
+            mode='lines',
+            name='Arrow',
+            line=Line(color='rgb(114, 132, 314)', width=1)
+            )
+    expected = Data([trace1, trace2])
+    assert tls.Quiver(x=[0], y=[0], u=[1], v=[1], scale=1)) == expected
