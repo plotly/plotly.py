f12ac2d3b0dc44dcd0505ebf980ad99d3ca28522
Chelsea <cldougl@gmail.com>
2015-05-28 16:49:06 -0400

    streamline test

--- plotly/tests/test_core/test_tools/test_Streamline.py
+++ plotly/tests/test_core/test_tools/test_Streamline.py
@@ -2,3 +2,14 @@ import plotly
 from plotly.graph_objs import *
 import plotly.tools as tls
 from nose.tools import raises
+import numpy as np
+
+
+@raises(Exception)
+def test_wrong_kwarg():
+    fig = tls.make_subplots(stuff='not gonna work')
+
+
+def test_two_arrow():
+    expected = 2
+    assert 1 == expected
