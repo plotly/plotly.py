fb74755da80601cff931aebb806705cf9dca1320
Chris P <christopher.parmer@gmail.com>
2015-07-02 16:54:39 -0400

    rm tests that dont apply anymore

--- plotly/tests/test_optional/test_offline/test_offline.py
+++ plotly/tests/test_optional/test_offline/test_offline.py
@@ -7,24 +7,16 @@ from nose.tools import raises
 from unittest import TestCase
 import os
 
-from plotly.exceptions import PlotlyError
+from plotly.exceptions import PlotlyOfflineNotFound
 import plotly
 
 
 class PlotlyOfflineTestCase(TestCase):
-    @raises(PlotlyError)
-    def test_calling_iplot_before_initializing_raises_an_error(self):
-        plotly.offline.iplot([{'x': [1, 2, 3]}])
-
-    def test_no_errors_are_raised_when_initializing_offline_mode(self):
-        plotly.offline.init_notebook_mode()
-        plotly.offline.iplot([{'x': [1, 2, 3]}])
-
-    @raises(PlotlyError)
+    @raises(PlotlyOfflineNotFound)
     def test_initializing_before_downloading_raises_an_error(self):
         try:
             os.remove(plotly.offline.PLOTLY_OFFLINE_BUNDLE)
         except OSError:
             pass
 
-        plotly.offline.init_notebook_mode()
+        plotly.offline.iplot([{'x': [1, 2, 3]}])
