11f1f93044e735dacb0a44206907063dc3552748
Chris P <christopher.parmer@gmail.com>
2015-07-03 13:10:23 -0400

    fix import mixup in tests

--- plotly/tests/test_optional/test_offline/test_offline.py
+++ plotly/tests/test_optional/test_offline/test_offline.py
@@ -3,20 +3,32 @@ test__offline
 
 """
 from __future__ import absolute_import
-from nose.tools import raises
 from unittest import TestCase
 import os
 
-from plotly.exceptions import PlotlyOfflineNotFound
-import plotly
-
 
 class PlotlyOfflineTestCase(TestCase):
-    @raises(PlotlyOfflineNotFound)
     def test_initializing_before_downloading_raises_an_error(self):
+        # Delete the offline directory importing plotly
+        # Requires hardcoding this directory into the test
+        # from utils.py because simply importing plotly will set the
+        # __PLOTLY_OFFLINE_INITIALIZED flag
+        PLOTLY_OFFLINE_DIRECTORY = os.path.expanduser(
+            os.path.join(*'~/.plotly/plotlyjs'.split('/')))
+        PLOTLY_OFFLINE_BUNDLE = os.path.join(PLOTLY_OFFLINE_DIRECTORY,
+                                             'plotly-ipython-offline-bundle.js')
+
         try:
-            os.remove(plotly.offline.offline.PLOTLY_OFFLINE_BUNDLE)
+            os.remove(PLOTLY_OFFLINE_BUNDLE)
         except OSError:
             pass
 
-        plotly.offline.iplot([{'x': [1, 2, 3]}])
+        from plotly.exceptions import PlotlyOfflineNotFound
+        import plotly
+
+        try:
+            plotly.offline.iplot([{'x': [1, 2, 3]}])
+        except PlotlyOfflineNotFound:
+            pass
+        else:
+            raise Exception("PlotlyOfflineNotFound wasn't raised")
