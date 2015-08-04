ff96a3c8639af2cc184387c6b3cdf7b4e0f0cbe2
aneda <n.amin18@gmail.com>
2015-08-04 14:25:12 -0400

    updated doc for plot/ iplot

--- plotly/plotly/plotly.py
+++ plotly/plotly/plotly.py
@@ -154,11 +154,23 @@ def iplot(figure_or_data, **plot_options):
         'extend': add additional numbers (data) to existing traces
         'append': add additional traces to existing data lists
     world_readable (default=True) -- make this figure private/public
-    sharing ('public' | 'private' | 'secret')
-        'public': set the privacy of the plot to public
-        'private': set the privacy of the plot as private
-        'secret':  set the privacy of the plot as private and
-                   it also enables the plot to be shared
+    sharing ('public' | 'private' | 'sharing') -- Toggle who can view this
+                                                  graph
+        - 'public': Anyone can view this graph. It will appear in your profile
+                    and can appear in search engines. You do not need to be
+                    logged in to Plotly to view this chart.
+        - 'private': Only you can view this plot. It will not appear in the
+                     Plotly feed, your profile, or search engines. You must be
+                     logged in to Plotly to view this graph. You can privately
+                     share this graph with other Plotly users in your online
+                     Plotly account and they will need to be logged in to
+                     view this plot.
+        - 'secret': Anyone with this secret link can view this chart. It will
+                    not appear in the Plotly feed, your profile, or search
+                    engines. If it is embedded inside a webpage or an IPython
+                    notebook, anybody who is viewing that page will be able to
+                    view the graph. You do not need to be logged in to view
+                    this plot.
     """
     if 'auto_open' not in plot_options:
         plot_options['auto_open'] = False
@@ -204,11 +216,23 @@ def plot(figure_or_data, validate=True, **plot_options):
     auto_open (default=True) -- Toggle browser options
         True: open this plot in a new browser tab
         False: do not open plot in the browser, but do return the unique url
-    sharing ('public' | 'private' | 'secret')
-        'public': set the privacy of the plot to public
-        'private': set the privacy of the plot as private
-        'secret':  set the privacy of the plot as private and
-                   it also enables the plot to be shared
+    sharing ('public' | 'private' | 'sharing') -- Toggle who can view this
+                                                  graph
+        - 'public': Anyone can view this graph. It will appear in your profile
+                    and can appear in search engines. You do not need to be
+                    logged in to Plotly to view this chart.
+        - 'private': Only you can view this plot. It will not appear in the
+                     Plotly feed, your profile, or search engines. You must be
+                     logged in to Plotly to view this graph. You can privately
+                     share this graph with other Plotly users in your online
+                     Plotly account and they will need to be logged in to
+                     view this plot.
+        - 'secret': Anyone with this secret link can view this chart. It will
+                    not appear in the Plotly feed, your profile, or search
+                    engines. If it is embedded inside a webpage or an IPython
+                    notebook, anybody who is viewing that page will be able to
+                    view the graph. You do not need to be logged in to view
+                    this plot.
 
     """
     figure = tools.return_figure_from_figure_or_data(figure_or_data, validate)
