66ac993adab0212e0ce872418ff33026c216c3ba
chriddyp <chris@plot.ly>
2015-09-29 14:00:47 -0400

    use PlotlyTestCase for more robust signin

--- plotly/tests/test_optional/test_plotly/test_plot_mpl.py
+++ plotly/tests/test_optional/test_plotly/test_plot_mpl.py
@@ -17,33 +17,33 @@ from nose.tools import raises
 
 from plotly import exceptions
 from plotly.plotly import plotly as py
-
-py.sign_in('test-runner', '9h29fe3l0x')
-
-@raises(exceptions.PlotlyError)
-def test_update_type_error():
-    fig, ax = plt.subplots()
-    ax.plot([1, 2, 3])
-    update = []
-    py.plot_mpl(fig, update=update, filename="nosetests", auto_open=False)
+from plotly.tests.utils import PlotlyTestCase
 
 
-@raises(exceptions.PlotlyError)
-def test_update_validation_error():
-    fig, ax = plt.subplots()
-    ax.plot([1, 2, 3])
-    update = {'invalid': 'anything'}
-    py.plot_mpl(fig, update=update, filename="nosetests", auto_open=False)
+class PlotMPLTest(PlotlyTestCase):
+    @raises(exceptions.PlotlyError)
+    def test_update_type_error(self):
+        fig, ax = plt.subplots()
+        ax.plot([1, 2, 3])
+        update = []
+        py.plot_mpl(fig, update=update, filename="nosetests", auto_open=False)
 
+    @raises(exceptions.PlotlyError)
+    def test_update_validation_error(self):
+        fig, ax = plt.subplots()
+        ax.plot([1, 2, 3])
+        update = {'invalid': 'anything'}
+        py.plot_mpl(fig, update=update, filename="nosetests", auto_open=False)
 
-@attr('slow')
-def test_update():
-    fig, ax = plt.subplots()
-    ax.plot([1, 2, 3])
-    title = 'new title'
-    update = {'layout': {'title': title}}
-    url = py.plot_mpl(fig, update=update, filename="nosetests", auto_open=False)
-    un = url.replace("https://plot.ly/~", "").split('/')[0]
-    fid = url.replace("https://plot.ly/~", "").split('/')[1]
-    pfig = py.get_figure(un, fid)
-    assert pfig['layout']['title'] == title
+    @attr('slow')
+    def test_update(self):
+        fig, ax = plt.subplots()
+        ax.plot([1, 2, 3])
+        title = 'new title'
+        update = {'layout': {'title': title}}
+        url = py.plot_mpl(fig, update=update, filename="nosetests",
+                          auto_open=False)
+        un = url.replace("https://plot.ly/~", "").split('/')[0]
+        fid = url.replace("https://plot.ly/~", "").split('/')[1]
+        pfig = py.get_figure(un, fid)
+        assert pfig['layout']['title'] == title
