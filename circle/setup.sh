a9899df3f6451f11594278e71c4064efc30ff6f8
Andrew Seier <andseier@gmail.com>
2015-09-30 15:04:12 +0700

    Clean up the output text and add a `SIG`.
    I like how you can tell what *we’ve* printed out vs what has been
    printed out by code execution.

--- circle/setup.sh
+++ circle/setup.sh
@@ -1,42 +1,44 @@
 #!/bin/bash
 
-echo "running setup routine with python versions:"
+SIG="☁☀☂"
+
+echo "${SIG} Running setup routine with Python versions:"
 for version in ${PLOTLY_PYTHON_VERSIONS[@]}; do
-    echo "    ${version}"
+    echo "${SIG}     ${version}"
 done
 
 PROGNAME=$(basename $0)
 function error_exit
 {
-    echo -e "${PROGNAME}: ${1:-"Unknown Error"}\n" 1>&2
+    echo -e "${SIG} ${PROGNAME}: ${1:-"Unknown Error"}\n" 1>&2
     exit 1
 }
 
 # PYENV shims need to be infront of the rest of the path to work!
-echo "adding pyenv shims to the beginning of the path in this shell"
+echo "${SIG} Adding pyenv shims to the beginning of the path in this shell."
 export PATH="/home/ubuntu/.pyenv/shims:$PATH"
 
 # for each version we want, setup a functional virtual environment
 for version in ${PLOTLY_PYTHON_VERSIONS[@]}; do
-    echo Setting up Python ${version}
+    echo "${SIG} Setting up Python ${version}"
 
     # exporting this variable (in this scope) chooses the python version
     export PYENV_VERSION=${version}
-    echo "Using pyenv version $(pyenv version)"
+    echo "${SIG} Using pyenv version $(pyenv version)"
 
     # this was a major issue previously, sanity check that we're using the
     # version we *think* we're using (that pyenv is pointing to)
-    echo "python -c 'import sys; print(sys.version)'"
+    echo "${SIG} python -c 'import sys; print(sys.version)'"
     python -c 'import sys; print(sys.version)'
 
     # install core requirements all versions need
     pip install -r ${PLOTLY_CORE_REQUIREMENTS_FILE} ||
-        error_exit "${LINENO}: can't install core reqs for Python ${version}"
+        error_exit "${SIG} ${LINENO}: can't install core reqs for Python ${version}."
 
     pip install -r ${PLOTLY_OPTIONAL_REQUIREMENTS_FILE} ||
-        error_exit "${LINENO}: can't install optional for Python ${version}"
+        error_exit "${SIG} ${LINENO}: can't install optional for Python ${version}."
 
     # install some test tools
     pip install nose coverage ||
-        error_exit "${LINENO}: can't install test tools for Python ${version}"
+        error_exit "${SIG} ${LINENO}: can't install test tools for Python ${version}."
 done
--- circle/test.sh
+++ circle/test.sh
@@ -1,45 +1,47 @@
 #!/bin/bash
 
-echo "running test routine with python versions:"
+SIG="☁☀☂"
+
+echo "${SIG} Running test routine with python versions:"
 for version in ${PLOTLY_PYTHON_VERSIONS[@]}; do
-    echo "    ${version}"
+    echo "${SIG}     ${version}"
 done
 
 PROGNAME=$(basename $0)
 function error_exit
 {
-    echo -e "${PROGNAME}: ${1:-"Unknown Error"}\n" 1>&2
+    echo -e "${SIG} ${PROGNAME}: ${1:-"Unknown Error"}\n" 1>&2
     exit 1
 }
 
 # PYENV shims need to be infront of the rest of the path to work!
-echo "adding pyenv shims to the beginning of the path in this shell"
+echo "${SIG} Adding pyenv shims to the beginning of the path in this shell."
 export PATH="/home/ubuntu/.pyenv/shims:$PATH"
 
 # for each version we want, setup a functional virtual environment
 for version in ${PLOTLY_PYTHON_VERSIONS[@]}; do
-    echo Testing Python ${version}
+    echo "${SIG} Testing Python ${version}"
 
     # exporting this variable (in this scope) chooses the python version
     export PYENV_VERSION=${version}
-    echo "Using pyenv version $(pyenv version)"
+    echo "${SIG} Using pyenv version $(pyenv version)."
 
     # this was a major issue previously, sanity check that we're using the
     # version we *think* we're using (that pyenv is pointing to)
-    echo "Running: python -c 'import sys; print(sys.version)'. We've got:"
+    echo "${SIG} Running: python -c 'import sys; print(sys.version)'. We've got:"
     python -c 'import sys; print(sys.version)'
 
 
-    echo "install plotly (ignoring possibly cached versions)"
+    echo "${SIG} Install plotly (ignoring possibly cached versions)."
     pip install -I ${PLOTLY_PACKAGE_ROOT} >/dev/null ||
-        error_exit "${LINENO}: can't install plotly package from project root"
+        error_exit "${SIG} ${LINENO}: can't install plotly package from project root"
 
-    echo "import plotly to create .plotly dir if DNE"
+    echo "${SIG} Import plotly to create .plotly dir if DNE."
     python -c 'import plotly' >/dev/null ||
-        error_exit "${LINENO}: can't import plotly package"
+        error_exit "${SIG} ${LINENO}: can't import plotly package"
 
-    echo "running tests for Python ${version} as user '$(whoami)'"
+    echo "${SIG} Running tests for Python ${version} as user '$(whoami)'."
     nosetests -x plotly/tests ||
-        error_exit "${LINENO}: test suite failed for Python ${version}"
+        error_exit "${SIG} ${LINENO}: test suite failed for Python ${version}"
 
 done
