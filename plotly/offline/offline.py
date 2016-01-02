577177c5bdb30598f9f4a0ebb94150a3a14d798a
Josh Burkart <joshburkart@users.noreply.github.com>
2016-01-01 20:06:42 -0800

    Change to offline.init_notebook_mode()
    Make init_notebook_mode() only import the JS library once. Otherwise if someone puts it at the top of a Jupyter cell and it gets executed every time (typical pattern of usage?), it adds ~1 s to the cell's execution time.

--- plotly/offline/offline.py
+++ plotly/offline/offline.py
@@ -46,15 +46,16 @@ def init_notebook_mode():
     from IPython.display import HTML, display
 
     global __PLOTLY_OFFLINE_INITIALIZED
+    if not __PLOTLY_OFFLINE_INITIALIZED:
+        display(HTML('<script type="text/javascript">' +
+                     # ipython's includes `require` as a global, which
+                     # conflicts with plotly.js. so, unrequire it.
+                     'require=requirejs=define=undefined;' +
+                     '</script>' +
+                     '<script type="text/javascript">' +
+                     get_plotlyjs() +
+                     '</script>'))
     __PLOTLY_OFFLINE_INITIALIZED = True
-    display(HTML('<script type="text/javascript">' +
-                 # ipython's includes `require` as a global, which
-                 # conflicts with plotly.js. so, unrequire it.
-                 'require=requirejs=define=undefined;' +
-                 '</script>' +
-                 '<script type="text/javascript">' +
-                 get_plotlyjs() +
-                 '</script>'))
 
 
 def iplot(figure_or_data, show_link=True, link_text='Export to plot.ly',
