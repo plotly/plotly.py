6305c5bdf2751185bc18bc6c2a3523c1ff2b3f39
Kyle Kelley <rgbkrk@gmail.com>
2016-01-07 12:58:35 -0600

    more skeletons for the skeleton gods

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-${PYTHON} setup.py install || exit 1;
+$PYTHON setup.py install
 
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
