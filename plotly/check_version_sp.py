91e3f59db88cfe1808ac70b4c8b938e70d52a1f0
yankev <kevinn.yan@gmail.com>
2016-04-11 19:57:00 -0400

    added new method to get latest version with pip

---
+++ plotly/check_version_sp.py
@@ -0,0 +1,18 @@
+import subprocess
+
+def get_latest():
+	latest_version = subprocess.check_output(['pip', 'show', 'plotly']).split('\n')[2][9:]
+	return latest_version
+
+def run_duration(f, **kwargs):
+	import timeit
+	start_time = timeit.default_timer()
+	f(**kwargs)
+	return (timeit.default_timer() - start_time)
+
+
+
+
+
+
+
