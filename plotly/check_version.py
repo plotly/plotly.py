323b9ad2592ecf3d16d0c15ddf4d0f6b69c3b936
yankev <kevinn.yan@gmail.com>
2016-04-11 17:51:08 -0400

    updated plot schema and more robust regex

--- plotly/check_version.py
+++ plotly/check_version.py
@@ -5,7 +5,7 @@ import re
 def get_latest():
 	data = urllib2.urlopen('https://raw.githubusercontent.com/plotly/plotly.py/master/plotly/version.py') # it's a file like object and works just like a file
 	version_string = [i for i in data][0]
-	latest_version = re.findall(r'\'.*', version_string)[0][1:-1]
+	latest_version = re.findall(r'\'.*\'', version_string)[0][1:-1]
 	return latest_version
 
 def run_duration(f, **kwargs):
@@ -13,7 +13,7 @@ def run_duration(f, **kwargs):
 	start_time = timeit.default_timer()
 	f(**kwargs)
 	return (timeit.default_timer() - start_time)
-	
+
 
 
 
