15d88cf24d6ee5d334be2abce71d8594a9ac0b36
yankev <kevinn.yan@gmail.com>
2016-04-12 15:01:50 -0400

    using six for python 3 compatibility

--- plotly/__init__.py
+++ plotly/__init__.py
@@ -35,7 +35,9 @@ from plotly.check_version import get_latest
 
 _latest = get_latest()
 
-if __version__ != _latest :
-	print("Your version of Plotly is currently outdated,\n" + 
-		  "You're currently running {old}".format(old=__version__) +
-		  ", while the latest version is {}".format(_latest))
+if __version__ != _latest:
+    print("Your version of Plotly is currently outdated,\n" +
+          "You're currently running {}".format(__version__) +
+          ", while the latest version is {}".format(_latest))
+else:
+    print("Plotly {} is currently up to date".format(__version__))
--- plotly/check_version.py
+++ plotly/check_version.py
@@ -1,22 +1,17 @@
-import urllib2  # the lib that handles the url stuff
+from six.moves.urllib.request import urlopen
 import re
 
 
 def get_latest():
-	data = urllib2.urlopen('https://raw.githubusercontent.com/plotly/plotly.py/master/plotly/version.py') # it's a file like object and works just like a file
-	version_string = [i for i in data][0]
-	latest_version = re.findall(r'\'.*\'', version_string)[0][1:-1]
-	return latest_version
+    data = urlopen('https://raw.githubusercontent.com/plotly/plotly.py/' +
+                   'master/plotly/version.py')
+    version = [i for i in data][0]
+    latest_version = re.findall(r'([0-9\.]+)', str(version))[0]
+    return latest_version
+
 
 def run_duration(f, **kwargs):
-	import timeit
-	start_time = timeit.default_timer()
-	f(**kwargs)
-	return (timeit.default_timer() - start_time)
-
-
-
-
-
-
-
+    import timeit
+    start_time = timeit.default_timer()
+    f(**kwargs)
+    return (timeit.default_timer() - start_time)
