882e8e70d6456ff3fce5ff41e7655554166c4cb9
yankev <kevinn.yan@gmail.com>
2016-04-12 11:53:14 -0400

    supporting py3 because of pip3 show format

--- plotly/check_version.py
+++ plotly/check_version.py
@@ -1,11 +1,12 @@
 import os
 import re
+import sys
 
 
 def get_latest():
     os.system('pip show plotly > pip_version.txt')
     with open('pip_version.txt', 'r') as file:
-        output = [i for i in file][2]
+        output = [i for i in file][sys.version_info.major]
     latest_version = re.findall(r'([0-9\.]+)', output)[0]
     return latest_version
 
