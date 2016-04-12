246431d1d1de70f6eb0d0279796784140bac6dbc
yankev <kevinn.yan@gmail.com>
2016-04-12 17:04:52 -0400

    changed import name in case of conflict

--- plotly/check_version.py
+++ plotly/check_version.py
@@ -1,10 +1,10 @@
-from six.moves.urllib.request import urlopen
+from six.moves.urllib.request import urlopen as uo
 import re
 
 
 def get_latest():
-    data = urlopen('https://raw.githubusercontent.com/plotly/plotly.py/' +
-                   'master/plotly/version.py')
+    data = uo('https://raw.githubusercontent.com/plotly/plotly.py/' +
+              'master/plotly/version.py')
     version = [i for i in data][0]
     latest_version = re.findall(r'([0-9\.]+)', str(version))[0]
     return latest_version
