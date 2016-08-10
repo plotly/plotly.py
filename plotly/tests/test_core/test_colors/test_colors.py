a9a9d2c923ccc8252d23ddc863dbbf3f39934048
Adam Kulidjian <adam.kulidjian@gmail.com>
2016-08-10 16:05:08 -0400

    Added colors.py and test_colors folder with test file inside

---
+++ plotly/tests/test_core/test_colors/test_colors.py
@@ -0,0 +1,41 @@
+import math
+from unittest import TestCase
+
+from nose.tools import raises
+import plotly.tools as tls
+from plotly.exceptions import PlotlyError
+from plotly import colors
+
+"""
+class TestColors(TestCase):
+
+    def test_validate_colors(self):
+
+        # test string input
+        color = 'foo'
+
+        pattern = ("If your colors variable is a string, it must be a "
+                   "Plotly scale, an rgb color or a hex color.")
+
+        self.assertRaisesRegexp(PlotlyError, pattern, colors.validate_colors,
+                                color)
+
+        # test rgb color
+        color2 = 'rgb(265,0,0)'
+
+        pattern2 = ("Whoops! The elements in your rgb colors tuples cannot "
+                    "exceed 255.0.")
+
+        self.assertRaisesRegexp(PlotlyError, pattern2, colors.validate_colors,
+                                color2)
+
+        # test tuple color
+
+        color3 = (1.1, 1, 1)
+
+        pattern3 = ("Whoops! The elements in your colors tuples cannot "
+                    "exceed 1.0.")
+
+        self.assertRaisesRegexp(PlotlyError, pattern3, colors.validate_colors,
+                                color3)
+"""
