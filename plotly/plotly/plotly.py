9b8a0aab26fc57bb4a06e828ea910218e497bb51
Adam Kulidjian <adam.kulidjian@gmail.com>
2016-12-07 14:55:49 -0500

    added grid_url to message - forgot it before

--- plotly/plotly/plotly.py
+++ plotly/plotly/plotly.py
@@ -1488,9 +1488,10 @@ def get_grid(grid_url, raw=False):
     # check if request is ok
     if not r_meta.ok:
         raise exceptions.PlotlyError(
-            "Whoops, you got a 404 response. Your grid url {} may be "
+            "Whoops, you got a 404 response. {} may be "
             "pointing to a plot rather than a grid. If it is a grid, make "
-            "sure your grid is 'public' and not 'private' or 'secret'."
+            "sure your grid is 'public' and not 'private' or "
+            "'secret'.".format(grid_url)
         )
 
     json_res_meta = json.loads(r_meta.text)
