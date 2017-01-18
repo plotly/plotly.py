8ea90fe2d0be7bdcc5e8890c4f7cab0d56c74ade
Andrew Seier <andseier@gmail.com>
2017-01-17 18:28:07 -0800

    Add `users.current` endpoint to v2 api.

--- plotly/api/v2/__init__.py
+++ plotly/api/v2/__init__.py
@@ -1,3 +1,4 @@
 from __future__ import absolute_import
 
-from plotly.api.v2 import files, folders, grids, images, plot_schema, plots
+from plotly.api.v2 import (files, folders, grids, images, plot_schema, plots,
+                           users)
