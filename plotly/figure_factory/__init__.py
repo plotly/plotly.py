6e3962a01e044c698f9f0947ca44913b78614c25
Adam Kulidjian <adam.kulidjian@gmail.com>
2017-04-17 22:28:39 -0400

    add **kwargs to facet_grid

--- plotly/figure_factory/__init__.py
+++ plotly/figure_factory/__init__.py
@@ -8,6 +8,7 @@ from plotly.figure_factory._annotated_heatmap import create_annotated_heatmap
 from plotly.figure_factory._candlestick import create_candlestick
 from plotly.figure_factory._dendrogram import create_dendrogram
 from plotly.figure_factory._distplot import create_distplot
+from plotly.figure_factory._facet_grid import create_facet_grid
 from plotly.figure_factory._gantt import create_gantt
 from plotly.figure_factory._ohlc import create_ohlc
 from plotly.figure_factory._quiver import create_quiver
@@ -16,4 +17,3 @@ from plotly.figure_factory._streamline import create_streamline
 from plotly.figure_factory._table import create_table
 from plotly.figure_factory._trisurf import create_trisurf
 from plotly.figure_factory._violin import create_violin
-from plotly.figure_factory._facet_grid import create_facet_grid
