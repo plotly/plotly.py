f696f5ad0ba8c2429a34e7318d76ebeac2273c60
Adam Kulidjian <adam.kulidjian@gmail.com>
2017-04-23 13:18:35 -0400

    add some templates to the file

--- plotly/presentation_objs/presentation_objs.py
+++ plotly/presentation_objs/presentation_objs.py
@@ -9,9 +9,74 @@ A module for creating and manipulating spectacle-presentation dashboards.
 import pprint
 
 from plotly import exceptions, optional_imports
-from plotly.utils import node_generator
 
 IPython = optional_imports.get_module('IPython')
+
+paragraph_styles = {u'Body': {u'color': u'#3d3d3d',
+                              u'fontFamily': u'Open Sans',
+                              u'fontSize': 11,
+                              u'fontStyle': u'normal',
+                              u'fontWeight': 400,
+                              u'lineHeight': u'normal',
+                              u'minWidth': 20,
+                              u'opacity': 1,
+                              u'textAlign': u'center',
+                              u'textDecoration': u'none'},
+                    u'Body Small': {u'color': u'#3d3d3d',
+                                    u'fontFamily': u'Open Sans',
+                                    u'fontSize': 10,
+                                    u'fontStyle': u'normal',
+                                    u'fontWeight': 400,
+                                    u'lineHeight': u'normal',
+                                    u'minWidth': 20,
+                                    u'opacity': 1,
+                                    u'textAlign': u'center',
+                                    u'textDecoration': u'none'},
+                    u'Caption': {u'color': u'#3d3d3d',
+                                 u'fontFamily': u'Open Sans',
+                                 u'fontSize': 11,
+                                 u'fontStyle': u'italic',
+                                 u'fontWeight': 400,
+                                 u'lineHeight': u'normal',
+                                 u'minWidth': 20,
+                                 u'opacity': 1,
+                                 u'textAlign': u'center',
+                                 u'textDecoration': u'none'},
+                    u'Heading 1': {u'color': u'#3d3d3d',
+                                   u'fontFamily': u'Open Sans',
+                                   u'fontSize': 26,
+                                   u'fontStyle': u'normal',
+                                   u'fontWeight': 400,
+                                   u'lineHeight': u'normal',
+                                   u'minWidth': 20,
+                                   u'opacity': 1,
+                                   u'textAlign': u'center',
+                                   u'textDecoration': u'none'},
+                 u'Heading 2': {u'color': u'#3d3d3d',
+                                u'fontFamily': u'Open Sans',
+                                u'fontSize': 20,
+                                u'fontStyle': u'normal',
+                                u'fontWeight': 400,
+                                u'lineHeight': u'normal',
+                                u'minWidth': 20,
+                                u'opacity': 1,
+                                u'textAlign': u'center',
+                                u'textDecoration': u'none'},
+                 u'Heading 3': {u'color': u'#3d3d3d',
+                                u'fontFamily': u'Open Sans',
+                                u'fontSize': 11,
+                                u'fontStyle': u'normal',
+                                u'fontWeight': 700,
+                                u'lineHeight': u'normal',
+                                u'minWidth': 20,
+                                u'opacity': 1,
+                                u'textAlign': u'center',
+                                u'textDecoration': u'none'}}
+
+def _slide_template(id, transition):
+    slide_temp = {'children': [],
+                  'id': id,
+                  'props': {'style': {}, 'transition': transition}}
 
 pres_template = {
     'presentation': {
