c5f90230b8e596307af49ec0f97eb8e9db8ce1f0
Patrick Burris <patrick@briefmedia.com>
2017-05-13 18:51:02 -0500

    Fix: spelling on PlotlyDataTypeError message

--- plotly/exceptions.py
+++ plotly/exceptions.py
@@ -122,7 +122,7 @@ class PlotlyDataTypeError(PlotlyGraphObjectError):
         format_dict = {'index': path[-1], 'object_name': obj._name}
         message = ("Invalid entry found in '{object_name}' at index, '{index}'"
                    .format(**format_dict))
-        note = "It's invalid because it does't contain a valid 'type' value."
+        note = "It's invalid because it doesn't contain a valid 'type' value."
         notes = [note] + list(notes)
         super(PlotlyDataTypeError, self).__init__(
             message=message, path=path, notes=notes
