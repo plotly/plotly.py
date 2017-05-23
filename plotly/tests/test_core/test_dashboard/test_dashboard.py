6a39523175c0d77fe4dce00df3f95ef277b66e8f
Adam Kulidjian <adam.kulidjian@gmail.com>
2017-05-23 11:25:31 -0400

    updated dashboard tests

--- plotly/tests/test_core/test_dashboard/test_dashboard.py
+++ plotly/tests/test_core/test_dashboard/test_dashboard.py
@@ -96,10 +96,10 @@ class TestDashboard(TestCase):
         )
 
         dash = dashboard.Dashboard()
-        dash.insert(my_box, 'above', 0)
+        dash.insert(my_box, 'above')
 
         self.assertRaisesRegexp(PlotlyError, message, dash.insert,
-                                my_box, 'somewhere', 1)
+                                my_box, 'somewhere', 'a')
 
     def test_dashboard_dict(self):
         my_box = {
@@ -111,8 +111,8 @@ class TestDashboard(TestCase):
         }
 
         dash = dashboard.Dashboard()
-        dash.insert(my_box, '', 0)
-        dash.insert(my_box, 'above', 1)
+        dash.insert(my_box, '')
+        dash.insert(my_box, 'above', 'a')
 
         expected_dashboard = {
             'layout': {'direction': 'vertical',
