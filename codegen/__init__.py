b60e90e7dc0f786f279bf815ea3452df14ce006d
Jon M. Mease <jon.mease@jhuapl.edu>
2018-03-10 11:46:43 -0500

    Integrate graph_objs codegen and improve codegen reliability

--- codegen/__init__.py
+++ codegen/__init__.py
@@ -1,18 +1,26 @@
 import json
 import os.path as opath
-import os
 import shutil
 
-import time
-
-from codegen.datatypes import build_datatypes_py, write_datatypes_py, append_figure_class
-from codegen.utils import TraceNode, PlotlyNode, LayoutNode, FrameNode
-from codegen.validators import write_validator_py, append_traces_validator_py
-
+from codegen.graph_objs import (delete_graph_objs_non_generated_lines,
+                                append_graph_objs_generated_lines)
 
 def perform_codegen():
     outdir = 'plotly/'
-    # outdir = 'codegen/output'
+
+    # Delete non-generated lines from graph_objs.py
+    # ---------------------------------------------
+    delete_graph_objs_non_generated_lines()
+
+    # Perform codegen imports
+    # -----------------------
+    # this must happend after deleting non-generated lines above
+    from codegen.datatypes import (build_datatypes_py, write_datatypes_py,
+                                   append_figure_class)
+    from codegen.utils import TraceNode, PlotlyNode, LayoutNode, FrameNode
+    from codegen.validators import (write_validator_py,
+                                    append_traces_validator_py)
+
     # Load plotly schema
     # ------------------
     with open('plotly/package_data/default-schema.json', 'r') as f:
@@ -72,6 +80,10 @@ def perform_codegen():
     # Append figure class to datatypes
     # --------------------------------
     append_figure_class(datatypes_pkgdir, base_traces_node)
+
+    # Append generated lines to graph_objs.py
+    # ------------------------------------
+    append_graph_objs_generated_lines()
 
 
 if __name__ == '__main__':
--- codegen/graph_objs.py
+++ codegen/graph_objs.py
@@ -1,8 +1,5 @@
 from __future__ import print_function
 
-from plotly.graph_objs import graph_objs_tools
-from plotly.graph_reference import ARRAYS, CLASSES, TRACE_NAMES
-
 FLAG = '# AUTO-GENERATED BELOW. DO NOT EDIT! See makefile.'
 
 
@@ -30,6 +27,8 @@ def get_non_generated_file_lines():
 
 
 def print_class(name, f):
+    from plotly.graph_reference import ARRAYS, CLASSES, TRACE_NAMES
+
     if name == 'Figure':
         print('from plotly.datatypes import Figure', file=f, end='\n\n\n')
     elif name == 'Layout':
@@ -48,20 +47,32 @@ def print_class(name, f):
             print('    pass', file=f, end='\n\n\n')
 
 
-copied_lines = get_non_generated_file_lines()
-with open('./plotly/graph_objs/graph_objs.py', 'w') as graph_objs_file:
+def delete_graph_objs_non_generated_lines():
+    copied_lines = get_non_generated_file_lines()
 
-    # Keep things *exactly* as they were above our special FLAG.
-    for line in copied_lines:
-        print(line, file=graph_objs_file, end='')
-    print(FLAG, file=graph_objs_file)
+    with open('./plotly/graph_objs/graph_objs.py', 'w') as graph_objs_file:
+        # Keep things *exactly* as they were above our special FLAG.
+        for line in copied_lines:
+            print(line, file=graph_objs_file, end='')
+        print(FLAG, file=graph_objs_file)
 
-    # For each object in the plot schema, generate a class in the file.
-    class_names = list(CLASSES.keys())
-    class_names.sort()
-    for class_name in class_names:
-        print_class(class_name, graph_objs_file)
 
-    # Finish off the file by only exporting plot-schema names.
-    print('__all__ = [cls for cls in graph_reference.CLASSES.keys() '
-          'if cls in globals()] + ["FigureWidget"]', file=graph_objs_file)
+def append_graph_objs_generated_lines():
+    from plotly.graph_reference import CLASSES
+
+    with open('./plotly/graph_objs/graph_objs.py', 'a') as graph_objs_file:
+
+        # For each object in the plot schema, generate a class in the file.
+        class_names = list(CLASSES.keys())
+        class_names.sort()
+        for class_name in class_names:
+            print_class(class_name, graph_objs_file)
+
+        # Print FigureWidget import
+        print('from plotly.datatypes import FigureWidget\n\n',
+              file=graph_objs_file)
+
+        # Finish off the file by only exporting plot-schema names.
+        print("""\
+__all__ = [cls for cls in graph_reference.CLASSES.keys() 
+           if cls in globals()] + ['FigureWidget']""", file=graph_objs_file)
