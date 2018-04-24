53bcdec37aa1e76cf6e79ff5ceda6dff64b62255
Adam Kulidjian <adam.kulidjian@gmail.com>
2018-04-24 17:15:33 -0400

    fixed most of test_update

--- plotly/tests/test_core/test_graph_objs/test_update.py
+++ plotly/tests/test_core/test_graph_objs/test_update.py
@@ -4,6 +4,7 @@ from unittest import skip
 from plotly.graph_objs import Data, Figure, Layout, Line, Scatter, XAxis
 from plotly.tests.utils import strip_dict_params
 
+
 def test_update_dict():
     title = 'this'
     fig = Figure()
@@ -18,30 +19,35 @@ def test_update_list():
     trace2 = Scatter(x=[1, 2, 3], y=[3, 2, 1])
     fig = Figure([trace1, trace2])
     update = dict(x=[2, 3, 4], y=[1, 2, 3])
-    fig.data[0].update({'x': [2, 3, 4], 'y': [1, 2, 3]})
-    fig.data[1].update({'x': [2, 3, 4], 'y': [1, 2, 3]})
+    fig.data[0].update(update)
+    fig.data[1].update(update)
 
-    d1, d2 = strip_dict_params()
-    assert fig.data[0] == Scatter(x=[2, 3, 4], y=[1, 2, 3])
-    assert fig.data[1] == Scatter(x=[2, 3, 4], y=[1, 2, 3])
+    d1, d2 = strip_dict_params(fig.data[0], Scatter(x=[2, 3, 4], y=[1, 2, 3]))
+    assert d1 == d2
+    d1, d2 = strip_dict_params(fig.data[1], Scatter(x=[2, 3, 4], y=[1, 2, 3]))
+    assert d1 == d2
 
 
 def test_update_dict_empty():
     trace1 = Scatter(x=[1, 2, 3], y=[2, 1, 2])
     trace2 = Scatter(x=[1, 2, 3], y=[3, 2, 1])
-    data = Data([trace1, trace2])
-    data.update({})
-    assert data[0] == Scatter(x=[1, 2, 3], y=[2, 1, 2])
-    assert data[1] == Scatter(x=[1, 2, 3], y=[3, 2, 1])
+    fig = Figure([trace1, trace2])
+    fig.update({})
+    d1, d2 = strip_dict_params(fig.data[0], Scatter(x=[1, 2, 3], y=[2, 1, 2]))
+    assert d1 == d2
+    d1, d2 = strip_dict_params(fig.data[1], Scatter(x=[1, 2, 3], y=[3, 2, 1]))
+    assert d1 == d2
 
 
 def test_update_list_empty():
     trace1 = Scatter(x=[1, 2, 3], y=[2, 1, 2])
     trace2 = Scatter(x=[1, 2, 3], y=[3, 2, 1])
-    data = Data([trace1, trace2])
-    data.update([])
-    assert data[0] == Scatter(x=[1, 2, 3], y=[2, 1, 2])
-    assert data[1] == Scatter(x=[1, 2, 3], y=[3, 2, 1])
+    fig = Figure([trace1, trace2])
+    fig.update([])
+    d1, d2 = strip_dict_params(fig.data[0], Scatter(x=[1, 2, 3], y=[2, 1, 2]))
+    assert d1 == d2
+    d1, d2 = strip_dict_params(fig.data[1], Scatter(x=[1, 2, 3], y=[3, 2, 1]))
+    assert d1 == d2
 
 
 @skip('See https://github.com/plotly/python-api/issues/291')
