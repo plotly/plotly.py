38127fa961ae23e873828c86d80d1cfc2e368ec4
Jon Mease <jon.mease@gmail.com>
2018-08-11 19:50:53 -0400

    Added OrcaStatus singleton to hold status of orca server process
    Added orca server process management tests

--- plotly/io/_orca.py
+++ plotly/io/_orca.py
@@ -22,7 +22,18 @@ def _find_open_port():
 
     return port
 
+def which(cmd):
+    import shutil
+    # TODO: this doesn't exist on Python 2.7 :-(
+    return shutil.which(cmd)
+
 class OrcaConfig(object):
+    """
+    Contains user defined configuration for orca
+
+    These should eventually be loaded from somewhere in the ~/.plotly
+    directory.
+    """
     _props = {
         'port': None,
         'hostname': 'localhost',
@@ -87,11 +98,145 @@ orca configuration
 config = OrcaConfig()
 del OrcaConfig
 
+
+class OrcaStatus(object):
+    """
+    Class to store information about the current status of the orca server.
+
+    This class is intended to be updated only by the _orca module, and
+    viewed by the user
+    """
+    _props = {
+        'state': 'unvalidated', # or 'validated' or 'running'
+        'executable': None,
+        'version': None,
+        'pid': None,
+        'port': None
+    }
+
+    @property
+    def state(self):
+        """
+        A string representing the state of the orca server process
+
+        One of:
+          - unvalidated: The orca executable has not yet been searched for or
+            tested for validity
+          - verified: The orca executable has been located and tested for
+            validity, but is not running
+          - running: The orca server process is currently running
+
+        """
+        return self._props['state']
+
+    @property
+    def executable(self):
+        """
+        If the `state` is 'validated' or 'running', this property contains the
+        full path to the orca executable.  This path can be specified
+        explicitly by setting the `executable` property of the
+        `plotly.io.orca.config` object.
+        """
+        return self._props['executable']
+
+    @property
+    def version(self):
+        """
+        The version of the verified orca executable, if any. This property
+        will be None if the `state` is 'unvalidated'
+        """
+        return self._props['version']
+
+
+    @property
+    def pid(self):
+        """
+        The process id of the orca server process, if any. This property
+        will be None if the `state` is not 'running'
+        """
+        return self._props['pid']
+
+
+    @property
+    def port(self):
+        """
+        The port number that the orca server process is listening to, if any.
+        This property will be None if the `state` is not 'running'
+        """
+        return self._props['port']
+
+    def __repr__(self):
+        return """\
+    orca status
+    -----------
+""" + pformat(self._props, width=40)
+
+
+status = OrcaStatus()
+del OrcaStatus
+
+
+def validate_orca_executable():
+    if status.state != 'unvalidated':
+        # Nothing more to do
+        return
+
+    # Try to find an executable named orca
+    # ------------------------------------
+    executable = which(config.executable)
+    if executable is None:
+        raise ValueError("""
+The orca executable could not be found on the system path.
+
+If you havne't installed orca...
+
+If you have already installed orca, make sure the orca executable is on your
+system path. Or specify the orca path explicitly in the executable property
+of the plotly.io.orca.config configuration object.""")
+
+    # Run executable with --help and see if it's our orca
+    # ---------------------------------------------------
+    invalid_executable_msg = "Invalid orca executable at..."
+    try:
+        help_result = subprocess.check_output([executable, '--help'])
+    except subprocess.CalledProcessError:
+        raise ValueError(invalid_executable_msg)
+
+    if not help_result:
+        raise ValueError(invalid_executable_msg)
+
+    if 'plotly' not in help_result.decode('utf-8').lower():
+        raise ValueError(invalid_executable_msg)
+
+    # Get orca version
+    # ----------------
+    try:
+        orca_version = subprocess.check_output([executable, '--version'])
+    except subprocess.CalledProcessError:
+        raise ValueError("version failed")
+
+    if not orca_version:
+        raise ValueError("No version reported")
+    else:
+        orca_version = orca_version.decode()
+
+    # Check version >= 1.1.0 so we have --graph-only support.
+    status._props['executable'] = executable
+    status._props['version'] = orca_version.strip()
+    status._props['state'] = 'validated'
+
+
+def reset_orca_status():
+    shutdown_orca_server()
+    status._props['executable'] = None
+    status._props['version'] = None
+    status._props['state'] = 'unvalidated'
+
+
 # Initialze process control
 __orca_lock = threading.Lock()
 __orca_state = {'proc': None,
                 'shutdown_timer': None}
-
 
 @atexit.register
 def shutdown_orca_server():
@@ -105,16 +250,26 @@ def shutdown_orca_server():
                     else:
                         __orca_state['proc'].terminate()  # Unix
 
+                    child_status = __orca_state['proc'].wait()
+
                     __orca_state['proc'] = None
                     __orca_state['shutdown_timer'].cancel()
                     __orca_state['shutdown_timer'] = None
                     __orca_state['port'] = None
+
+                    # Update status
+                    status._props['state'] = 'validated'
+                    status._props['pid'] = None
+                    status._props['port'] = None
                 except:
                     pass
 
 
 # Launch or get server
 def ensure_orca_server():
+    if status.state == 'unvalidated':
+        validate_orca_executable()
+
     with __orca_lock:
         if __orca_state['shutdown_timer'] is not None:
             __orca_state['shutdown_timer'].cancel()
@@ -135,23 +290,31 @@ def ensure_orca_server():
                  '--graph-only'],
                 creationflags=creationflags)
 
+            # Update status
+            status._props['state'] = 'running'
+            status._props['pid'] = __orca_state['proc'].pid
+            status._props['port'] = __orca_state['port']
+
         t = threading.Timer(config.timeout, shutdown_orca_server)
         t.start()
         __orca_state['shutdown_timer'] = t
 
 
 @retrying.retry(wait_random_min=5, wait_random_max=10, stop_max_delay=10000)
-def _request_image_with_retrying(fig):
+def _request_image_with_retrying(**kwargs):
     server_url = 'http://{hostname}:{port}'.format(
         hostname=config.hostname, port=__orca_state['port'])
-    json_str = json.dumps({'figure': fig}, cls=plotly.utils.PlotlyJSONEncoder)
+
+    request_params = {k: v for k, v, in kwargs.items() if v is not None}
+    json_str = json.dumps(request_params, cls=plotly.utils.PlotlyJSONEncoder)
     r = requests.post(server_url + '/', data=json_str)
     return r.content
 
 
-def to_image(fig):
+def to_image(fig, format='png', scale=1.0, width=None, height=None):
     """
     Convert a figure to an image bytes string
     """
     ensure_orca_server()
-    return _request_image_with_retrying(fig)
+    return _request_image_with_retrying(
+        figure=fig, format=format, scale=scale, width=width, height=height)
--- plotly/io/orca.py
+++ plotly/io/orca.py
@@ -1,1 +1,7 @@
-from ._orca import (ensure_orca_server, shutdown_orca_server, config)
+from ._orca import (
+    ensure_orca_server,
+    shutdown_orca_server,
+    validate_orca_executable,
+    reset_orca_status,
+    config,
+    status)
