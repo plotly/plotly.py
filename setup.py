d8958aa39d2760f2c994f9c6a50f8317c3fb140e
Jon Mease <jon.mease@gmail.com>
2018-08-25 09:50:16 -0400

    Remove topojson from packagedata (not distributing it after all)

--- setup.py
+++ setup.py
@@ -265,7 +265,7 @@ setup(name='plotly',
                 'plotly/matplotlylib/mplexporter',
                 'plotly/matplotlylib/mplexporter/renderers',
                 '_plotly_utils'] + graph_objs_packages + validator_packages,
-      package_data={'plotly': ['package_data/*', 'package_data/topojson/*'],
+      package_data={'plotly': ['package_data/*'],
                     'plotlywidget': ['static/*']},
       data_files=[
           ('share/jupyter/nbextensions/plotlywidget', [
