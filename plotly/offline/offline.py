e41ed94ee35a243db72e30ebcd0cc9ea4a305a98
Jon Mease <jon.mease@gmail.com>
2018-09-02 05:57:47 -0400

    Fix JavaScript `Error: addFrames failure` in offline html export

--- plotly/offline/offline.py
+++ plotly/offline/offline.py
@@ -174,9 +174,9 @@ def _plot_html(figure_or_data, config, validate, default_width,
     jdata = _json.dumps(figure.get('data', []), cls=utils.PlotlyJSONEncoder)
     jlayout = _json.dumps(figure.get('layout', {}),
                           cls=utils.PlotlyJSONEncoder)
-    if 'frames' in figure_or_data:
-        jframes = _json.dumps(figure.get('frames', {}),
-                              cls=utils.PlotlyJSONEncoder)
+
+    jframes = _json.dumps(figure.get('frames', []),
+                          cls=utils.PlotlyJSONEncoder)
 
     configkeys = (
         'staticPlot',
@@ -229,7 +229,7 @@ def _plot_html(figure_or_data, config, validate, default_width,
         config['linkText'] = link_text
         jconfig = jconfig.replace('Export to plot.ly', link_text)
 
-    if 'frames' in figure_or_data:
+    if jframes:
         script = '''
         Plotly.plot(
             '{id}',
