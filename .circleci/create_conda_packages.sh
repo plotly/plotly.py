9c8918e7ed80b0e20a4efadb27f02c7e0b856b5f
Jon Mease <jon.mease@gmail.com>
2018-09-03 19:40:19 -0400

    Fix conda_packages directory name and install zip utility

--- .circleci/create_conda_packages.sh
+++ .circleci/create_conda_packages.sh
@@ -1,3 +1,6 @@
+# Install zip
+sudo apt-get install -y zip
+
 # Install conda dependencies
 conda install -y conda-build conda-verify
 
@@ -7,10 +10,10 @@ conda build --python $PYTHON_VERSION recipe/
 # Convert to other architectures
 mkdir -p ./conda_packages_${PYTHON_VERSION}/linux-64/
 mv /opt/conda/conda-bld/linux-64/plotly-*.tar.bz2 ./conda_packages_${PYTHON_VERSION}/linux-64/
-conda convert -p linux-32 conda_packages/linux-64/plotly-*.tar.bz2 -o ./conda_packages_${PYTHON_VERSION}/
-conda convert -p osx-64 conda_packages/linux-64/plotly-*.tar.bz2 -o ./conda_packages_${PYTHON_VERSION}/
-conda convert -p win-64 conda_packages/linux-64/plotly-*.tar.bz2 -o ./conda_packages_${PYTHON_VERSION}/
-conda convert -p win-32 conda_packages/linux-64/plotly-*.tar.bz2 -o ./conda_packages_${PYTHON_VERSION}/
+conda convert -p linux-32 ./conda_packages_${PYTHON_VERSION}/linux-64/plotly-*.tar.bz2 -o ./conda_packages_${PYTHON_VERSION}/
+conda convert -p osx-64 ./conda_packages_${PYTHON_VERSION}/linux-64/plotly-*.tar.bz2 -o ./conda_packages_${PYTHON_VERSION}/
+conda convert -p win-64 ./conda_packages_${PYTHON_VERSION}/linux-64/plotly-*.tar.bz2 -o ./conda_packages_${PYTHON_VERSION}/
+conda convert -p win-32 ./conda_packages_${PYTHON_VERSION}/linux-64/plotly-*.tar.bz2 -o ./conda_packages_${PYTHON_VERSION}/
 
 # zip up packages into artifacts directory
 mkdir artifacts
