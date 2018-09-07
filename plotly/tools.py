58bbae755454b538bd5994cf99947d0f05944cf9
Jon Mease <jon.mease@gmail.com>
2018-09-07 18:48:47 -0400

    Revert "Set skip_invalid=True when constructing figure wtih `get_figure`"
    This will take a bit more thought. We'll get back to this during the
    develop,ent of plotly.io.
    
    This reverts commit 744d947857b8ba3a78d1dfd431849ad34a698e75.

--- plotly/tools.py
+++ plotly/tools.py
@@ -1364,7 +1364,10 @@ def make_subplots(rows=1, cols=1,
 
 def get_graph_obj(obj, obj_type=None):
     """Returns a new graph object.
-    This will *silently* strip out invalid pieces of the object.
+
+    OLD FUNCTION: this will *silently* strip out invalid pieces of the object.
+    NEW FUNCTION: no striping of invalid pieces anymore - only raises error
+        on unrecognized graph_objs
     """
     # TODO: Deprecate or move. #283
     from plotly.graph_objs import graph_objs
@@ -1374,7 +1377,7 @@ def get_graph_obj(obj, obj_type=None):
         raise exceptions.PlotlyError(
             "'{}' is not a recognized graph_obj.".format(obj_type)
         )
-    return cls(obj, skip_invalid=True)
+    return cls(obj)
 
 
 def validate(obj, obj_type):
