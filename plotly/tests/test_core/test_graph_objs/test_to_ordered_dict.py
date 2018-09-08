764f31d61a1c095bb44eb911d213bb13298d5f3a
Jon Mease <jon.mease@gmail.com>
2018-09-08 19:13:05 -0400

    Format indentation [skip ci]

--- plotly/tests/test_core/test_graph_objs/test_to_ordered_dict.py
+++ plotly/tests/test_core/test_graph_objs/test_to_ordered_dict.py
@@ -48,16 +48,16 @@ class FigureTest(TestCase):
 
     def test_to_ordered_with_frames(self):
         frame = go.Frame(layout={'yaxis': {'range': [1, 2]},
-                               'xaxis': {'range': [1, 2]},
-                               'shapes': [{'xsizemode': 'pixel',
-                                           'type': 'circle'},
-                                          {'type': 'line',
-                                           'xsizemode': 'pixel'}]},
-                       data=[{'type': 'scatter',
-                              'marker': {'size': 12, 'color': 'green'}},
-                             {'type': 'bar',
-                              'y': [1, 2],
-                              'x': [1, 2]}])
+                                 'xaxis': {'range': [1, 2]},
+                                 'shapes': [{'xsizemode': 'pixel',
+                                             'type': 'circle'},
+                                            {'type': 'line',
+                                             'xsizemode': 'pixel'}]},
+                         data=[{'type': 'scatter',
+                                'marker': {'size': 12, 'color': 'green'}},
+                               {'type': 'bar',
+                                'y': [1, 2],
+                                'x': [1, 2]}])
 
         fig = go.Figure(frames=[{}, frame])
         result = fig.to_ordered_dict()
@@ -69,19 +69,19 @@ class FigureTest(TestCase):
             OrderedDict([('type', 'bar'),
                          ('x', [1, 2]),
                          ('y', [1, 2])])]),
-                                ('layout', OrderedDict([
-                                    ('shapes', [
-                                        OrderedDict([
-                                            ('type', 'circle'),
-                                            ('xsizemode', 'pixel')]),
-                                        OrderedDict([
-                                            ('type', 'line'),
-                                            ('xsizemode', 'pixel')])]),
-                                    ('xaxis', OrderedDict(
-                                        [('range', [1, 2])])),
-                                    ('yaxis', OrderedDict(
-                                        [('range', [1, 2])]))
-                                ]))])
+                                      ('layout', OrderedDict([
+                                          ('shapes', [
+                                              OrderedDict([
+                                                  ('type', 'circle'),
+                                                  ('xsizemode', 'pixel')]),
+                                              OrderedDict([
+                                                  ('type', 'line'),
+                                                  ('xsizemode', 'pixel')])]),
+                                          ('xaxis', OrderedDict(
+                                              [('range', [1, 2])])),
+                                          ('yaxis', OrderedDict(
+                                              [('range', [1, 2])]))
+                                      ]))])
 
         expected = OrderedDict([('data', []),
                                 ('layout', OrderedDict()),
