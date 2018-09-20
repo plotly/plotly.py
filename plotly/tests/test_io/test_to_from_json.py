d84a4dd26c21961e8fa76bf9c6a2f69db0dc8f5d
Jon Mease <jon.mease@gmail.com>
2018-09-20 08:15:31 -0400

    Add backports.tempfile for Python 2.7 testing compatibility

--- plotly/tests/test_io/test_to_from_json.py
+++ plotly/tests/test_io/test_to_from_json.py
@@ -4,13 +4,14 @@ import pytest
 import plotly
 import json
 import sys
-import tempfile
 import os
 
 if sys.version_info.major == 3 and sys.version_info.minor >= 3:
     from unittest.mock import MagicMock
+    import tempfile
 else:
     from mock import MagicMock
+    from backports import tempfile
 
 # fixtures
 # --------
