01c65e1f3de6450865744278b5489caa9e9c4515
Jon Mease <jon.mease@gmail.com>
2018-10-29 06:36:53 -0400

    Added get_plotlyjs and get_plotlyjs_version functions (#1246)
    * Make get_plotlyjs public and add docstring
    * Added plotly.offline.get_plotlyjs_version() function.
      This function returns the plotly.js versions that is bundled with plotly.py.
      The returned version is updated automatically during the updatebundle setup.py command.

--- plotly/offline/offline.py
+++ plotly/offline/offline.py
@@ -18,6 +18,7 @@ from requests.compat import json as _json
 import plotly
 from plotly import optional_imports, tools, utils
 from plotly.exceptions import PlotlyError
+from ._plotlyjs_version import __plotlyjs_version__
 
 ipython = optional_imports.get_module('IPython')
 ipython_display = optional_imports.get_module('IPython.display')
@@ -37,10 +38,65 @@ def download_plotlyjs(download_url):
     pass
 
 
+def get_plotlyjs_version():
+    """
+    Returns the version of plotly.js that is bundled with plotly.py.
+
+    Returns
+    -------
+    str
+        Plotly.js version string
+    """
+    return __plotlyjs_version__
+
+
 def get_plotlyjs():
+    """
+    Return the contents of the minified plotly.js library as a string.
+
+    This may be useful when building standalone HTML reports.
+
+    Returns
+    -------
+    str
+        Contents of the minified plotly.js library as a string
+
+    Examples
+    --------
+    Here is an example of creating a standalone HTML report that contains
+    two plotly figures, each in their own div.  The include_plotlyjs argument
+    is set to False when creating the divs so that we don't include multiple
+    copies of the plotly.js library in the output.  Instead, a single copy
+    of plotly.js is included in a script tag in the html head element.
+
+    >>> import plotly.graph_objs as go
+    >>> from plotly.offline import plot, get_plotlyjs
+    >>> fig1 = go.Figure(data=[{'type': 'bar', 'y': [1, 3, 2]}],
+    ...                 layout={'height': 400})
+    >>> fig2 = go.Figure(data=[{'type': 'scatter', 'y': [1, 3, 2]}],
+    ...                  layout={'height': 400})
+    >>> div1 = plot(fig1, output_type='div', include_plotlyjs=False)
+    >>> div2 = plot(fig2, output_type='div', include_plotlyjs=False)
+
+    >>> html = '''
+    ... <html>
+    ...     <head>
+    ...         <script type="text/javascript">{plotlyjs}</script>
+    ...     </head>
+    ...     <body>
+    ...        {div1}
+    ...        {div2}
+    ...     </body>
+    ... </html>
+    ...'''.format(plotlyjs=get_plotlyjs(), div1=div1, div2=div2)
+
+    >>> with open('multi_plot.html', 'w') as f:
+    ...      f.write(html)
+    """
     path = os.path.join('package_data', 'plotly.min.js')
     plotlyjs = pkgutil.get_data('plotly', path).decode('utf-8')
     return plotlyjs
+
 
 def get_image_download_script(caller):
     """
