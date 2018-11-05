plotly/plotly.py

commit ae085bf186a7c841bfad97f7337bbddd73057143

Prevent iplot from breaking when Jupyter notebook has headline named "Plotly" (#1250)
This PR works around the issue where a Jupyter Notebook heading
named "Plotly" clashes with the window.Plotly variable where we store
the plotly.js module.

The workaround is to name this variable the much less likely _Plotly

plotly/offline/offline.py
@@ -204,9 +204,9 @@ def init_notebook_mode(connected=False):
             # Note we omit the extension .js because require will include it.
             '\'plotly\': [\'https://cdn.plot.ly/plotly-latest.min\']}},'
             '}});'
-            'if(!window.Plotly) {{'
+            'if(!window._Plotly) {{'
             'require([\'plotly\'],'
-            'function(plotly) {{window.Plotly=plotly;}});'
+            'function(plotly) {{window._Plotly=plotly;}});'
             '}}'
             '</script>'
         ).format(win_config=_window_plotly_config,
@@ -217,12 +217,12 @@ def init_notebook_mode(connected=False):
             '{win_config}'
             '{mathjax_config}'
             '<script type=\'text/javascript\'>'
-            'if(!window.Plotly){{'
+            'if(!window._Plotly){{'
             'define(\'plotly\', function(require, exports, module) {{'
             '{script}'
             '}});'
             'require([\'plotly\'], function(Plotly) {{'
-            'window.Plotly = Plotly;'
+            'window._Plotly = Plotly;'
             '}});'
             '}}'
             '</script>'

plotly/tests/test_optional/test_jupyter/js_tests/connected_false.js
@@ -24,7 +24,7 @@ test('should inject raw plotly.js code into DOM', function(t) {
     nodes = Array.prototype.slice.call(nodes, 0, 10);
 
     var results = nodes.filter(function(node) {
-        return node.innerHTML.substr(0, 19) === 'if(!window.Plotly){';
+        return node.innerHTML.substr(0, 20) === 'if(!window._Plotly){';
     });
 
     t.equal(results.length, 1);

commit 497f3edd56b7306cbed2ad8df73d679a2c784634

Fix iplot resize in classic notebook that was broken by
https://github.com/plotly/plotly.py/pull/1250

plotly/offline/offline.py
@@ -98,13 +98,13 @@ def get_plotlyjs():
     return plotlyjs
 
 
-def _build_resize_script(plotdivid):
+def _build_resize_script(plotdivid, plotly_root='Plotly'):
     resize_script = (
         '<script type="text/javascript">'
         'window.addEventListener("resize", function(){{'
-        'Plotly.Plots.resize(document.getElementById("{id}"));}});'
+        '{plotly_root}.Plots.resize(document.getElementById("{id}"));}});'
         '</script>'
-    ).format(id=plotdivid)
+    ).format(plotly_root=plotly_root, id=plotdivid)
     return resize_script
 
 
@@ -450,7 +450,8 @@ def iplot(figure_or_data, show_link=True, link_text='Export to plot.ly',
         )
         resize_script = ''
         if width == '100%' or height == '100%':
-            resize_script = _build_resize_script(plotdivid)
+            resize_script = _build_resize_script(
+                plotdivid, 'window._Plotly')
 
         display_bundle['text/html'] = plot_html + resize_script
         display_bundle['text/vnd.plotly.v1+html'] = plot_html + resize_script