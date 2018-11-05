42b1184bfa373d2884f56da33b348a09f5b42eb3
Jon Mease <jon.mease@gmail.com>
2018-11-05 08:24:29 -0500

    Work around ""[Error 35] Resource unavailable" on Python 2.7

--- .circleci/create_conda_packages.sh
+++ .circleci/create_conda_packages.sh
@@ -1,8 +1,11 @@
 # Install conda dependencies
 conda install -y conda-build conda-verify zip
 
+# Make artifacts directory (will be uploaded to CircleCI after build)
+mkdir artifacts
+
 # Perform build
-conda build --python $PYTHON_VERSION recipe/
+conda build --python $PYTHON_VERSION recipe/ > conda-build.out.txt 2>&1
 
 # Convert to other architectures
 mkdir -p ./conda_packages_${PYTHON_VERSION}/linux-64/
@@ -13,5 +16,4 @@ conda convert -p win-64 ./conda_packages_${PYTHON_VERSION}/linux-64/plotly-*.tar
 conda convert -p win-32 ./conda_packages_${PYTHON_VERSION}/linux-64/plotly-*.tar.bz2 -o ./conda_packages_${PYTHON_VERSION}/
 
 # zip up packages into artifacts directory
-mkdir artifacts
 zip -r artifacts/conda_packages_${PYTHON_VERSION}.zip ./conda_packages_${PYTHON_VERSION}/
