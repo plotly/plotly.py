ba714ae15cd2fab619f5fa4f613587445ff87432
Adam Kulidjian <adam.kulidjian@gmail.com>
2018-11-22 13:38:29 -0500

    copied convert_dict_colors_to_same_type over to utils from colors module

--- plotly/figure_factory/utils.py
+++ plotly/figure_factory/utils.py
@@ -597,6 +597,45 @@ def convert_colors_to_same_type(colors, colortype='rgb', scale=None,
                                      'for your colortype variable.')
 
 
+def convert_dict_colors_to_same_type(colors_dict, colortype='rgb'):
+    """
+    Converts a colors in a dictioanry of colors to the specified color type
+
+    :param (dict) colors_dict: a dictioanry whose values are single colors
+    """
+    for key in colors_dict:
+        if '#' in colors_dict[key]:
+            colors_dict[key] = color_parser(
+                colors_dict[key], hex_to_rgb
+            )
+            colors_dict[key] = color_parser(
+                colors_dict[key], label_rgb
+            )
+
+        elif isinstance(colors_dict[key], tuple):
+            colors_dict[key] = color_parser(
+                colors_dict[key], convert_to_RGB_255
+            )
+            colors_dict[key] = color_parser(
+                colors_dict[key], label_rgb
+            )
+
+    if colortype == 'rgb':
+        return colors_dict
+    elif colortype == 'tuple':
+        for key in colors_dict:
+            colors_dict[key] = color_parser(
+                colors_dict[key], unlabel_rgb
+            )
+            colors_dict[key] = color_parser(
+                colors_dict[key], unconvert_from_RGB_255
+            )
+        return colors_dict
+    else:
+        raise exceptions.PlotlyError('You must select either rgb or tuple '
+                                     'for your colortype variable.')
+
+
 def colorscale_to_colors(colorscale):
     """
     Extracts the colors from colorscale as a list
