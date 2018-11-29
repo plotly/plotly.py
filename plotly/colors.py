b6c61c10d368da21280e21606bb6b47097af9ed8
Adam Kulidjian <adam.kulidjian@gmail.com>
2018-11-29 12:55:18 -0500

    move from __future__... import at top of file

--- plotly/colors.py
+++ plotly/colors.py
@@ -1,28 +1,7 @@
-a = ['DEFAULT_PLOTLY_COLORS',
- 'Number',
- 'PLOTLY_SCALES',
- 'absolute_import',
- 'color_parser',
- 'colorscale_to_colors',
- 'colorscale_to_scale',
- 'convert_colors_to_same_type',
- 'convert_colorscale_to_rgb',
- 'convert_dict_colors_to_same_type',
- 'convert_to_RGB_255',
- 'decimal',
- 'exceptions',
- 'find_intermediate_color',
- 'hex_to_rgb',
- 'label_rgb',
- 'make_colorscale',
- 'n_colors',
- 'unconvert_from_RGB_255',
- 'unlabel_rgb',
- 'validate_colors',
- 'validate_scale_values']
-
 from __future__ import absolute_import
+
 from numbers import Number
+
 from figure_factory.utils import (
 	DEFAULT_PLOTLY_COLORS, PLOTLY_SCALES, color_parser,
 	colorscale_to_colors,
@@ -40,4 +19,5 @@ from figure_factory.utils import (
  unconvert_from_RGB_255,
  unlabel_rgb,
  validate_colors,
- validate_scale_values)
+ validate_scale_values
+ )
