a51730bc84ff666c93b75603fd7c1b1be47b5d14
Jon Mease <jon.mease@gmail.com>
2019-03-22 06:50:06 -0400

    Add extract_chart_studio to v4 future group

--- _plotly_future_/v4.py
+++ _plotly_future_/v4.py
@@ -1,2 +1,3 @@
 from __future__ import absolute_import
-from _plotly_future_ import renderer_defaults, template_defaults
+from _plotly_future_ import (
+    renderer_defaults, template_defaults, extract_chart_studio)
