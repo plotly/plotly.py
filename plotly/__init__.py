6071473c185fb3fa59fbaa862dbacfc7ac34ad82
Jon Mease <jon.mease@gmail.com>
2019-03-26 18:20:26 -0400

    Remove non_recursive_imports flag for this PR

--- plotly/__init__.py
+++ plotly/__init__.py
@@ -29,15 +29,14 @@ Modules:
 from __future__ import absolute_import
 from _plotly_future_ import _future_flags
 
-if 'non_recursive_imports' not in _future_flags:
-    from plotly import (
-        graph_objs,
-        tools,
-        utils,
-        offline,
-        colors,
-        io
-    )
+from plotly import (
+    graph_objs,
+    tools,
+    utils,
+    offline,
+    colors,
+    io
+)
 
 from plotly.version import __version__
 from ._version import get_versions
@@ -45,8 +44,7 @@ __version__ = get_versions()['version']
 del get_versions
 
 
-if ('extract_chart_studio' not in _future_flags
-        and 'non_recursive_imports' not in _future_flags):
+if 'extract_chart_studio' not in _future_flags:
     from plotly import (
         plotly,
         dashboard_objs,
