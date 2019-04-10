2263e4e3287ec9f52b50f6b49d87baafdd9538c3
Nicolas Kruchten <nicolas@kruchten.com>
2019-04-10 18:24:00 -0400

    clean up PyPI metadata (#1480)

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ def plotly_js_version():
 
 
 def readme():
-    with open('README.rst') as f:
+    with open('README.md') as f:
         return f.read()
 
 
@@ -391,20 +391,23 @@ setup(name='plotly',
       use_2to3=False,
       author='Chris P',
       author_email='chris@plot.ly',
-      maintainer='Chris P',
-      maintainer_email='chris@plot.ly',
+      maintainer='Jon Mease',
+      maintainer_email='jon@plot.ly',
       url='https://plot.ly/python/',
-      description="Python plotting library for collaborative, "
-                  "interactive, publication-quality graphs.",
+      project_urls={"Github": "https://github.com/plotly/plotly.py"},
+      description="An open-source, interactive graphing library for Python",
       long_description=readme(),
+      long_description_content_type="text/markdown",
       classifiers=[
-          'Development Status :: 4 - Beta',
+          'Development Status :: 5 - Production/Stable',
           'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.3',
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
           'Topic :: Scientific/Engineering :: Visualization',
       ],
       license='MIT',
