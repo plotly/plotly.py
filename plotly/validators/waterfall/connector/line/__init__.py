59adb8d23d7b8320d7e901dc37a20a03fd89c73b
Jon Mease <jon.mease@gmail.com>
2019-04-19 09:07:02 -0400

    Update to plotly.js 1.47.3 (#1529)

--- plotly/validators/waterfall/connector/line/__init__.py
+++ plotly/validators/waterfall/connector/line/__init__.py
@@ -15,7 +15,7 @@ class WidthValidator(_plotly_utils.basevalidators.NumberValidator):
             plotly_name=plotly_name,
             parent_name=parent_name,
             anim=kwargs.pop('anim', True),
-            edit_type=kwargs.pop('edit_type', 'style'),
+            edit_type=kwargs.pop('edit_type', 'plot'),
             min=kwargs.pop('min', 0),
             role=kwargs.pop('role', 'style'),
             **kwargs
