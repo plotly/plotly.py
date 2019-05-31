56ffe91cb54282e6c2a6d9fe1477859e4cf591fd
Jon Mease <jon.mease@gmail.com>
2019-05-31 14:05:03 -0400

    Python 2 docstring compatibility

--- plotly/_docstring_gen.py
+++ plotly/_docstring_gen.py
@@ -2,20 +2,27 @@ from __future__ import absolute_import
 import re as _re
 import plotly.io as pio
 from plotly.basedatatypes import BaseFigure as _BaseFigure
-
+import sys
 
 # Perform docstrings generation
-def _remove_fig_docstr(docstr):
+def copy_doc_without_fig(from_fn, to_method):
     """
-    Remove the fig argument from the input docstring and return the result
+    Copy docstring from a plotly.io function to a Figure method, removing the
+    fig argument docstring in the process
     """
-    return _re.sub(r' {4}fig:(?:.*?\n)*? {4}(\w+)', r'    \1', docstr)
+    docstr = _re.sub(r' {4}fig:(?:.*?\n)*? {4}(\w+)', r'    \1',
+                     from_fn.__doc__)
+    if sys.version_info[0] < 3:
+        to_method.__func__.__doc__ = docstr
+    else:
+        to_method.__doc__ = docstr
 
 
-_BaseFigure.show.__doc__ = _remove_fig_docstr(pio.show.__doc__)
-_BaseFigure.to_json.__doc__ = _remove_fig_docstr(pio.to_json.__doc__)
-_BaseFigure.write_json.__doc__ = _remove_fig_docstr(pio.write_json.__doc__)
-_BaseFigure.to_html.__doc__ = _remove_fig_docstr(pio.to_html.__doc__)
-_BaseFigure.write_html.__doc__ = _remove_fig_docstr(pio.write_html.__doc__)
-_BaseFigure.to_image.__doc__ = _remove_fig_docstr(pio.to_image.__doc__)
-_BaseFigure.write_image.__doc__ = _remove_fig_docstr(pio.write_image.__doc__)
+copy_doc_without_fig(pio.show, _BaseFigure.show)
+copy_doc_without_fig(pio.to_json, _BaseFigure.to_json)
+copy_doc_without_fig(pio.write_json, _BaseFigure.write_json)
+copy_doc_without_fig(pio.to_html, _BaseFigure.to_html)
+copy_doc_without_fig(pio.write_html, _BaseFigure.write_html)
+copy_doc_without_fig(pio.to_image, _BaseFigure.to_image)
+copy_doc_without_fig(pio.write_image, _BaseFigure.write_image)
+
