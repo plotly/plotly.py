e3390a02b49d193bf941c0312294bda2c37cb3b6
Jon Mease <jon.mease@gmail.com>
2019-06-13 15:09:44 -0400

    Remove namespace package path extension
    This was added back when figure_factory was a separate package.

--- packages/python/plotly/plotly/__init__.py
+++ packages/python/plotly/plotly/__init__.py
@@ -27,10 +27,6 @@ Modules:
 """
 from __future__ import absolute_import
 
-# https://packaging.python.org/guides/packaging-namespace-packages/
-#         pkgutil-style-namespace-packages
-__path__ = __import__('pkgutil').extend_path(__path__, __name__)
-
 from plotly import (
     graph_objs,
     tools,
