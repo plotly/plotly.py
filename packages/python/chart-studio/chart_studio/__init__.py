27300592627b077be5191e1f0b0755a72f22ebca
Jon Mease <jon.mease@gmail.com>
2019-06-15 13:56:25 -0400

    V4 alpha 2 release (#1620)
    * package metadata updates
    * conda recipe updates
    * package-lock update
    * Add plotly express datasets as package data
    * chart_studio -> chart-studio in error message
    * auto-import tools below chart_studio

--- packages/python/chart-studio/chart_studio/__init__.py
+++ packages/python/chart-studio/chart_studio/__init__.py
@@ -1,2 +1,8 @@
 from __future__ import absolute_import
-from chart_studio import (plotly, dashboard_objs, grid_objs, session)
+from chart_studio import (
+    plotly,
+    dashboard_objs,
+    grid_objs,
+    session,
+    tools,
+)
