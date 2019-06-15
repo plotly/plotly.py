17cf071147e9682291f6ca3371a6ba9f1aa38fa7
Jon Mease <jon.mease@gmail.com>
2019-06-15 13:56:25 -0400

    V4 alpha 2 release (#1620)
    * package metadata updates
    * conda recipe updates
    * package-lock update
    * Add plotly express datasets as package data
    * chart_studio -> chart-studio in error message
    * auto-import tools below chart_studio

--- packages/python/plotly/plotly/data/__init__.py
+++ packages/python/plotly/plotly/data/__init__.py
@@ -75,4 +75,10 @@ def _get_dataset(d):
     import pandas
     import os
 
-    return pandas.read_csv(os.path.join(os.path.dirname(__file__), d + ".csv.gz"))
+    return pandas.read_csv(
+        os.path.join(
+            os.path.dirname(os.path.dirname(__file__)),
+            'package_data',
+            'datasets',
+            d + ".csv.gz")
+    )
