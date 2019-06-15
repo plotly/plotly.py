72c69d7e15ba6f81281b16f0330835d65f9ec3ec
Jon Mease <jon.mease@gmail.com>
2019-06-15 12:02:45 -0400

    V4 release instructions (#1618)
    Initial work on updating the release process for the three separate distribution packages

---
+++ setup_submodules.py
@@ -0,0 +1,58 @@
+import os
+import shutil
+import subprocess
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+if __name__ == '__main__':
+    # Init submodules
+    subprocess.check_output(['git', 'submodule', 'init'])
+
+    # Update submodules
+    subprocess.check_output(['git', 'submodule', 'update'])
+
+    # Replace mplexporter directory
+    mpl_dst = os.path.join(
+            here,
+            'packages',
+            'python',
+            'plotly',
+            'plotly',
+            'matplotlylib',
+            'mplexporter',
+    )
+
+    shutil.rmtree(mpl_dst, ignore_errors=True)
+
+    shutil.copytree(
+        os.path.join(
+            here,
+            'submodules',
+            'mplexporter',
+            'mplexporter',
+        ),
+        mpl_dst,
+    )
+
+    # Replace chunked_requests directory
+    chunked_dst = os.path.join(
+            here,
+            'packages',
+            'python',
+            'chart-studio',
+            'chart_studio',
+            'plotly',
+            'chunked_requests',
+        )
+
+    shutil.rmtree(chunked_dst, ignore_errors=True)
+
+    shutil.copytree(
+        os.path.join(
+            here,
+            'submodules',
+            'chunked_requests',
+            'chunked_requests',
+        ),
+        os.path.join(chunked_dst),
+    )
