9d8746cbbc369b63c89e8dc118b66341350123ef
Jon Mease <jon.mease@gmail.com>
2019-06-17 14:08:41 -0400

    Revert figures to static images on WebGL context loss

--- packages/javascript/jupyterlab-plotly/src/lib.d.ts
+++ packages/javascript/jupyterlab-plotly/src/lib.d.ts
@@ -3,4 +3,8 @@ declare module 'plotly.js/dist/plotly' {
   export type Frame = { [key: string]: any };
   export function addFrames(root: Plotly.Root, frames: Frame[]): Promise<void>;
   export function animate(root: Plotly.Root): void;
+
+  export interface PlotlyHTMLElement extends HTMLElement {
+    on(event: 'plotly_webglcontextlost', callback: () => void): void;
+  }
 }
