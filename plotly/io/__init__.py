0f8e2a578dc213ed40cfdf2e5e5b027a6085f73d
Emmanuelle Gouillart <emmanuelle.gouillart@nsup.org>
2019-07-03 15:59:54 +0200

    Scraper function for sphinx-gallery, in plotly.io (#1577)
    * scraper function for sphinx-gallery, in plotly.io.  scraper function + skeleton of sphinx doc with minimal gallery (two examples)
    
    * added psutil dep to tox.ini
    
    * python 2 compatibility
    
    * added orca dep to package.json for tests
    
    * removed typo
    
    * corrected bug
    
    * python 2 compatibility
    
    * added electron
    
    * try to configure orca
    
    * Moved test file so that it's tested by the orca build only.
    
    * use custom renderer for sphinx-gallery
    
    * removed sphinx files which are now in plotly-sphinx-gallery
    
    + changed name of renderer sphinx -> sphinx-gallery, and changed wording
    of documentation
    
    * Modified test with new renderer name
    
    * try to fix py2 compatibility
    
    * write_html instead of offline.plot
    
    * change import order

--- plotly/io/__init__.py
+++ plotly/io/__init__.py
@@ -5,8 +5,9 @@ from ._json import to_json, from_json, read_json, write_json
 
 from ._templates import templates, to_templated
 
+from ._html import to_html, write_html
+
 from ._renderers import renderers, show
 
 from . import base_renderers
 
-from ._html import to_html, write_html
