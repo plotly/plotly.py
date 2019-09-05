76c7c61bc4c25c2c940ce80da6c827b2370bf34b
Emmanuelle Gouillart <emma@plot.ly>
2019-09-05 14:54:45 -0400

    added minimal test for px

---
+++ packages/python/plotly/plotly/tests/test_core/test_px/test_px.py
@@ -0,0 +1,13 @@
+import plotly.express as px
+import numpy as np
+
+def test_scatter():
+    iris = px.data.iris()
+    fig = px.scatter(iris, x="sepal_width", y="sepal_length")
+    assert fig.data[0].type == 'scatter'
+    assert np.all(fig.data[0].x == iris.sepal_width)
+    assert np.all(fig.data[0].y == iris.sepal_length)
+    # test defaults
+    assert fig.data[0].mode == 'markers'
+
+
