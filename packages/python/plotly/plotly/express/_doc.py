d73e34e37d33e51b785db01b6c0b9d3884677e68
Nicolas Kruchten <nicolas@plot.ly>
2019-10-21 14:02:35 -0700

    initial build-out of facet wrapping

--- packages/python/plotly/plotly/express/_doc.py
+++ packages/python/plotly/plotly/express/_doc.py
@@ -183,6 +183,11 @@ docs = dict(
         colref_desc,
         "Values from this column or array_like are used to assign marks to facetted subplots in the horizontal direction.",
     ],
+    facet_col_wrap=[
+        "int",
+        "Wrap the column variable at this width, so that the column facets span multiple rows.",
+        "Forced to 0 if `facet_row` or a `marginal` is set.",
+    ],
     animation_frame=[
         colref_type,
         colref_desc,
