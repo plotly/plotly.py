09c255ebe9764818080bb6169c4034dcf0e71ef6
joelostblom <joel.ostblom@gmail.com>
2019-11-04 11:24:31 +0100

    Document box and whisker ranges

--- packages/python/plotly/plotly/express/_chart_types.py
+++ packages/python/plotly/plotly/express/_chart_types.py
@@ -461,6 +461,11 @@ def box(
     """
     In a box plot, rows of `data_frame` are grouped together into a
     box-and-whisker mark to visualize their distribution.
+    Each box spans from quartile 1 (Q1) to quartile 3 (Q3). The
+    second quartile (Q2) is marked by a line inside the box. By
+    default, the whiskers correspond to the box' edges +/- 1.5
+    times the interquartile range (IQR: Q3-Q1), see "points" for
+    other options.
     """
     return make_figure(
         args=locals(),
