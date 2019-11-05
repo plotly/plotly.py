10271ff7d2830db5cd4f32cf8565ea5eef96b7cf
joelostblom <joel.ostblom@gmail.com>
2019-11-05 10:50:24 +0100

    Use the default template's background for swatches
    Instead of always using the plotly template

--- packages/python/plotly/_plotly_utils/colors/_swatches.py
+++ packages/python/plotly/_plotly_utils/colors/_swatches.py
@@ -2,7 +2,8 @@ def _swatches(module_names, module_contents):
     """
     Returns:
         A `Figure` object. This figure demonstrates the color scales and
-        sequences in this module, as stacked bar charts.
+        sequences in this module as stacked bar charts against the default
+        template's background.
     """
     import plotly.graph_objs as go
 
@@ -28,7 +29,6 @@ def _swatches(module_names, module_contents):
             title=module_names,
             barmode="stack",
             barnorm="fraction",
-            template="plotly",
             bargap=0.5,
             showlegend=False,
             xaxis=dict(range=[-0.02, 1.02], showticklabels=False, showgrid=False),
