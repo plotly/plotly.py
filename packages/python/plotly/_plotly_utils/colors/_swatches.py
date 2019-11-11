5dd6f7b228af9f6e72c4de8f0d44d76b253b6e52
Nicolas Kruchten <nicolas@plot.ly>
2019-11-11 09:31:08 -0500

    Apply suggestions from code review
    Co-Authored-By: Joel Ostblom <joelostblom@users.noreply.github.com>

--- packages/python/plotly/_plotly_utils/colors/_swatches.py
+++ packages/python/plotly/_plotly_utils/colors/_swatches.py
@@ -2,13 +2,11 @@ def _swatches(module_names, module_contents, template=None):
     """
     Parameters
     ----------
-
     template : str or dict or plotly.graph_objects.layout.Template instance
         The figure template name or definition.
 
     Returns
     -------
-
     fig : graph_objects.Figure containing the displayed image
         A `Figure` object. This figure demonstrates the color scales and
         sequences in this module, as stacked bar charts.
