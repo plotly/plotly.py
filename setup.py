2020dc53ae12c011bf6272b799647d563282a27f
Nicolas Kruchten <nicolas@plot.ly>
2020-02-24 21:31:55 -0800

    dummy setup.py for Github 'Used By'

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+raise Exception(
+    """
+
+    This file exists to set up the Github "Used By" counter properly and is not meant to be run.
+
+    Please refer to ./packages/python/plotly/setup.py instead.
+    """
+)
+
+setup(name="plotly")
