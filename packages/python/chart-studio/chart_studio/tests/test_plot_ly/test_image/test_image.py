13a33172a7344f621f93b9b4ce2bf663f5f8cbc9
Emmanuelle Gouillart <emma@plot.ly>
2020-03-03 14:27:04 -0500

    fixture

--- packages/python/chart-studio/chart_studio/tests/test_plot_ly/test_image/test_image.py
+++ packages/python/chart-studio/chart_studio/tests/test_plot_ly/test_image/test_image.py
@@ -10,17 +10,27 @@ import pytest
 import _plotly_utils.exceptions
 from chart_studio.plotly import plotly as py
 
-py.sign_in("PlotlyImageTest", "786r5mecv0")
-data = [{"x": [1, 2, 3], "y": [3, 1, 6]}]
+from chart_studio.tests.utils import PlotlyTestCase
 
 
-@pytest.mark.parametrize('image_format', ("png", "jpeg", "pdf", "svg", "emf"))
-@pytest.mark.parametrize('width', (None, 300))
-@pytest.mark.parametrize('height', (None, 300))
-@pytest.mark.parametrize('scale', (None, 5))
-def test_image_get_returns_valid_image_test(image_format, width, height, scale):
+@pytest.fixture
+def setup_image():
+    py.sign_in("PlotlyImageTest", "786r5mecv0")
+    data = [{"x": [1, 2, 3], "y": [3, 1, 6]}]
+
+    return data
+
+
+@pytest.mark.parametrize("image_format", ("png", "jpeg", "pdf", "svg", "emf"))
+@pytest.mark.parametrize("width", (None, 300))
+@pytest.mark.parametrize("height", (None, 300))
+@pytest.mark.parametrize("scale", (None, 3))
+def test_image_get_returns_valid_image_test(
+    setup_image, image_format, width, height, scale
+):
     # TODO: better understand why this intermittently fails. See #649
-    num_attempts = 5
+    data = setup_image
+    num_attempts = 2
     for i in range(num_attempts):
         if i > 0:
             warnings.warn("image test intermittently failed, retrying...")
@@ -34,19 +44,17 @@ def test_image_get_returns_valid_image_test(image_format, width, height, scale):
                 raise
 
 
-@pytest.mark.parametrize('image_format', ("png", "jpeg", "pdf", "svg", "emf"))
-@pytest.mark.parametrize('width', (None, 300))
-@pytest.mark.parametrize('height', (None, 300))
-@pytest.mark.parametrize('scale', (None, 5))
-def test_image_save_as_saves_valid_image(image_format, width, height, scale):
+@pytest.mark.parametrize("image_format", ("png", "jpeg", "pdf", "svg", "emf"))
+@pytest.mark.parametrize("width", (None, 300))
+@pytest.mark.parametrize("height", (None, 300))
+@pytest.mark.parametrize("scale", (None, 3))
+def test_image_save_as_saves_valid_image(
+    setup_image, image_format, width, height, scale
+):
+    data = setup_image()
     f, filename = tempfile.mkstemp(".{}".format(image_format))
     py.image.save_as(
-        data,
-        filename,
-        format=image_format,
-        width=width,
-        height=height,
-        scale=scale,
+        data, filename, format=image_format, width=width, height=height, scale=scale,
     )
     if image_format in ["png", "jpeg"]:
         assert imghdr.what(filename) == image_format
@@ -54,4 +62,3 @@ def test_image_save_as_saves_valid_image(image_format, width, height, scale):
         assert os.path.getsize(filename) > 0
 
     os.remove(filename)
-
