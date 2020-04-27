08b9d97cb2e198df35fcd0fbc9f7fc34cb51e937
Emmanuelle Gouillart <emma@plot.ly>
2020-04-27 18:03:33 +0200

    extended test to lowess, and more precise check of attribute length

--- packages/python/plotly/plotly/tests/test_core/test_px/test_trendline.py
+++ packages/python/plotly/plotly/tests/test_core/test_px/test_trendline.py
@@ -6,7 +6,10 @@ def test_trendline_nan_values():
     df = px.data.gapminder().query("continent == 'Oceania'")
     start_date = 1970
     df["pop"][df["year"] < start_date] = np.nan
-    fig = px.scatter(df, x="year", y="pop", color="country", trendline="ols")
-    country_numbers = len(fig["data"]) // 2
-    for trendline in fig["data"][1::2]:
-        assert trendline.x[0] >= start_date
+    modes = ["ols", "lowess"]
+    for mode in modes:
+        fig = px.scatter(df, x="year", y="pop", color="country", trendline=mode)
+        country_numbers = len(fig["data"]) // 2
+        for trendline in fig["data"][1::2]:
+            assert trendline.x[0] >= start_date
+            assert len(trendline.x) == len(trendline.y)
