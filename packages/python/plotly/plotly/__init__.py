0b442b1894bd77851e563d539a949f7cea7087b8
Nicolas Kruchten <nicolas@plot.ly>
2020-05-16 21:40:59 -0400

    cleaner pandas backend

--- packages/python/plotly/plotly/__init__.py
+++ packages/python/plotly/plotly/__init__.py
@@ -75,3 +75,75 @@ else:
         ],
         [".version.__version__"],
     )
+
+
+def plot(data_frame, kind, **kwargs):
+    """
+    Pandas plotting backend function, not meant to be called directly.
+    To activate, set pandas.options.plotting.backend="plotly.express.pandas_backend"
+    See https://github.com/pandas-dev/pandas/blob/master/pandas/plotting/__init__.py
+    """
+    from .express import scatter, line, area, bar, box, histogram
+
+    if kind == "scatter":
+        new_kwargs = {k: kwargs[k] for k in kwargs if k not in ["s", "c"]}
+        return scatter(data_frame, **new_kwargs)
+    if kind == "line":
+        return line(data_frame, **kwargs)
+    if kind == "area":
+        return area(data_frame, **kwargs)
+    if kind == "bar":
+        return bar(data_frame, **kwargs)
+    if kind == "barh":
+        return bar(data_frame, orientation="h", **kwargs)
+    if kind == "box":
+        new_kwargs = {k: kwargs[k] for k in kwargs if k not in ["by"]}
+        return box(data_frame, **new_kwargs)
+    if kind in "hist":
+        new_kwargs = {k: kwargs[k] for k in kwargs if k not in ["by", "bins"]}
+        return histogram(data_frame, **new_kwargs)
+    raise NotImplementedError(
+        "The plotly.express backend doesn't yet support kind='%s'" % kind
+    )
+
+
+def boxplot_frame(data_frame, **kwargs):
+    """
+    Pandas plotting backend function, not meant to be called directly.
+    To activate, set pandas.options.plotting.backend="plotly.express.pandas_backend"
+    See https://github.com/pandas-dev/pandas/blob/master/pandas/plotting/__init__.py
+    """
+    from .express import box
+
+    skip = ["by", "column", "ax", "fontsize", "rot", "grid", "figsize", "layout"]
+    skip += ["return_type"]
+    new_kwargs = {k: kwargs[k] for k in kwargs if k not in skip}
+    return box(data_frame, **new_kwargs)
+
+
+def hist_frame(data_frame, **kwargs):
+    """
+    Pandas plotting backend function, not meant to be called directly.
+    To activate, set pandas.options.plotting.backend="plotly.express.pandas_backend"
+    See https://github.com/pandas-dev/pandas/blob/master/pandas/plotting/__init__.py
+    """
+    from .express import histogram
+
+    skip = ["column", "by", "grid", "xlabelsize", "xrot", "ylabelsize", "yrot"]
+    skip += ["ax", "sharex", "sharey", "figsize", "layout", "bins"]
+    new_kwargs = {k: kwargs[k] for k in kwargs if k not in skip}
+    return histogram(data_frame, **new_kwargs)
+
+
+def hist_series(data_frame, **kwargs):
+    """
+    Pandas plotting backend function, not meant to be called directly.
+    To activate, set pandas.options.plotting.backend="plotly.express.pandas_backend"
+    See https://github.com/pandas-dev/pandas/blob/master/pandas/plotting/__init__.py
+    """
+    from .express import histogram
+
+    skip = ["by", "grid", "xlabelsize", "xrot", "ylabelsize", "yrot", "ax"]
+    skip += ["figsize", "bins"]
+    new_kwargs = {k: kwargs[k] for k in kwargs if k not in skip}
+    return histogram(data_frame, **new_kwargs)
