9236eae7e94918e7a1c79ecaded3bffae53c4ab2
Nicholas Esterer <nicholas.esterer@plot.ly>
2020-06-19 11:42:42 -0400

    Added hline etc. tests for empty subplots and single plot

--- packages/python/plotly/plotly/tests/test_core/test_update_objects/test_paper_span_shapes.py
+++ packages/python/plotly/plotly/tests/test_core/test_update_objects/test_paper_span_shapes.py
@@ -11,12 +11,19 @@ def subplot_fig_fixture():
         px.data.tips(), x="total_bill", y="tip", facet_row="smoker", facet_col="sex"
     )
     # explicitly set domains so that we know what they will be
-    for ax in ["x", "y"]:
-        for n in range(1, 5):
-            axname = ax + "axis" + str(n) if n != 1 else ax + "axis"
-            start = ((n - 1) // 2) * 0.5
-            stop = start + 0.4
-            fig["layout"][axname]["domain"] = [start, stop]
+    # these could be anything but we make them plausible
+    for ax, dom in zip(
+        [("x", ""), ("x", "2"), ("x", "3"), ("x", "4")],
+        [(0, 0.4), (0.5, 0.9), (0, 0.4), (0.5, 0.9)],
+    ):
+        axname = ax[0] + "axis" + ax[1]
+        fig["layout"][axname]["domain"] = dom
+    for ax, dom in zip(
+        [("y", ""), ("y", "2"), ("y", "3"), ("y", "4")],
+        [(0, 0.4), (0, 0.4), (0.5, 0.9), (0.5, 0.9)],
+    ):
+        axname = ax[0] + "axis" + ax[1]
+        fig["layout"][axname]["domain"] = dom
     return fig
 
 
@@ -25,6 +32,21 @@ def subplot_empty_traces_fig_fixture():
     fig = px.scatter(
         px.data.tips(), x="total_bill", y="tip", facet_row="day", facet_col="time"
     )
+    # explicitly set domains so that we know what they will be
+    # these could be anything but we make them plausible
+    for ax, dom in zip(
+        [("x", "")] + [("x", str(n)) for n in range(2, 9)],
+        [((n % 2) * 0.5, (n % 2) * 0.5 + 0.4) for n in range(8)],
+    ):
+        axname = ax[0] + "axis" + ax[1]
+        fig["layout"][axname]["domain"] = dom
+    for ax, dom in zip(
+        [("y", "")] + [("y", str(n)) for n in range(2, 9)],
+        [((n // 2) * 0.25, (n // 2) * 0.25 + 0.2) for n in range(8)],
+    ):
+        axname = ax[0] + "axis" + ax[1]
+        fig["layout"][axname]["domain"] = dom
+    return fig
 
 
 @pytest.fixture
@@ -50,6 +72,15 @@ def _cmp_partial_dict(a, b):
         except KeyError:
             return False
     return ret
+
+
+def _check_figure_shapes(test_input, expected, fig):
+    f, kwargs = test_input
+    f(fig, **kwargs)
+    ret = True
+    for s, d in zip(fig.layout.shapes, expected):
+        ret &= _cmp_partial_dict(s, d)
+    assert ret
 
 
 @pytest.mark.parametrize(
@@ -218,9 +249,109 @@ def _cmp_partial_dict(a, b):
     ],
 )
 def test_add_span_shape(test_input, expected, subplot_fig_fixture):
-    f, kwargs = test_input
-    f(subplot_fig_fixture, **kwargs)
-    ret = True
-    for s, d in zip(subplot_fig_fixture.layout.shapes, expected):
-        ret &= _cmp_partial_dict(s, d)
-    assert ret
+    _check_figure_shapes(test_input, expected, subplot_fig_fixture)
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    # test_input: (function,kwargs)
+    # expected: list of dictionaries with key:value pairs we expect in the added shapes
+    [
+        (
+            (go.Figure.add_vline, dict(x=20, row=[3, 4], col="all")),
+            [
+                {
+                    "type": "line",
+                    "x0": 20,
+                    "x1": 20,
+                    "xref": "x5",
+                    "y0": 0.5,
+                    "y1": 0.7,
+                    "yref": "paper",
+                },
+                {
+                    "type": "line",
+                    "x0": 20,
+                    "x1": 20,
+                    "xref": "x7",
+                    "y0": 0.75,
+                    "y1": 0.95,
+                    "yref": "paper",
+                },
+            ],
+        ),
+        (
+            (
+                go.Figure.add_vline,
+                dict(x=20, row="all", col=2, exclude_subplots_without_data=False),
+            ),
+            [
+                {
+                    "type": "line",
+                    "x0": 20,
+                    "x1": 20,
+                    "xref": "x2",
+                    "y0": 0.0,
+                    "y1": 0.2,
+                    "yref": "paper",
+                },
+                {
+                    "type": "line",
+                    "x0": 20,
+                    "x1": 20,
+                    "xref": "x4",
+                    "y0": 0.25,
+                    "y1": 0.45,
+                    "yref": "paper",
+                },
+                {
+                    "type": "line",
+                    "x0": 20,
+                    "x1": 20,
+                    "xref": "x6",
+                    "y0": 0.5,
+                    "y1": 0.7,
+                    "yref": "paper",
+                },
+                {
+                    "type": "line",
+                    "x0": 20,
+                    "x1": 20,
+                    "xref": "x8",
+                    "y0": 0.75,
+                    "y1": 0.95,
+                    "yref": "paper",
+                },
+            ],
+        ),
+    ],
+)
+def test_add_span_shape_no_empty_plot(
+    test_input, expected, subplot_empty_traces_fig_fixture
+):
+    _check_figure_shapes(test_input, expected, subplot_empty_traces_fig_fixture)
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    # test_input: (function,kwargs)
+    # expected: list of dictionaries with key:value pairs we expect in the added shapes
+    [
+        (
+            (go.Figure.add_hline, dict(y=6)),
+            [
+                {
+                    "type": "line",
+                    "x0": 0,
+                    "x1": 0.4,
+                    "xref": "paper",
+                    "y0": 6,
+                    "y1": 6,
+                    "yref": "y",
+                }
+            ],
+        )
+    ],
+)
+def test_non_subplot_add_span_shape(test_input, expected, non_subplot_fig_fixture):
+    _check_figure_shapes(test_input, expected, non_subplot_fig_fixture)
