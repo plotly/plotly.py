3e1cbd4e295030ff8185b41164dd4ebe187dd548
Jon Mease <jon.mease@gmail.com>
2020-07-08 07:08:49 -0400

    Added kaleido engine tests

---
+++ packages/python/plotly/plotly/tests/test_optional/test_kaleido/test_kaleido.py
@@ -0,0 +1,92 @@
+import plotly.io as pio
+import plotly.io.kaleido
+from unittest.mock import Mock
+from contextlib import contextmanager
+
+
+fig = {"layout": {"title": {"text": "figure title"}}}
+
+
+@contextmanager
+def mocked_scope():
+    # Code to acquire resource, e.g.:
+    scope_mock = Mock()
+    original_scope = pio._kaleido.scope
+    pio._kaleido.scope = scope_mock
+    try:
+        yield scope_mock
+    finally:
+        pio._kaleido.scope = original_scope
+
+
+def test_kaleido_engine_to_image():
+    with mocked_scope() as scope:
+        pio.to_image(fig, engine="kaleido", validate=False)
+
+    scope.transform.assert_called_with(
+        fig, format=None, width=None, height=None, scale=None
+    )
+
+
+def test_kaleido_engine_write_image():
+    writeable_mock = Mock()
+    with mocked_scope() as scope:
+        pio.write_image(fig, writeable_mock, engine="kaleido", validate=False)
+
+    scope.transform.assert_called_with(
+        fig, format=None, width=None, height=None, scale=None
+    )
+
+    writeable_mock.write.assert_called_once()
+
+
+def test_kaleido_engine_to_image_kwargs():
+    with mocked_scope() as scope:
+        pio.to_image(
+            fig,
+            format="pdf",
+            width=700,
+            height=600,
+            scale=2,
+            engine="kaleido",
+            validate=False,
+        )
+
+    scope.transform.assert_called_with(
+        fig, format="pdf", width=700, height=600, scale=2
+    )
+
+
+def test_kaleido_engine_write_image_kwargs():
+    writeable_mock = Mock()
+    with mocked_scope() as scope:
+        pio.write_image(
+            fig,
+            writeable_mock,
+            format="jpg",
+            width=700,
+            height=600,
+            scale=2,
+            engine="kaleido",
+            validate=False,
+        )
+
+    scope.transform.assert_called_with(
+        fig, format="jpg", width=700, height=600, scale=2
+    )
+
+    writeable_mock.write.assert_called_once()
+
+
+def test_image_renderer():
+    with mocked_scope() as scope:
+        pio.show(fig, renderer="svg", engine="kaleido", validate=False)
+
+    renderer = pio.renderers["svg"]
+    scope.transform.assert_called_with(
+        fig,
+        format="svg",
+        width=renderer.width,
+        height=renderer.height,
+        scale=renderer.scale,
+    )
