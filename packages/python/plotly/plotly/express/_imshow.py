135b01bd174a76cc9f634cbc9ac30d85b9e3ba82
Emmanuelle Gouillart <emma@plot.ly>
2020-11-24 21:50:47 +0100

    added example with sequence of images

--- packages/python/plotly/plotly/express/_imshow.py
+++ packages/python/plotly/plotly/express/_imshow.py
@@ -519,7 +519,9 @@ def imshow(
     # Now build figure
     col_labels = []
     if facet_col is not None:
-        slice_label = "slice" if labels.get("facet") is None else labels["facet"]
+        slice_label = (
+            "slice" if labels.get("facet_col") is None else labels["facet_col"]
+        )
         col_labels = ["%s = %d" % (slice_label, i) for i in facet_slices]
     fig = init_figure(args, "xy", [], nrows, ncols, col_labels, [])
     for attr_name in ["height", "width"]:
