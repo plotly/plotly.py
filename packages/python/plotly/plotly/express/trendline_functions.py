2d3e8b0b9aec836b9c45b2adbfebaeea97933a54
Nicolas Kruchten <nicolas@plot.ly>
2020-12-29 21:20:10 -0500

    tests for new trendlines

--- packages/python/plotly/plotly/express/trendline_functions.py
+++ packages/python/plotly/plotly/express/trendline_functions.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-def ols(options, x, y, x_label, y_label, non_missing):
+def ols(options, x_raw, x, y, x_label, y_label, non_missing):
     import statsmodels.api as sm
 
     add_constant = options.get("add_constant", True)
@@ -30,14 +30,14 @@ def ols(options, x, y, x_label, y_label, non_missing):
             fit_results.params[0],
         )
     elif not add_constant:
-        hover_header += "%s = %g* %s<br>" % (y_label, fit_results.params[0], x_label,)
+        hover_header += "%s = %g * %s<br>" % (y_label, fit_results.params[0], x_label,)
     else:
         hover_header += "%s = %g<br>" % (y_label, fit_results.params[0],)
     hover_header += "R<sup>2</sup>=%f<br><br>" % fit_results.rsquared
     return y_out, hover_header, fit_results
 
 
-def lowess(options, x, y, x_label, y_label, non_missing):
+def lowess(options, x_raw, x, y, x_label, y_label, non_missing):
     import statsmodels.api as sm
 
     frac = options.get("frac", 0.6666666)
@@ -46,13 +46,13 @@ def lowess(options, x, y, x_label, y_label, non_missing):
     return y_out, hover_header, None
 
 
-def ma(options, x, y, x_label, y_label, non_missing):
-    y_out = pd.Series(y, index=x).rolling(**options).mean()[non_missing]
-    hover_header = "<b>Moving Average trendline</b><br><br>"
+def ma(options, x_raw, x, y, x_label, y_label, non_missing):
+    y_out = pd.Series(y, index=x_raw).rolling(**options).mean()[non_missing]
+    hover_header = "<b>MA trendline</b><br><br>"
     return y_out, hover_header, None
 
 
-def ewm(options, x, y, x_label, y_label, non_missing):
-    y_out = pd.Series(y, index=x).ewm(**options).mean()[non_missing]
-    hover_header = "<b>EWM trendline</b><br><br>"
+def ewma(options, x_raw, x, y, x_label, y_label, non_missing):
+    y_out = pd.Series(y, index=x_raw).ewm(**options).mean()[non_missing]
+    hover_header = "<b>EWMA trendline</b><br><br>"
     return y_out, hover_header, None
--- packages/python/plotly/plotly/tests/test_optional/test_px/test_trendline.py
+++ packages/python/plotly/plotly/tests/test_optional/test_px/test_trendline.py
@@ -5,10 +5,27 @@ import pytest
 from datetime import datetime
 
 
-@pytest.mark.parametrize("mode", ["ols", "lowess"])
-def test_trendline_results_passthrough(mode):
+@pytest.mark.parametrize(
+    "mode,options",
+    [
+        ("ols", None),
+        ("ols", dict(log_x=True, log_y=True)),
+        ("lowess", None),
+        ("lowess", dict(frac=0.3)),
+        ("ma", dict(window=2)),
+        ("ewma", dict(alpha=0.5)),
+    ],
+)
+def test_trendline_results_passthrough(mode, options):
     df = px.data.gapminder().query("continent == 'Oceania'")
-    fig = px.scatter(df, x="year", y="pop", color="country", trendline=mode)
+    fig = px.scatter(
+        df,
+        x="year",
+        y="pop",
+        color="country",
+        trendline=mode,
+        trendline_options=options,
+    )
     assert len(fig.data) == 4
     for trace in fig["data"][0::2]:
         assert "trendline" not in trace.hovertemplate
@@ -20,90 +37,161 @@ def test_trendline_results_passthrough(mode):
     if mode == "ols":
         assert len(results) == 2
         assert results["country"].values[0] == "Australia"
-        assert results["country"].values[0] == "Australia"
         au_result = results["px_fit_results"].values[0]
         assert len(au_result.params) == 2
     else:
         assert len(results) == 0
 
 
-@pytest.mark.parametrize("mode", ["ols", "lowess"])
-def test_trendline_enough_values(mode):
-    fig = px.scatter(x=[0, 1], y=[0, 1], trendline=mode)
+@pytest.mark.parametrize(
+    "mode,options",
+    [
+        ("ols", None),
+        ("ols", dict(add_constant=False, log_x=True, log_y=True)),
+        ("lowess", None),
+        ("lowess", dict(frac=0.3)),
+        ("ma", dict(window=2)),
+        ("ewma", dict(alpha=0.5)),
+    ],
+)
+def test_trendline_enough_values(mode, options):
+    fig = px.scatter(x=[0, 1], y=[0, 1], trendline=mode, trendline_options=options)
     assert len(fig.data) == 2
     assert len(fig.data[1].x) == 2
-    fig = px.scatter(x=[0], y=[0], trendline=mode)
+    fig = px.scatter(x=[0], y=[0], trendline=mode, trendline_options=options)
     assert len(fig.data) == 2
     assert fig.data[1].x is None
-    fig = px.scatter(x=[0, 1], y=[0, None], trendline=mode)
-    assert len(fig.data) == 2
-    assert fig.data[1].x is None
-    fig = px.scatter(x=[0, 1], y=np.array([0, np.nan]), trendline=mode)
-    assert len(fig.data) == 2
-    assert fig.data[1].x is None
-    fig = px.scatter(x=[0, 1, None], y=[0, None, 1], trendline=mode)
+    fig = px.scatter(x=[0, 1], y=[0, None], trendline=mode, trendline_options=options)
     assert len(fig.data) == 2
     assert fig.data[1].x is None
     fig = px.scatter(
-        x=np.array([0, 1, np.nan]), y=np.array([0, np.nan, 1]), trendline=mode
+        x=[0, 1], y=np.array([0, np.nan]), trendline=mode, trendline_options=options
     )
     assert len(fig.data) == 2
     assert fig.data[1].x is None
-    fig = px.scatter(x=[0, 1, None, 2], y=[1, None, 1, 2], trendline=mode)
+    fig = px.scatter(
+        x=[0, 1, None], y=[0, None, 1], trendline=mode, trendline_options=options
+    )
+    assert len(fig.data) == 2
+    assert fig.data[1].x is None
+    fig = px.scatter(
+        x=np.array([0, 1, np.nan]),
+        y=np.array([0, np.nan, 1]),
+        trendline=mode,
+        trendline_options=options,
+    )
+    assert len(fig.data) == 2
+    assert fig.data[1].x is None
+    fig = px.scatter(
+        x=[0, 1, None, 2], y=[1, None, 1, 2], trendline=mode, trendline_options=options
+    )
     assert len(fig.data) == 2
     assert len(fig.data[1].x) == 2
     fig = px.scatter(
-        x=np.array([0, 1, np.nan, 2]), y=np.array([1, np.nan, 1, 2]), trendline=mode
+        x=np.array([0, 1, np.nan, 2]),
+        y=np.array([1, np.nan, 1, 2]),
+        trendline=mode,
+        trendline_options=options,
     )
     assert len(fig.data) == 2
     assert len(fig.data[1].x) == 2
 
 
-@pytest.mark.parametrize("mode", ["ols", "lowess"])
-def test_trendline_nan_values(mode):
+@pytest.mark.parametrize(
+    "mode,options",
+    [
+        ("ols", None),
+        ("ols", dict(add_constant=False, log_x=True, log_y=True)),
+        ("lowess", None),
+        ("lowess", dict(frac=0.3)),
+        ("ma", dict(window=2)),
+        ("ewma", dict(alpha=0.5)),
+    ],
+)
+def test_trendline_nan_values(mode, options):
     df = px.data.gapminder().query("continent == 'Oceania'")
     start_date = 1970
     df["pop"][df["year"] < start_date] = np.nan
-    fig = px.scatter(df, x="year", y="pop", color="country", trendline=mode)
+    fig = px.scatter(
+        df,
+        x="year",
+        y="pop",
+        color="country",
+        trendline=mode,
+        trendline_options=options,
+    )
     for trendline in fig["data"][1::2]:
         assert trendline.x[0] >= start_date
         assert len(trendline.x) == len(trendline.y)
 
 
-def test_no_slope_ols_trendline():
+def test_ols_trendline_slopes():
     fig = px.scatter(x=[0, 1], y=[0, 1], trendline="ols")
-    assert "y = 1" in fig.data[1].hovertemplate  # then + x*(some small number)
+    assert "y = 1 * x + 0<br>" in fig.data[1].hovertemplate
     results = px.get_trendline_results(fig)
     params = results["px_fit_results"].iloc[0].params
     assert np.all(np.isclose(params, [0, 1]))
 
+    fig = px.scatter(x=[0, 1], y=[1, 2], trendline="ols")
+    assert "y = 1 * x + 1<br>" in fig.data[1].hovertemplate
+    results = px.get_trendline_results(fig)
+    params = results["px_fit_results"].iloc[0].params
+    assert np.all(np.isclose(params, [1, 1]))
+
+    fig = px.scatter(
+        x=[0, 1], y=[1, 2], trendline="ols", trendline_options=dict(add_constant=False)
+    )
+    assert "y = 2 * x<br>" in fig.data[1].hovertemplate
+    results = px.get_trendline_results(fig)
+    params = results["px_fit_results"].iloc[0].params
+    assert np.all(np.isclose(params, [2]))
+
+    fig = px.scatter(
+        x=[1, 1], y=[0, 0], trendline="ols", trendline_options=dict(add_constant=False)
+    )
+    assert "y = 0 * x<br>" in fig.data[1].hovertemplate
+    results = px.get_trendline_results(fig)
+    params = results["px_fit_results"].iloc[0].params
+    assert np.all(np.isclose(params, [0]))
+
     fig = px.scatter(x=[1, 1], y=[0, 0], trendline="ols")
-    assert "y = 0" in fig.data[1].hovertemplate
+    assert "y = 0<br>" in fig.data[1].hovertemplate
     results = px.get_trendline_results(fig)
     params = results["px_fit_results"].iloc[0].params
     assert np.all(np.isclose(params, [0]))
 
     fig = px.scatter(x=[1, 2], y=[0, 0], trendline="ols")
-    assert "y = 0" in fig.data[1].hovertemplate
+    assert "y = 0 * x + 0<br>" in fig.data[1].hovertemplate
     fig = px.scatter(x=[0, 0], y=[1, 1], trendline="ols")
-    assert "y = 0 * x + 1" in fig.data[1].hovertemplate
+    assert "y = 0 * x + 1<br>" in fig.data[1].hovertemplate
     fig = px.scatter(x=[0, 0], y=[1, 2], trendline="ols")
-    assert "y = 0 * x + 1.5" in fig.data[1].hovertemplate
+    assert "y = 0 * x + 1.5<br>" in fig.data[1].hovertemplate
 
 
-@pytest.mark.parametrize("mode", ["ols", "lowess"])
-def test_trendline_on_timeseries(mode):
+@pytest.mark.parametrize(
+    "mode,options",
+    [
+        ("ols", None),
+        ("ols", dict(add_constant=False, log_x=True, log_y=True)),
+        ("lowess", None),
+        ("lowess", dict(frac=0.3)),
+        ("ma", dict(window=2)),
+        ("ma", dict(window="10d")),
+        ("ewma", dict(alpha=0.5)),
+    ],
+)
+def test_trendline_on_timeseries(mode, options):
     df = px.data.stocks()
 
     with pytest.raises(ValueError) as err_msg:
-        px.scatter(df, x="date", y="GOOG", trendline=mode)
+        px.scatter(df, x="date", y="GOOG", trendline=mode, trendline_options=options)
     assert "Could not convert value of 'x' ('date') into a numeric type." in str(
         err_msg.value
     )
 
     df["date"] = pd.to_datetime(df["date"])
     df["date"] = df["date"].dt.tz_localize("CET")  # force a timezone
-    fig = px.scatter(df, x="date", y="GOOG", trendline=mode)
+    fig = px.scatter(df, x="date", y="GOOG", trendline=mode, trendline_options=options)
     assert len(fig.data) == 2
     assert len(fig.data[0].x) == len(fig.data[1].x)
     assert type(fig.data[0].x[0]) == datetime
