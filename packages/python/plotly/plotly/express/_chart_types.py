5e14acdc52442f1bef86f758399d9154bd6597f6
Nicolas Kruchten <nicolas@plot.ly>
2021-06-18 08:28:52 -0400

    black

--- packages/python/plotly/plotly/express/_chart_types.py
+++ packages/python/plotly/plotly/express/_chart_types.py
@@ -200,9 +200,7 @@ density_heatmap.__doc__ = make_docstring(
         z=[
             "For `density_heatmap` and `density_contour` these values are used as the inputs to `histfunc`.",
         ],
-        histfunc=[
-            "The arguments to this function are the values of `z`.",
-        ],
+        histfunc=["The arguments to this function are the values of `z`.",],
     ),
 )
 
@@ -459,9 +457,7 @@ def histogram(
         args=locals(),
         constructor=go.Histogram,
         trace_patch=dict(
-            histnorm=histnorm,
-            histfunc=histfunc,
-            cumulative=dict(enabled=cumulative),
+            histnorm=histnorm, histfunc=histfunc, cumulative=dict(enabled=cumulative),
         ),
         layout_patch=dict(barmode=barmode, barnorm=barnorm),
     )
@@ -521,11 +517,7 @@ def violin(
         args=locals(),
         constructor=go.Violin,
         trace_patch=dict(
-            points=points,
-            box=dict(visible=box),
-            scalegroup=True,
-            x0=" ",
-            y0=" ",
+            points=points, box=dict(visible=box), scalegroup=True, x0=" ", y0=" ",
         ),
         layout_patch=dict(violinmode=violinmode),
     )
