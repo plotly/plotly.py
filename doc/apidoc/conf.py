648bc0a94a4dc885887f1bbce12eda7fcddb7615
Nicolas Kruchten <nicolas@plot.ly>
2021-06-21 12:17:29 -0400

    bump doc versions

--- doc/apidoc/conf.py
+++ doc/apidoc/conf.py
@@ -28,7 +28,7 @@ author = "Plotly"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "4.14.3"
+release = "5.0.0"
 
 
 # -- General configuration ---------------------------------------------------
