6e3919134af23dcafb6b6a0800be6d71bc68d7ce
Mathieu Fernandez <61463398+FernandezMathieu@users.noreply.github.com>
2021-10-05 15:38:46 +0200

    fix(test): change value for plotly.js-dist in test_plotlyjs_version

--- packages/python/plotly/plotly/tests/test_core/test_offline/test_offline.py
+++ packages/python/plotly/plotly/tests/test_core/test_offline/test_offline.py
@@ -323,7 +323,7 @@ class PlotlyOfflineTestCase(PlotlyOfflineBaseTestCase):
         )
         with open(path, "rt") as f:
             package_json = json.load(f)
-            expected_version = package_json["dependencies"]["plotly.js"]
+            expected_version = package_json["dependencies"]["plotly.js-dist"]
             if expected_version[0] == "^":
                 expected_version = expected_version[1:]
 
