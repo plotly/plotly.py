bf1a97a9f8d796926439620911eca559d09e32d4
lucafs <luca@ambientedecriacao.com.br>
2021-12-09 13:15:24 -0300

    Fixes format

--- packages/python/plotly/plotly/tests/test_optional/test_px/test_pie.py
+++ packages/python/plotly/plotly/tests/test_optional/test_px/test_pie.py
@@ -7,7 +7,7 @@ import pytest
 
 
 def _compare_legend_to_column_name(name_column, name_from_fig):
-    """Compare legend title name to the name given to it 
+    """Compare legend title name to the name given to it
     in "names" value to see if is equal to it and not None.
     """
     assert_equal(name_from_fig, name_column)
@@ -16,9 +16,10 @@ def _compare_legend_to_column_name(name_column, name_from_fig):
 
 def test_pie_like_px():
     # Pie
-    data_name = 'col1'
-    names_name = 'col2'
+    data_name = "col1"
+    names_name = "col2"
     df = pd.DataFrame(data={data_name: [3, 2, 1], names_name: [1, 2, 4]})
-    fig = px.pie(df, values=data_name, names=names_name,
-                 title='Population of European continent')
+    fig = px.pie(
+        df, values=data_name, names=names_name, title="Population of European continent"
+    )
     _compare_legend_to_column_name(names_name, fig.layout.legend.title.text)
