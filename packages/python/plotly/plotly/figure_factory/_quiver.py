6229a403ed3fd78cc90af97afe4d190afd288a16
Baptiste Chacun <baptiste.chacun@imt-atlantique.net>
2021-12-10 16:53:16 +0100

    quiv

--- packages/python/plotly/plotly/figure_factory/_quiver.py
+++ packages/python/plotly/plotly/figure_factory/_quiver.py
@@ -2,6 +2,9 @@ from __future__ import absolute_import
 
 import math
 
+import numpy as np
+import plotly.graph_objects as go
+
 from plotly import exceptions
 from plotly.graph_objs import graph_objs
 from plotly.figure_factory import utils
@@ -122,6 +125,343 @@ def create_quiver(
         )
 
     return graph_objs.Figure(data=data, layout=layout)
+
+
+##### ADD #########################################################################################################
+# Constant:
+r2 = np.sqrt(2)
+
+
+# Score functions :
+
+# Return 0, no impact on color
+def zero_score(u1, v1):
+    return 0
+
+
+# Size function, return the squar of the quiver size
+def size_score(u1, v1):
+    return u1 ** 2 + v1 ** 2
+
+
+# Return cos(a) with a the angle between a vector of coordinates (1,1) and therefore of norm square root of 2
+def orientation_score(u1, v1):
+    return (u1 + v1) / ((np.sqrt(size_score(u1, v1))) * r2)
+
+
+# Insertion sorting function to order the arrows and assign them a gradual coloring
+def tri_insertion_each(tab):
+    for i in range(1, len(tab)):
+        k = tab[i]
+        j = i - 1
+        while j >= 0 and k[4][0] < tab[j][4][0]:
+            tab[j + 1] = tab[j]
+            j -= 1
+        tab[j + 1] = k
+    return tab
+
+
+# Function that generates the colorbars for our diagram.
+# Plotly being designed to display the components present in the figure,
+# it is necessary to create invisible components that generate this bar. To do this,
+# we create here about forty invisible points (opacity = 1) which follow the desired color bar to create the colorbar.
+def colorbar_quiv(x1, y1, fig, colorbar_title, colorscale, n_color):
+    max_x = np.max(x1)
+    max_y = np.max(y1)
+    values_x = [i * max_x / 40 for i in range(40)]
+    values_y = [i * max_y / 40 for i in range(40)]
+
+    fig.add_trace(
+        go.Scatter(
+            x=values_x,
+            y=values_y,
+            opacity=0,
+            showlegend=False,
+            marker=dict(
+                size=16,
+                cmax=max_x,
+                cmin=0,
+                color=values_x,
+                colorbar=dict(title=colorbar_title, x=1 + n_color * 0.15),
+                colorscale=colorscale,
+            ),
+            mode="markers",
+        )
+    )
+
+
+# This function is used to give the color of each buttock
+# so that it follows a 1D gradient chosen according to their order in the list sorted by score.
+def gradient_1D(r, g, b, each_col, it, length_2, each_col_2):
+
+    color = [r, g, b]
+
+    # We use the 3 colors
+    if 0 not in color:
+
+        # Colors have gradient types given by the int associated with their variable
+        # (r for red, g for green and b for blue)
+        for i in range(3):
+
+            if color[i] == 3:
+                color[i] = round(each_col * it)
+
+            elif color[i] == 1:
+                color[i] = round(256 - each_col * it)
+
+            # middle color
+            if color[i] == 2:
+                if it < length_2:
+                    color[i] = round(each_col_2 * it)
+                else:
+                    color[i] = round(256 - each_col_2 * (it - length_2))
+
+    # We only use two colors, the one with an assigned 0 is not used
+    else:
+
+        # Colors have gradient types given by the int associated with their variable
+        # (r for red, g for green and b for blue)
+        for i in range(3):
+
+            if color[i] == 1:
+                color[i] = round(each_col * it)
+
+            elif color[i] == 2:
+                color[i] = round(255 - each_col * it)
+
+    # returns the color for the arrow
+    return "rgba(" + str(color[0]) + "," + str(color[1]) + "," + str(color[2]) + ",1)"
+
+
+# This function gives the color for an arrow in a figure using 2 dimensions for the colors.
+# One main and one secondary.
+def gradient_2D(r, g, b, each_col, it, it_2, length):
+
+    color = [r, g, b]
+
+    # Colors have gradient types given by the int associated with their variable
+    # (r for red, g for green and b for blue)
+    for i in range(3):
+        if color[i] == 2:
+            color[i] = round(each_col * it)
+
+        elif color[i] == 1:
+            color[i] = round(256 - each_col * it)
+        else:
+            color[i] = round(each_col * it_2)
+
+    return "rgba(" + str(color[0]) + "," + str(color[1]) + "," + str(color[2]) + ",1)"
+
+
+def create_colorised_quiver(x1, y1, u1, v1, **kwargs):
+    color = kwargs.get("color", None)
+    color_bar = kwargs.get("color_bar", None)
+    colorscale = kwargs.get("colorscale", None)
+    inv = kwargs.get("inv", None)
+
+    colorbar_title = ""
+
+    if color:
+        # Forme de la fonction color (u1,v1) -> int :
+        if color == "size_gradient":
+            color = size_score
+            mode = "1D"
+
+        if isinstance(color, list):
+            mode = color[0]
+            color_1 = color[1]
+            if mode == "2D":
+                color_2 = color[2]
+
+        if (inv) == None:
+            inv = [0, 0, 0]
+
+        if (colorscale) == None:
+
+            if mode == "1D":
+                colorscale = ["green", "red"]
+
+        if mode == "1D":
+            quivers_data = []
+
+            for i in range(len(x1)):
+                for j in range(len(u1[i])):
+
+                    each_size = color_1(u1[i][j], v1[i][j])
+
+                    quivers_data.append(
+                        [
+                            [x1[i][j]],
+                            [y1[i][j]],
+                            [u1[i][j]],
+                            [v1[i][j]],
+                            [[each_size], [i, j]],
+                        ]
+                    )
+
+            A = quivers_data
+            quivers_data = tri_insertion_each(quivers_data)
+
+            each_col = 255 / len(quivers_data)
+
+            r, g, b = inv
+
+            if len(colorscale) == 2:
+
+                add_value = 2
+                for color in colorscale:
+                    if color == "red":
+                        r += add_value
+                    if color == "green":
+                        g += add_value
+                    if color == "blue":
+                        b += add_value
+                    add_value += 1
+
+                    if add_value == 3:
+                        add_value = 1
+
+            else:
+                add_value = 1
+                for color in colorscale:
+                    if color == "red":
+                        r += add_value
+                    if color == "green":
+                        g += add_value
+                    if color == "blue":
+                        b += add_value
+                    add_value += 1
+
+            # constants :
+            length_2 = len(quivers_data) / 2
+            each_col_2 = each_col * 2
+
+            fig = create_quiver(
+                quivers_data[0][0],
+                quivers_data[0][1],
+                quivers_data[0][2],
+                quivers_data[0][3],
+                showlegend=False,
+            )
+
+            for i in range(1, len(quivers_data)):
+
+                if quivers_data[i][2] != []:
+                    fig_each = create_quiver(
+                        quivers_data[i][0],
+                        quivers_data[i][1],
+                        quivers_data[i][2],
+                        quivers_data[i][3],
+                        showlegend=False,
+                    )
+
+                    rgba_color = gradient_1D(r, g, b, each_col, i, length_2, each_col_2)
+
+                    fig_each.update_traces(line_color=rgba_color)
+                    fig.add_traces(data=fig_each.data)
+
+        if mode == "2D":
+
+            quivers_data = []
+
+            dict_2 = {}
+            quivers_data_2 = []
+
+            for i in range(len(x1)):
+                dict_2[i] = {}
+                for j in range(len(u1[i])):
+
+                    each_size = color_1(u1[i][j], v1[i][j])
+                    each_2 = color_2(u1[i][j], v1[i][j])
+
+                    quivers_data.append(
+                        [
+                            [x1[i][j]],
+                            [y1[i][j]],
+                            [u1[i][j]],
+                            [v1[i][j]],
+                            [[each_size], [i, j]],
+                        ]
+                    )
+                    quivers_data_2.append(
+                        [
+                            [x1[i][j]],
+                            [y1[i][j]],
+                            [u1[i][j]],
+                            [v1[i][j]],
+                            [[each_2], [i, j]],
+                        ]
+                    )
+
+            quivers_data = tri_insertion_each(quivers_data)
+            quivers_data_2 = tri_insertion_each(quivers_data_2)
+
+            each_col = 255 / len(quivers_data)
+
+            r, g, b = inv
+
+            add_value = 1
+            for color in colorscale:
+                if color == "red":
+                    r += add_value
+                if color == "green":
+                    g += add_value
+                if color == "blue":
+                    b += add_value
+                add_value += 1
+
+                if 3 in (r, g, b):
+                    add_value = 1
+
+            fig = create_quiver(
+                quivers_data[0][0],
+                quivers_data[0][1],
+                quivers_data[0][2],
+                quivers_data[0][3],
+                showlegend=False,
+            )
+            for i in range(1, len(quivers_data)):
+
+                if quivers_data[i][2] != []:
+                    fig_each = create_quiver(
+                        quivers_data[i][0],
+                        quivers_data[i][1],
+                        quivers_data[i][2],
+                        quivers_data[i][3],
+                        showlegend=False,
+                    )
+
+                    i_2 = 0
+                    while_condition = quivers_data[i][0:2]
+                    while while_condition != quivers_data_2[i_2][0:2]:
+                        i_2 += 1
+
+                    rgba_color = gradient_2D(
+                        r, g, b, each_col, i, i_2, len(quivers_data)
+                    )
+
+                    fig_each.update_traces(line_color=rgba_color)
+                    fig.add_traces(data=fig_each.data)
+
+        if color_bar:
+            if color_bar == True:
+                colorbar_quiv(x1, y1, fig, colorbar_title, ["green", "red"])
+            if isinstance(color_bar, list):
+
+                if mode == "1D":
+                    # Forme de la liste attendue : [colorbar_title, colorbar_scale]
+                    colorbar_quiv(x1, y1, fig, color_bar[0], colorscale, 1)
+                if mode == "2D":
+                    # Forme de la liste attendue : [colorbar_title, colorbar_scale]
+                    colorbar_quiv(x1, y1, fig, color_bar[0], colorscale[:2], 1)
+                    colorbar_quiv(
+                        x1, y1, fig, color_bar[1], ["white"] + [colorscale[2]], 2
+                    )
+
+        fig.show()
+
+
+############################################################################################
 
 
 class _Quiver(object):
