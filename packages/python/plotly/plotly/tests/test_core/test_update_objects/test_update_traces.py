8de1a16ea7dae6a6627bc27951f05923c8f0ce61
Wolfgang F. Riedl <github@wfriedl.de>
2022-08-01 11:13:31 +0200

    Update test_update_traces.py

--- packages/python/plotly/plotly/tests/test_core/test_update_objects/test_update_traces.py
+++ packages/python/plotly/plotly/tests/test_core/test_update_objects/test_update_traces.py
@@ -430,6 +430,7 @@ def select_traces_fixture():
 
 def test_select_traces_integer(select_traces_fixture):
     fig = select_traces_fixture
+    
     # check that selecting first trace does indeed only select the first
     tr = list(fig.select_traces(selector=0))
     assert len(tr) == 1
