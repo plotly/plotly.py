d9dbcb81cac55ee3511d553a227a9058863c0603
Nicolas Kruchten <nicolas@plot.ly>
2023-01-04 17:36:19 -0500

    Update setup.py

--- packages/python/plotly/setup.py
+++ packages/python/plotly/setup.py
@@ -511,6 +511,8 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Scientific/Engineering :: Visualization",
         "License :: OSI Approved :: MIT License",
     ],
