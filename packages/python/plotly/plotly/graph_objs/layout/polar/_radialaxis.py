ac0be21e02e3824d38146925d33ef103cc431765
Nicolas Kruchten <nicolas@kruchten.com>
2023-01-11 09:55:33 -0500

    conditional docstring

--- packages/python/plotly/plotly/graph_objs/layout/polar/_radialaxis.py
+++ packages/python/plotly/plotly/graph_objs/layout/polar/_radialaxis.py
@@ -75,8 +75,8 @@ class RadialAxis(_BaseLayoutHierarchyType):
         angle.
 
         The 'angle' property is a angle (in degrees) that may be
-        specified as a number between -180 and 180. Numeric values outside this
-        range are converted to the equivalent value
+        specified as a number between -180 and 180.
+        Numeric values outside this range are converted to the equivalent value
         (e.g. 270 is converted to -90).
 
         Returns
@@ -920,8 +920,8 @@ class RadialAxis(_BaseLayoutHierarchyType):
         labels vertically.
 
         The 'tickangle' property is a angle (in degrees) that may be
-        specified as a number between -180 and 180. Numeric values outside this
-        range are converted to the equivalent value
+        specified as a number between -180 and 180.
+        Numeric values outside this range are converted to the equivalent value
         (e.g. 270 is converted to -90).
 
         Returns
