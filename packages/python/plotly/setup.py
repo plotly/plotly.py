59cfb3b942c16922cc989348c50674e14fa8e9b0
alexcjohnson <alex@plot.ly>
2023-03-16 16:17:58 -0400

    require package packaging seems it's implicitly included by another dep in py3.7+, but not in 3.6

--- packages/python/plotly/setup.py
+++ packages/python/plotly/setup.py
@@ -556,7 +556,7 @@ setup(
     data_files=[
         ("etc/jupyter/nbconfig/notebook.d", ["jupyterlab-plotly.json"]),
     ],
-    install_requires=["tenacity>=6.2.0"],
+    install_requires=["tenacity>=6.2.0", "packaging"],
     zip_safe=False,
     cmdclass=dict(
         build_py=js_prerelease(versioneer_cmds["build_py"]),
