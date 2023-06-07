605ac4fc717b79ec679866a2106728ac3d07ac01
Alex Johnson <alex@plot.ly>
2023-06-07 16:48:13 -0400

    Update packages/python/plotly/_plotly_utils/basevalidators.py

--- packages/python/plotly/_plotly_utils/basevalidators.py
+++ packages/python/plotly/_plotly_utils/basevalidators.py
@@ -2649,6 +2649,7 @@ class BaseDataValidator(BaseValidator):
         # Import Histogram2dcontour, this is the deprecated name of the
         # Histogram2dContour trace.
         from plotly.graph_objs import Histogram2dcontour
+
         if v is None:
             v = []
         else:
