7ab9d80ef3bf888ec9debae72dd810a4708783a8
alexcjohnson <alex@plot.ly>
2023-08-16 15:47:12 -0400

    docs 5.16.1

--- doc/apidoc/conf.py
+++ doc/apidoc/conf.py
@@ -28,7 +28,7 @@ author = "Plotly"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "5.16.0"
+release = "5.16.1"
 
 
 # -- General configuration ---------------------------------------------------
