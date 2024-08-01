a609a2fad4241dcf0ea48a331cdddd3e12068fe3
Martha Cryan <martha.cryan326@gmail.com>
2024-08-01 14:22:12 -0700

    Update variables used in tests

--- packages/python/plotly/plotly/tests/test_core/test_graph_objs/test_performance.py
+++ packages/python/plotly/plotly/tests/test_core/test_graph_objs/test_performance.py
@@ -1,47 +1,74 @@
-import sys
 import time
-from unittest import TestCase
-import pytest
 import numpy as np
 import plotly.graph_objs as go
-import plotly.io as pio
-from plotly.tests.b64 import b64, _b64
+from plotly.tests.b64 import b64
 
+np.random.seed(1)
+
+def test_performance_scatter3d():
+    N = 10000
+
+    x = np.random.randn(N)
+    y = np.random.randn(N).astype('float32')
+    z = np.random.randint(size=N, low=0, high=256, dtype='uint8')
+    c = np.random.randint(size=N, low=-10, high=10, dtype='int8')
+
+    # Test the performance with lists
+    list_start = time.time()
+    fig = go.Figure(data=[go.Scatter3d(
+        x=x.tolist(),
+        y=y.tolist(),
+        z=z.tolist(),
+        marker=dict(color=c.tolist()),
+        mode='markers',
+        opacity=0.2
+    )])
+    list_time_elapsed = time.time() - list_start
+
+    # Test the performance with base64 arrays
+    b64_start = time.time()
+    fig = go.Scatter3d(
+        x=b64(x),
+        y=b64(y),
+        z=b64(z),
+        marker=dict(color=b64(c)),
+        mode='markers',
+        opacity=0.2
+    )
+    b64_time_elapsed = time.time() - b64_start
+
+    # b64 should be faster than raw
+    assert (b64_time_elapsed / list_time_elapsed) < 0.85
 
 def test_performance_b64_float64():
-    np_arr_1 = np.random.random(100000)
-    np_arr_2 = np.random.random(100000)
-    list_1 = np_arr_1.tolist()
-    list_2 = np_arr_2.tolist()
-    b64_arr_1 = b64(np_arr_1)
-    b64_arr_2 = b64(np_arr_2)
+    np_arr_1 = np.random.random(10000)
+    np_arr_2 = np.random.random(10000)
 
     # Test the performance of the base64 arrays
     b64_start = time.time()
-    fig = go.Scatter(x=b64_arr_1, y=b64_arr_2)
+    fig = go.Scatter(x=b64(np_arr_1), y=b64(np_arr_2))
     b64_time_elapsed = time.time() - b64_start
 
     # Test the performance of the raw arrays
-    raw_start = time.time()
-    fig = go.Scatter(x=list_1, y=list_2)
-    raw_time_elapsed = time.time() - raw_start
+    list_start = time.time()
+    fig = go.Scatter(x=np_arr_1.tolist(), y=np_arr_2.tolist())
+    list_time_elapsed = time.time() - list_start
 
     # b64 should be faster than raw
-    assert (b64_time_elapsed / raw_time_elapsed) < 0.85
+    assert (b64_time_elapsed / list_time_elapsed) < 0.85
 
 
 def test_size_performance_b64_uint8():
-    rand_arr_1 = (np.random.random(100000) * 256).astype("uint8")
-    rand_arr_2 = (np.random.random(100000) * 256).astype("uint8")
-    raw_arr_1 = rand_arr_1.tolist()
-    raw_arr_2 = rand_arr_2.tolist()
-    b64_arr_1 = b64(rand_arr_1)
-    b64_arr_2 = b64(rand_arr_2)
+    np_arr_1 = (np.random.random(100000) * 256).astype("uint8")
+    np_arr_2 = (np.random.random(100000) * 256).astype("uint8")
 
-    # Compare the size of figures with b64 arrays and raw arrays
-    fig_b64 = go.Scatter(x=b64_arr_1, y=b64_arr_2)
+    # Measure the size of figures with b64 arrays
+    fig_b64 = go.Scatter(x=b64(np_arr_1), y=b64(np_arr_2))
     size_b64 = fig_b64.to_json().__sizeof__()
-    fig_raw = go.Scatter(x=raw_arr_1, y=raw_arr_2)
-    size_raw = fig_raw.to_json().__sizeof__()
 
-    assert size_b64 / size_raw < 0.85
+    # Measure the size of the figure with normal python lists
+    fig_list = go.Scatter(x=np_arr_1.tolist(), y=np_arr_2.tolist())
+    size_list = fig_list.to_json().__sizeof__()
+
+    # b64 should be smaller than raw
+    assert size_b64 / size_list < 0.85
