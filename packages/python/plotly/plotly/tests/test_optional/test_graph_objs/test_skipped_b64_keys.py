259d509d6311df6ddfb939f1b5d1bb0096752e20
Martha Cryan <martha.cryan326@gmail.com>
2024-08-27 10:50:24 -0500

    Add tests for layers and range keys

--- packages/python/plotly/plotly/tests/test_optional/test_graph_objs/test_skipped_b64_keys.py
+++ packages/python/plotly/plotly/tests/test_optional/test_graph_objs/test_skipped_b64_keys.py
@@ -38,3 +38,58 @@ class TestSkippedBase64Keys(NumpyTestUtilsMixin, TestCase):
         fig.show()  
 
         assert (fig["data"][0]["geojson"]["geometry"]["coordinates"] == normal_coordinates).all()
+
+    def test_np_layers(self):
+        layout = {
+            "mapbox": {
+                "layers": [
+                    {
+                        "sourcetype": "geojson",
+                        "type": "line",
+                        "line": {
+                            "dash": np.array([2.5, 1])
+                        },
+                        "source": {
+                            "type": "FeatureCollection",
+                            "features": [
+                                {
+                                    "type": "Feature",
+                                    "geometry": {
+                                        "type": "LineString",
+                                        "coordinates": np.array([[0.25, 52], [0.75, 50]])
+                                    },
+                                }
+                            ]
+                        }
+                    },
+                ],
+                "center": {
+                    "lon": 0.5,
+                    "lat": 51
+                }
+            },
+        }
+        data = [
+            {
+                "type": "scattermapbox"
+            }
+        ]
+
+        fig = go.Figure(data=data, layout=layout)
+
+        # TODO: This is failing because the actual value of the "dash" field
+        # is converted to the { b64, dtype } object. 
+        # assert fig.layout['mapbox']['layers'][0]['line']['dash'] == (2.5, 1)
+        
+        assert (fig.layout['mapbox']['layers'][0]['source']['features'][0]['geometry']['coordinates'] == [[0.25, 52], [0.75, 50]]).all()
+
+    def test_np_range(self):
+        layout = {
+            "xaxis": {
+                "range": np.array([0, 1])
+            }
+        }
+
+        fig = go.Figure(data=[{ "type": "scatter" }], layout=layout)
+
+        assert fig.layout["xaxis"]["range"] == (0, 1)
