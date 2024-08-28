b83b26e0ebd9ded5afe78058bac37855d839deb4
Martha Cryan <martha.cryan326@gmail.com>
2024-08-28 18:58:19 -0500

    Potential fix to conversion bug

--- packages/python/plotly/plotly/tests/test_optional/test_graph_objs/test_skipped_b64_keys.py
+++ packages/python/plotly/plotly/tests/test_optional/test_graph_objs/test_skipped_b64_keys.py
@@ -70,7 +70,7 @@ class TestShouldNotUseBase64InUnsupportedKeys(NumpyTestUtilsMixin, TestCase):
 
         # TODO: This is failing because the actual value of the "dash" field
         # is converted to the { b64, dtype } object.
-        # assert fig.layout['mapbox']['layers'][0]['line']['dash'] == (2.5, 1)
+        assert fig.layout['mapbox']['layers'][0]['line']['dash'] == (2.5, 1)
 
         assert (
             fig.layout["mapbox"]["layers"][0]["source"]["features"][0]["geometry"][
