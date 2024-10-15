7968cffca1e8244eed08f97297a6d096c05460be
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2024-10-15 22:01:15 +0200

    conftest

---
+++ packages/python/plotly/plotly/tests/conftest.py
@@ -0,0 +1,46 @@
+from typing import Any
+from typing import Callable
+
+import pandas as pd
+import polars as pl
+import pyarrow as pa
+import pytest
+
+from narwhals.typing import IntoDataFrame
+from narwhals.utils import parse_version
+
+
+def pandas_constructor(obj: dict[str, Any]) -> IntoDataFrame:
+    return pd.DataFrame(obj)  # type: ignore[no-any-return]
+
+
+def pandas_nullable_constructor(obj: dict[str, Any]) -> IntoDataFrame:
+    return pd.DataFrame(obj).convert_dtypes(dtype_backend="numpy_nullable")  # type: ignore[no-any-return]
+
+
+def pandas_pyarrow_constructor(obj: dict[str, Any]) -> IntoDataFrame:
+    return pd.DataFrame(obj).convert_dtypes(dtype_backend="pyarrow")  # type: ignore[no-any-return]
+
+
+def polars_eager_constructor(obj: dict[str, Any]) -> IntoDataFrame:
+    return pl.DataFrame(obj)
+
+
+def pyarrow_table_constructor(obj: dict[str, Any]) -> IntoDataFrame:
+    return pa.table(obj)  # type: ignore[no-any-return]
+
+
+constructors = [polars_eager_constructor, pyarrow_table_constructor, pandas_constructor]
+
+if parse_version(pd.__version__) >= parse_version("2.0.0"):
+    constructors = [
+        pandas_nullable_constructor,
+        pandas_pyarrow_constructor,
+    ]
+
+
+@pytest.fixture(params=constructors)
+def constructor(
+    request: pytest.FixtureRequest,
+) -> Callable[[dict[str, Any]], IntoDataFrame]:
+    return request.param  # type: ignore[no-any-return]
