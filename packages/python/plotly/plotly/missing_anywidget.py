0c3cd58df441bdcd57bf70f5ce1c7e7ece3f3a27
Martha Cryan <martha.cryan326@gmail.com>
2024-10-21 16:31:01 -0500

    Update docstring

--- packages/python/plotly/plotly/missing_anywidget.py
+++ packages/python/plotly/plotly/missing_anywidget.py
@@ -3,9 +3,9 @@ from .basedatatypes import BaseFigure
 
 class FigureWidget(BaseFigure):
     """
-    FigureWidget stand-in for use when ipywidgets is not installed. The only purpose
+    FigureWidget stand-in for use when anywidget is not installed. The only purpose
     of this class is to provide something to import as
-    `plotly.graph_objs.FigureWidget` when ipywidgets is not installed. This class
+    `plotly.graph_objs.FigureWidget` when anywidget is not installed. This class
     simply raises an informative error message when the constructor is called
     """
 
