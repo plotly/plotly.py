b6f916bb129affb29c8303feb539cba0e570a21c
Martha Cryan <martha.cryan326@gmail.com>
2024-10-21 17:07:32 -0500

    Update setup.py to include both widget and jupyterlab extension build

--- packages/python/plotly/setup.py
+++ packages/python/plotly/setup.py
@@ -20,12 +20,13 @@ import versioneer
 
 here = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.dirname(os.path.dirname(os.path.dirname(here)))
-node_root = os.path.join(here, "js")
+jupyterlab_extension_root = os.path.join(project_root, "javascript", "jupyterlab-plotly")
+widget_root = os.path.join(here, "js")
 is_repo = os.path.exists(os.path.join(project_root, ".git"))
 
 npm_path = os.pathsep.join(
     [
-        os.path.join(node_root, "node_modules", ".bin"),
+        os.path.join(jupyterlab_extension_root, "node_modules", ".bin"),
         os.environ.get("PATH", os.defpath),
     ]
 )
@@ -131,9 +132,12 @@ class NPM(Command):
 
     user_options = []
 
-    node_modules = os.path.join(node_root, "node_modules")
+    node_modules = os.path.join(jupyterlab_extension_root, "node_modules")
 
     targets = [
+        os.path.join(here, "jupyterlab_plotly", "nbextension", "extension.js"),
+        os.path.join(here, "jupyterlab_plotly", "nbextension", "index.js"),
+        os.path.join(here, "jupyterlab_plotly", "labextension", "package.json"),
         os.path.join(here, "plotly", "bundle.js"),
     ]
 
@@ -158,11 +162,6 @@ class NPM(Command):
         except:
             return False
 
-    def should_run_npm_install(self):
-        package_json = os.path.join(node_root, "package.json")
-        node_modules_exists = os.path.exists(self.node_modules)
-        return self.has_npm()
-
     def run(self):
         if skip_npm:
             log.info("Skipping npm-installation")
@@ -177,14 +176,20 @@ class NPM(Command):
         env = os.environ.copy()
         env["PATH"] = npm_path
 
-        if self.should_run_npm_install():
+        if self.has_npm():
             log.info(
                 "Installing build dependencies with npm.  This may take a while..."
             )
             npmName = self.get_npm_name()
             check_call(
                 [npmName, "install"],
-                cwd=node_root,
+                cwd=jupyterlab_extension_root,
+                stdout=sys.stdout,
+                stderr=sys.stderr,
+            )
+            check_call(
+                [npmName, "install"],
+                cwd=widget_root,
                 stdout=sys.stdout,
                 stderr=sys.stderr,
             )
@@ -192,13 +197,19 @@ class NPM(Command):
                 plotly_archive = os.path.join(self.local, "plotly.js.tgz")
                 check_call(
                     [npmName, "install", plotly_archive],
-                    cwd=node_root,
+                    cwd=jupyterlab_extension_root,
                     stdout=sys.stdout,
                     stderr=sys.stderr,
                 )
             check_call(
+                [npmName, "run", "build:prod"],
+                cwd=jupyterlab_extension_root,
+                stdout=sys.stdout,
+                stderr=sys.stderr,
+            )
+            check_call(
                 [npmName, "run", "build"],
-                cwd=node_root,
+                cwd=widget_root,
                 stdout=sys.stdout,
                 stderr=sys.stderr,
             )
@@ -449,7 +460,7 @@ class UpdateBundleSchemaDevCommand(Command):
             overwrite_schema_local(schema_uri)
 
         # Update plotly.js url in package.json
-        package_json_path = os.path.join(node_root, "package.json")
+        package_json_path = os.path.join(jupyterlab_extension_root, "package.json")
         with open(package_json_path, "r") as f:
             package_json = json.load(f)
 
@@ -504,7 +515,7 @@ class UpdatePlotlywidgetVersionCommand(Command):
         from plotly._version import git_pieces_from_vcs, render
 
         # Update plotly.js url in package.json
-        package_json_path = os.path.join(node_root, "package.json")
+        package_json_path = os.path.join(jupyterlab_extension_root, "package.json")
 
         with open(package_json_path, "r") as f:
             package_json = json.load(f)
