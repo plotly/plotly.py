6ffb554eacd2947e9f0af0fecba27d75475d83b3
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2024-10-21 23:17:56 +0200

    bump version and adjust core

--- packages/python/plotly/setup.py
+++ packages/python/plotly/setup.py
@@ -603,7 +603,7 @@ setup(
     data_files=[
         ("etc/jupyter/nbconfig/notebook.d", ["jupyterlab-plotly.json"]),
     ],
-    install_requires=["tenacity>=6.2.0", "narwhals>=1.9.2", "packaging"],
+    install_requires=["tenacity>=6.2.0", "narwhals>=1.10.0", "packaging"],
     zip_safe=False,
     cmdclass=dict(
         build_py=js_prerelease(versioneer_cmds["build_py"]),
