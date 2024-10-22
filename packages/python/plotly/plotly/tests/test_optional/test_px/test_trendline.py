0103aa69ff84d752d319cc9763b546e3ab22e8f6
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2024-10-22 18:02:01 +0200

    revert test

--- packages/python/plotly/plotly/tests/test_optional/test_px/test_trendline.py
+++ packages/python/plotly/plotly/tests/test_optional/test_px/test_trendline.py
@@ -189,10 +189,10 @@ def test_ols_trendline_slopes():
 def test_trendline_on_timeseries(constructor, mode, options):
     df = nw.from_native(constructor(px.data.stocks().to_dict(orient="list")))
 
-    with pytest.raises(
-        ValueError,
-        match=r"Could not convert value of 'x' \('date'\) into a numeric type.",
-    ):
+    pd_err_msg = r"Could not convert value of 'x' \('date'\) into a numeric type."
+    pl_err_msg = "conversion from `str` to `f64` failed in column 'date'"
+
+    with pytest.raises(Exception, match=rf"({pd_err_msg}|{pl_err_msg})"):
         px.scatter(
             df.to_native(),
             x="date",
