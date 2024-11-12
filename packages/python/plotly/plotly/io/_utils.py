fc8eff2ef331985d3c47f76f9a0bed199dc44f0b
Martha Cryan <martha.cryan326@gmail.com>
2024-11-12 10:52:57 -0600

    Move warnings out of utils file to avoid psutils import error

--- packages/python/plotly/plotly/io/_utils.py
+++ packages/python/plotly/plotly/io/_utils.py
@@ -1,35 +1,6 @@
 import plotly
 import plotly.graph_objs as go
 from plotly.offline import get_plotlyjs_version
-from plotly import optional_imports
-
-import warnings
-import psutil
-
-
-def display_jupyter_version_warnings():
-    parent_process = None
-    try:
-        parent_process = psutil.Process().parent().cmdline()[-1]
-    except Exception:
-        pass
-
-    if parent_process is None:
-        return
-    elif "jupyter-notebook" in parent_process:
-        jupyter_notebook = optional_imports.get_module("notebook")
-        if jupyter_notebook.__version__ < "7":
-            # Add warning about upgrading notebook
-            warnings.warn(
-                f"Plotly version >= 6 requires Jupyter Notebook >= 7 but you have {jupyter_notebook.__version__} installed.\n To upgrade Jupyter Notebook, please run `pip install notebook --upgrade`."
-            )
-    elif "jupyter-lab" in parent_process:
-        jupyter_lab = optional_imports.get_module("jupyterlab")
-        if jupyter_lab.__version__ < "3":
-            # Add warning about upgrading jupyterlab
-            warnings.warn(
-                f"Plotly version >= 6 requires JupyterLab >= 3 but you have {jupyter_lab.__version__} installed. To upgrade JupyterLab, please run `pip install jupyterlab --upgrade`."
-            )
 
 
 def validate_coerce_fig_to_dict(fig, validate):
