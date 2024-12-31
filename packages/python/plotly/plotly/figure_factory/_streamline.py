5249afb7e9be9e29765b9a11aefda0a5803eb095
Liam Connors <connorsl@tcd.ie>
2024-12-31 14:26:32 -0500

    Update _streamline.py

--- packages/python/plotly/plotly/figure_factory/_streamline.py
+++ packages/python/plotly/plotly/figure_factory/_streamline.py
@@ -40,30 +40,40 @@ def create_streamline(
     """
     Returns data for a streamline plot.
 
-    :param (list|ndarray) x: 1 dimensional, evenly spaced list or array
-    :param (list|ndarray) y: 1 dimensional, evenly spaced list or array
-    :param (ndarray) u: 2 dimensional array
-    :param (ndarray) v: 2 dimensional array
-    :param (float|int) density: controls the density of streamlines in
-        plot. This is multiplied by 30 to scale similiarly to other
-        available streamline functions such as matplotlib.
-        Default = 1
-    :param (angle in radians) angle: angle of arrowhead. Default = pi/9
-    :param (float in [0,1]) arrow_scale: value to scale length of arrowhead
-        Default = .09
-    :param kwargs: kwargs passed through plotly.graph_objs.Scatter
-        for more information on valid kwargs call
-        help(plotly.graph_objs.Scatter)
+    Parameters
+    ----------
+    x : list or ndarray
+        1-dimensional, evenly spaced list or array.
+    y : list or ndarray
+        1-dimensional, evenly spaced list or array.
+    u : ndarray
+        2-dimensional array representing the x-components of the vector field.
+    v : ndarray
+        2-dimensional array representing the y-components of the vector field.
+    density : float or int
+        Controls the density of streamlines in the plot. This is multiplied by 30 to scale similarly to other
+        available streamline functions such as matplotlib. Default is 1.
+    angle : float
+        Angle of arrowhead in radians. Default is pi/9.
+    arrow_scale : float
+        Value to scale the length of the arrowhead. Default is 0.09.
+    **kwargs
+        Additional keyword arguments passed through to `plotly.graph_objs.Scatter`.
+        For more information on valid kwargs, call `help(plotly.graph_objs.Scatter)`.
 
-    :rtype (dict): returns a representation of streamline figure.
+    Returns
+    -------
+    dict
+        A representation of the streamline figure.
 
+    Examples
+    --------
     Example 1: Plot simple streamline and increase arrow size
 
     >>> from plotly.figure_factory import create_streamline
     >>> import plotly.graph_objects as go
     >>> import numpy as np
     >>> import math
-
     >>> # Add data
     >>> x = np.linspace(-3, 3, 100)
     >>> y = np.linspace(-3, 3, 100)
@@ -72,7 +82,6 @@ def create_streamline(
     >>> v = 1 + X - Y**2
     >>> u = u.T  # Transpose
     >>> v = v.T  # Transpose
-
     >>> # Create streamline
     >>> fig = create_streamline(x, y, u, v, arrow_scale=.1)
     >>> fig.show()
@@ -82,7 +91,6 @@ def create_streamline(
     >>> from plotly.figure_factory import create_streamline
     >>> import numpy as np
     >>> import math
-
     >>> # Add data
     >>> N = 50
     >>> x_start, x_end = -2.0, 2.0
@@ -92,18 +100,14 @@ def create_streamline(
     >>> X, Y = np.meshgrid(x, y)
     >>> ss = 5.0
     >>> x_s, y_s = -1.0, 0.0
-
     >>> # Compute the velocity field on the mesh grid
     >>> u_s = ss/(2*np.pi) * (X-x_s)/((X-x_s)**2 + (Y-y_s)**2)
     >>> v_s = ss/(2*np.pi) * (Y-y_s)/((X-x_s)**2 + (Y-y_s)**2)
-
     >>> # Create streamline
     >>> fig = create_streamline(x, y, u_s, v_s, density=2, name='streamline')
-
     >>> # Add source point
     >>> point = go.Scatter(x=[x_s], y=[y_s], mode='markers',
     ...                    marker_size=14, name='source point')
-
     >>> fig.add_trace(point) # doctest: +SKIP
     >>> fig.show()
     """
