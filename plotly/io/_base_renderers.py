d496b63a8d84285bb7486238dc8779dc5bf603bd
Martha Cryan <martha.cryan326@gmail.com>
2025-03-19 19:40:03 -0500

    remove print statement

--- plotly/io/_base_renderers.py
+++ plotly/io/_base_renderers.py
@@ -85,7 +85,6 @@ class PlotlyRenderer(MimetypeRenderer):
         self.config = dict(config) if config else {}
 
     def to_mimebundle(self, fig_dict):
-        print("in PlotlyRenderer")
         config = _get_jconfig(self.config)
         if config:
             fig_dict["config"] = config
