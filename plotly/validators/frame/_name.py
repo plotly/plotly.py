5d29c6973d59499a8590e8901aaca26141ac14b2
Brian Benjamin Maranville <brian.maranville@nist.gov>
2025-05-08 00:26:18 -0400

    removing old generated validators

--- plotly/validators/frame/_name.py
+++
@@ -1,9 +0,0 @@
-#                   --- THIS FILE IS AUTO-GENERATED ---
-# Modifications will be overwitten the next time code generation run.
-
-import _plotly_utils.basevalidators as _bv
-
-
-class NameValidator(_bv.StringValidator):
-    def __init__(self, plotly_name="name", parent_name="frame", **kwargs):
-        super().__init__(plotly_name, parent_name, **kwargs)
--- plotly/validators/layout/scene/camera/projection/__init__.py
+++
@@ -1,4 +0,0 @@
-import sys
-from _plotly_utils.importers import relative_import
-
-__all__, __getattr__, __dir__ = relative_import(__name__, [], ["._type.TypeValidator"])
