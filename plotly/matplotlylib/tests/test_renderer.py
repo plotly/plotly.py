7f5b5edff6992b56064e865df373c70094e7b043
robertoffmoura <robertoffmoura@gmail.com>
2025-06-15 14:40:37 +0100

    Fix issue when converting matplotlib lines+markers legend to plotly

--- plotly/matplotlylib/tests/test_renderer.py
+++ plotly/matplotlylib/tests/test_renderer.py
@@ -84,3 +84,19 @@ def test_multiple_traces_native_legend():
     assert plotly_fig.data[0].mode == "lines"
     assert plotly_fig.data[1].mode == "markers"
     assert plotly_fig.data[2].mode == "lines+markers"
+
+
+def test_lines_markers_legend_plot():
+    x = [0, 1]
+    y = [0, 1]
+    label = "label"
+    plt.figure()
+    plt.plot(x, y, "o-", label=label)
+    plt.legend()
+
+    plotly_fig = tls.mpl_to_plotly(plt.gcf())
+
+    assert plotly_fig.data[0].mode == "lines+markers"
+    assert plotly_fig.data[0].x == tuple(x)
+    assert plotly_fig.data[0].y == tuple(y)
+    assert plotly_fig.data[0].name == "label"
