128dd25b8b6c577d223c5500e65950923c7cc369
robertoffmoura <robertoffmoura@gmail.com>
2025-08-12 10:02:08 +0100

    Run ruff format

--- plotly/matplotlylib/tests/test_renderer.py
+++ plotly/matplotlylib/tests/test_renderer.py
@@ -2,6 +2,7 @@ import plotly.tools as tls
 
 from . import plt
 
+
 def test_non_arithmetic_progression_xtickvals():
     xticks = [0.01, 0.53, 0.75]
     plt.figure()
@@ -11,6 +12,7 @@ def test_non_arithmetic_progression_xtickvals():
     plotly_fig = tls.mpl_to_plotly(plt.gcf())
 
     assert plotly_fig.layout.xaxis.tickvals == tuple(xticks)
+
 
 def test_non_arithmetic_progression_xticktext():
     xtickvals = [0.01, 0.53, 0.75]
