d12c162594f6a54f69504a87f414ea8465b5b74d
Cameron DeCoster <cameron.decoster@gmail.com>
2026-01-09 09:59:55 -0700

    fix: Handle arrays in BooleanValidator

--- tests/test_plotly_utils/validators/test_boolean_validator.py
+++ tests/test_plotly_utils/validators/test_boolean_validator.py
@@ -12,6 +12,14 @@ def validator(request):
     return BooleanValidator("prop", "parent", dflt=request.param)
 
 
+@pytest.fixture(params=[True, False])
+def validator_aok(request):
+    return BooleanValidator("prop", "parent", dflt=request.param, array_ok=True)
+
+
+# Array not ok (default)
+
+
 # Acceptance
 @pytest.mark.parametrize("val", [True, False])
 def test_acceptance(val, validator):
@@ -25,3 +33,27 @@ def test_rejection(val, validator):
         validator.validate_coerce(val)
 
     assert "Invalid value" in str(validation_failure.value)
+
+
+# Array ok
+
+
+# Acceptance
+@pytest.mark.parametrize("val", [(True, False), [True, False]])
+def test_acceptance_aok(val, validator_aok):
+    v = validator_aok.validate_coerce(val)
+    if isinstance(val, list):
+        assert val == v
+    else:
+        assert val == tuple(v)
+
+
+# Rejection
+@pytest.mark.parametrize(
+    "val", [(True, "Planet Express"), ["Hubert Farnsworth", False]]
+)
+def test_rejection_aok(val, validator_aok):
+    with pytest.raises(ValueError) as validation_failure:
+        validator_aok.validate_coerce(val)
+
+    assert "Invalid elements" in str(validation_failure.value)
