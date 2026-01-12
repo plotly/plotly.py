1d665ba230c5fb13f4e2230c53f0f46a9c66abcf
My-Tien Nguyen <my-tien@users.noreply.github.com>
2026-01-12 14:24:28 +0100

    Add array_ok to SubplotidValidator, used in pie.legend

--- tests/test_plotly_utils/validators/test_subplotid_validator.py
+++ tests/test_plotly_utils/validators/test_subplotid_validator.py
@@ -10,11 +10,16 @@ from ...test_optional.test_utils.test_utils import np_nan, np_inf
 def validator():
     return SubplotidValidator("prop", "parent", dflt="geo")
 
+@pytest.fixture()
+def validator_aok():
+    return SubplotidValidator("prop", "parent", dflt="legend", array_ok=True)
 
 # Tests
 
+# Array not ok (default)
 
 # Acceptance
+
 @pytest.mark.parametrize("val", ["geo"] + ["geo%d" % i for i in range(2, 10)])
 def test_acceptance(val, validator):
     assert validator.validate_coerce(val) == val
@@ -43,3 +48,44 @@ def test_rejection_value(val, validator):
         validator.validate_coerce(val)
 
     assert "Invalid value" in str(validation_failure.value)
+
+# Array ok
+
+# Acceptance
+
+@pytest.mark.parametrize("val", ["legend2", ["legend", "legend2"], ("legend1", "legend2")])
+def test_acceptance(val, validator_aok):
+    v = validator_aok.validate_coerce(val)
+    if isinstance(val, tuple):
+        assert val == tuple(v)
+    else:
+        assert val == v
+
+
+# Rejection by type
+@pytest.mark.parametrize("val", [23, [2, 3], {}, set(), np_inf(), np_nan()])
+def test_rejection_type(val, validator_aok):
+    with pytest.raises(ValueError) as validation_failure:
+        validator_aok.validate_coerce(val)
+
+    failure_msg = str(validation_failure.value)
+    assert "Invalid value" in failure_msg or "Invalid elements" in failure_msg
+
+
+# Rejection by value
+@pytest.mark.parametrize(
+    "val",
+    [
+        "",  # Cannot be empty
+        "bogus",  # Must begin with 'geo'
+        "legend0",  # If followed by a number the number must be > 1,
+        ["", "legend"],
+        ("bogus", "legend2")
+    ],
+)
+def test_rejection_value(val, validator_aok):
+    with pytest.raises(ValueError) as validation_failure:
+        validator_aok.validate_coerce(val)
+
+    failure_msg = str(validation_failure.value)
+    assert "Invalid value" in failure_msg or "Invalid elements" in failure_msg
