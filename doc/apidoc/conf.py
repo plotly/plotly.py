b4fed1a65ec3e9a3118e5f5ac174154e8c477911
Cameron DeCoster <cameron.decoster@gmail.com>
2026-09-22 10:21:55 -0600

    chore: Update plotly.py version in docs, API references

--- doc/apidoc/conf.py
+++ doc/apidoc/conf.py
@@ -24,7 +24,7 @@ author = "Plotly"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "7.0.0"
+release = "7.1.0"
 
 
 # -- General configuration ---------------------------------------------------
